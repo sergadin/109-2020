6b5ba4928c5de3ecb36198de9ed4e2ba342b3780
ki-zuev <kizuev02@gmail.com>
2020-03-24 21:30:40 +0300

    Решение задачи 2.3

--- Zuev_KP/2.3/func.c
+++ Zuev_KP/2.3/func.c
@@ -1,1 +1,26 @@
-f
+#include"stdio.h"
+#include"stdlib.h"
+#include"math.h"
+#include"func.h"
+
+int skal(int *a, int *b, int n, RRFUN fsum, RRFUN fproz)
+
+
+{
+     	int res = 0;
+	if (n == 0)
+	{
+	return 0;
+	}
+	res = (*fproz)(a[0], b[0]);
+	for(int i = 1; i < n; i++)
+	{
+		res = (*fsum)(res, (*fproz)(a[i], b[i]));
+	}
+	return res;
+}
+
+
+
+
+
