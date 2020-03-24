3911f6a8625d032042f95ec3a6fb3d11c75c2426
ki-zuev <kizuev02@gmail.com>
2020-03-24 23:38:45 +0300

    Решение задачи 2.3

--- Zuev_KP/2.3/func.c
+++ Zuev_KP/2.3/func.c
@@ -7,21 +7,22 @@ double simp(double a, double b, double n, RRFUN ffunc, RRFUN fsum)
 
 
 {
+	int i;
      	double h = 0, res1 = 0, res2 = 0, res = 0, x = 0;
 	if (n == 0)
 	{
 	return 0;
 	}
 	h = (b-a)/(2*n);
-	for(int i = 1; i < n-1; i++)
+	for(i = 1; i < n-1; i++)
 	{
 		x = a+2*i*h;
-		res1 = (*fsum)(res, (*ffunc)(x, 1));
+		res1 = (*fsum)(res1, ((*ffunc)(x, 1)));
 	}
-	for(int i = 1; i < n; i++)
+	for(i = 1; i < n; i++)
 	{
 		x = a+(2*i-1)*h;
-		res2 = (*fsum)(res, (*ffunc)(x, 1));
+		res2 = (*fsum)(res1, ((*ffunc)(x, 1)));
 	}
 	res = (h/3)*(2*res1 + 4*res2 + (*ffunc)(a, 1) + (*ffunc)(b, 1));
 	return res;
