8fdb898e2bf62469e5be4d10844b5d24196e32ea
Dasha <dasha975@yandex.ru>
2020-03-24 23:56:55 +0300

    task 2.2

--- Rogova_DR/2.2/inte.c
+++ Rogova_DR/2.2/inte.c
@@ -1,6 +1,6 @@
 #include<math.h>
 #include<stdio.h>
-#include"sum.h"
+#include"inte.h"
 
 
 
--- Rogova_DR/2.2/testinte.c
+++ Rogova_DR/2.2/testinte.c
@@ -14,16 +14,16 @@ double modul(double x);
 
 int main(void)
 {
-	int i, num = 4;
-	double res, e = 0.001;
+	int n, i, num = 4;
+	double a, b, res, e = 0.001;
 	double trueans[] = {16.25, 2.5, 6.3, 0};
-	RRF funcs[] = {incorr, line, square, justzero};
+	RRF funcs[] = {cube, line, square, justzero};
 	a = 2;
 	b = 3;
 	n = 10000;
 	for(i = 0; i < num; i++)
 	{
-		res = integ(a, b, n, funcs[i]);
+		res = (*integ)(a, b, n, funcs[i]);
 		if(modul(res - trueans[i]) < e*max3(res, trueans[i], 1))
 		{
 			printf("correct:\n%lf\nmy:\n%lf\nwin)))\n", trueans[i], res);
@@ -33,8 +33,6 @@ int main(void)
 			printf("correct:\n%lf\nmy:\n%lf\nlose(((\n", trueans[i], res);
 		}
 	}
-
-
 }
 
 
@@ -43,12 +41,12 @@ double cube(double x)
 	return x*x*x;
 }
 
-double line(double x);
+double line(double x)
 {
 	return x;
 }
 
-double square(double x);
+double square(double x)
 {
 	return x*x;
 }
