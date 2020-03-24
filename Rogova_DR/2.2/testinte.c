a6b2b67be228cf330064ba4b9e6f2e1518e2ff4a
Dasha <dasha975@yandex.ru>
2020-03-25 02:02:32 +0300

    task 2

--- Rogova_DR/2.2/testinte.c
+++ Rogova_DR/2.2/testinte.c
@@ -14,27 +14,21 @@ double modul(double x);
 
 int main(void)
 {
-<<<<<<< HEAD
 	int n, i, num = 4;
 	double a, b, res, e = 0.001;
 	double trueans[] = {16.25, 2.5, 6.3, 0};
 	RRF funcs[] = {cube, line, square, justzero};
-=======
 	int i, num = 4;
 	double res, e = 0.001;
 	double trueans[] = {16.25, 2.5, 6.3, 0};
-	RRF funcs[] = {incorr, line, square, justzero};
->>>>>>> 76ed227e4382bd0549084eb068794cfcdd37cf92
+	RRF funcs[] = {cube, line, square, justzero};
 	a = 2;
 	b = 3;
 	n = 10000;
 	for(i = 0; i < num; i++)
 	{
-<<<<<<< HEAD
 		res = (*integ)(a, b, n, funcs[i]);
-=======
 		res = integ(a, b, n, funcs[i]);
->>>>>>> 76ed227e4382bd0549084eb068794cfcdd37cf92
 		if(modul(res - trueans[i]) < e*max3(res, trueans[i], 1))
 		{
 			printf("correct:\n%lf\nmy:\n%lf\nwin)))\n", trueans[i], res);
@@ -44,11 +38,6 @@ int main(void)
 			printf("correct:\n%lf\nmy:\n%lf\nlose(((\n", trueans[i], res);
 		}
 	}
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 76ed227e4382bd0549084eb068794cfcdd37cf92
 }
 
 
@@ -57,20 +46,12 @@ double cube(double x)
 	return x*x*x;
 }
 
-<<<<<<< HEAD
 double line(double x)
-=======
-double line(double x);
->>>>>>> 76ed227e4382bd0549084eb068794cfcdd37cf92
 {
 	return x;
 }
 
-<<<<<<< HEAD
 double square(double x)
-=======
-double square(double x);
->>>>>>> 76ed227e4382bd0549084eb068794cfcdd37cf92
 {
 	return x*x;
 }
