2a83c15cf28569268d8d06596de3c28ac5edfe51
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-25 18:37:04 +0300

    Add files via upload

--- Gorev_VA/file.c
+++ Gorev_VA/file.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 
+define eps 0.000001;
+
 double one(double x);
 double one(double x)
 {
 	return 1 + 0*x;
 }
 
-double func(double (*f)(double x));
-double func(double (*f)(double x))
+double derivative(double (*f)(double x), double x);
+double derivative(double (*f)(double x), double x)
+{
+	return (f(x + eps) - f(x)) / eps;
+}
+
+double func(double (*f)(double));
+double func(double (*f)(double))
 {
 	return 2 * (*f)(1);
 }
 
+double SQR(double x);
+double SQR(double x)
+{
+	return x*x;
+}
+
 int main(void)
 {
-	printf("%f", func(one));
+	printf("%f", derivative(SQR, 2));
 	return 0;
 }
