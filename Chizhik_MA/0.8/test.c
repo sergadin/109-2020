d2636999ffc6b352629025b7b1b2ba0057d4d21d
Makar Chizhik <makar.chizhik@gmail.com>
2020-03-25 10:37:07 -0400

    Доработана тестовая функция

--- Chizhik_MA/0.8/test.c
+++ Chizhik_MA/0.8/test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "map.h"
 
 #define EPS 1e-7
+#define N 5
 
 double modulus(double x);
 double max(double *arr, int n);
@@ -25,18 +27,42 @@ static double sq(double x) {
     return x * x;
 }
 
+static double cube(double x) {
+	return x * x * x;
+}
+
 int main(void) {
-    double x = 3.56;
-    dFUNC funcs[] = {zero, one, id, sq};
-    double *stArray;
-    int n = (int)(sizeof(funcs) / sizeof(dFUNC));
-    valArray = getResults(funcs, n, x);
+    double x[] = {3.56, 2};
+    dFUNC funcs[N] = {zero, one, id, sq, cube};
+    double *valArray,
+		   ansArray[][N] = {
+					{0.0, 1.0, 3.56, 12.6736, 45.118016},
+					{0.0, 1.0, 2.0, 4.0, 8.0}
+				   };
+	int n, m;
+	m = (int)(sizeof(x) / sizeof(double));
 
-    for (int i = 0; i < n; i++) {
-    	fprintf(stdout, "arr[%d] = %lf\n", i, valArray[i]);
-    }
+    for (int j = 0; j < m; j++) {
+		valArray = getResults(funcs, N, x[j]);
+		fprintf(stdout, "Test %d\n", j + 1);
 
-    free(valArray);
+		for (int k = 0; k < N; k++) {
+			fprintf(stdout, "Test %d.%d\n", j + 1, k + 1);		    
+		    fprintf(stdout, "Result: %lf\n", valArray[k]);
+		    fprintf(stdout, "Expected answer: %lf\n", ansArray[j][k]);
+		    
+		    if (compareDoubles(valArray[k], ansArray[j][k]) == 0) {
+		        fprintf(stdout, "Test has been passed successfully.\n");
+		    } else {
+		        fprintf(stdout, "Something has gone wrong. Test has not been passed\n");
+				free(valArray);
+				return -1;
+		    }
+		}
+		fprintf(stdout, "Test %d has been passed\n", j + 1);
+		free(valArray);
+	}
+	fprintf(stdout, "The program's output is correct. Everything's great\n");
     return 0;
 }
 
