fad3a9fdc460d225f972f3c00b7cb9ba923b31df
ki-zuev <kizuev02@gmail.com>
2020-03-25 14:57:22 +0300

    Решение задачи 2.3

--- Zuev_KP/2.3/test23.c
+++ Zuev_KP/2.3/test23.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 #include "func.h"
-double func(double a, double b);
+double func1(double a, double b);
+double func2(double a, double b);
 #define MAX(a, b) (((a) > (b))?(a):(b))
 #define MAX1(a, b, c) MAX(a, MAX(b, c))
 
 int main(void)
 {
-	double ep = 0.0001, a = -2, b = 4, result = 0, otvet = 36, k = 0;
+	int i;
+	double ep = 1e-100, a = -2, b = 4, result = 0, k = 0;
 	double e = 1e-4;
-	k = rung(a, b, ep, func);
-	result = simp(a, b, k, func);
-	if (modul(result - otvet) < e*MAX1(result, otvet, 1))
+	double c[] = {36, 1.78244};
+	RRFUN funcs[] = {func1, func2};
+	for(i = 0; i < 2; i++)
 	{
-		printf("пройден\n");
+		k = rung(a, b, ep, funcs[i]);
+		result = simp(a, b, k, funcs[i]);
+		if (modul(result - c[i]) < e*MAX1(result, c[i], 1))
+		{
+			printf("пройден\n");
+		}
+		else
+		{
+			printf("не пройден\n");
+		}
 	}
-	else
-	{
-		printf("не пройден\n");
-	}
-	printf("%lf\n", result);
-	printf("%lf\n", k);
+
 	return 0;
 }
 
@@ -32,7 +38,6 @@ double func1(double a, double b)
 
 double func2(double a, double b)
 {
-	double k = b*(8+2*a-a*a);
+	double k = b*sin(a*a+a-100);
 	return k;
 }
-
