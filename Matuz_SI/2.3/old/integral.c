d8909d310615f4471bd956250f65aa9d85509cd5
fludaw <fludaw@mail.ru>
2020-03-28 00:30:51 +0300

    (• ◡•)

--- Matuz_SI/2.3/old/integral.c
+++ Matuz_SI/2.3/old/integral.c
@@ -1,7 +1,5 @@
 #include "integral.h"
 #include <stdio.h>
-#define Max2(a,b) ((a)>(b) ? (a) : (b))
-#define Max3(a, b, c) Max2(Max2((a), (b)), (c))
 double module(double x)
 {
     if(x < 0)
@@ -41,34 +39,20 @@ double integration(RRfun f, double a, double b, int n)
 
 
 
-struct answer simpsons_integration(RRfun f, double a, double b, double eps)
+double simpsons_integration(RRfun f, double a, double b, double eps)
 {
     double integral, new_integral;
     int n = 2;
-    struct answer ans;
     new_integral = integration(f, a, b, n);
     //printf("%lf", new_integral);
     do
-    {        
+    {
+        
         integral = new_integral;
-        n *= 2;
+        n += 2;
         //printf("%lf", new_integral);
         new_integral = integration(f, a, b, n);
         //printf("%lf", new_integral);
-
-	if (n > 10000000) // какое здесь должно быть ограничение?
-	{
-	    n = -1;
-	    break;
-	}
-
-    }while(module(new_integral-integral)>=eps*Max3(new_integral, integral, 1));
-    printf("%d", n);
-
-    ans.integral = new_integral;
-    
-    ans.n = n;
-    //printf("%d", ans.n);
-
-    return ans;
+    }while(module(new_integral-integral)>=eps);
+    return new_integral;
 }
--- Matuz_SI/2.3/old/integral.h
+++ Matuz_SI/2.3/old/integral.h
@@ -1,12 +1,5 @@
 #include "math.h"
 typedef double(*RRfun)(double x);
-
-struct answer
-{
-    double integral;
-    int n;
-};
-
 double integration(RRfun f, double a, double b, int n);
-struct answer simpsons_integration(RRfun f, double a, double b, double eps);
+double simpsons_integration(RRfun f, double a, double b, double eps);
 double module(double x);
