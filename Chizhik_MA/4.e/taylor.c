9d2156ab4671bb26eaa8740408697f5949d22b98
Makar Chizhik <makar.chizhik@gmail.com>
2020-03-28 17:06:35 +0300

    Обобщение решения

--- Chizhik_MA/4.e/taylor.c
+++ Chizhik_MA/4.e/taylor.c
@@ -4,14 +4,14 @@
 #include "umath.h"
 #include "taylor.h"
 
-double taylor_exp(double x, double precision, int *n, Status *s) {
+double taylor_recurrent(double x, double precision, int *n, Status *s, dFUNC start_value, diFUNC recurrence) {
 	*n = 1;
 	double comp_value, taylor_addendum;
-	
+
 	*s = OK;
-	comp_value = 1;
-	taylor_addendum = x;
-	
+	comp_value = start_value(x);
+	taylor_addendum = start_value(x) * recurrence(x, *n);
+
 	while (compareDoubles(taylor_addendum, 0, precision) != 0) {
 		comp_value += taylor_addendum;
 		if (*n > 1e2) {
@@ -20,55 +20,47 @@ double taylor_exp(double x, double precision, int *n, Status *s) {
 		}
 
 		*n += 1;
-		taylor_addendum *= x / *n;
+		taylor_addendum *= recurrence(x, *n);
 	}
-	
+
 	return comp_value;
 }
 
+
+double one(double x) {
+	return 1 + 0 * x;
+}
+
+double linear(double x, int n) {
+	return x / n;
+}
+
+double taylor_exp(double x, double precision, int *n, Status *s) {
+	return taylor_recurrent(x, precision, n, s, one, linear);
+}
+
+
+double id(double x) {
+	return x;
+}
+
+double sin_square(double x, int n) {
+	return -x * x / (2 * n * (2 * n + 1));
+}
+
 double taylor_sin(double x, double precision, int *n, Status *s) {
-        *n = 1;
-        double comp_value, taylor_addendum;
+        return taylor_recurrent(x, precision, n, s, id, sin_square);
+}
 
-        *s = OK;
-        comp_value = x;
-        taylor_addendum = -x * x * x / factorial(3);
 
-        while (compareDoubles(taylor_addendum, 0, precision) != 0) {
-                comp_value += taylor_addendum;
-                if (*n > 1e2) {
-                        *s = TOO_LONG;  
-                        return comp_value;
-                }
-
-                *n += 1;
-                taylor_addendum *= -x * x / (2 * (*n) * (2 * (*n) + 1));
-        }
-
-        return comp_value;
+double cos_square(double x, int n) {
+	return -x * x / ((2 * n - 1) * 2 * n);
 }
 
 double taylor_cos(double x, double precision, int *n, Status *s) {
-        *n = 1;
-        double comp_value, taylor_addendum;
-
-        *s = OK;
-        comp_value = 1;
-        taylor_addendum = - x * x / 2;
-
-        while (compareDoubles(taylor_addendum, 0, precision) != 0) {
-                comp_value += taylor_addendum;
-                if (*n > 1e2) {
-                        *s = TOO_LONG;  
-                        return comp_value;
-                }
-
-                *n += 1;
-                taylor_addendum *= -x * x / ((2 * (*n) - 1) * 2 * (*n));
-        }
-
-        return comp_value;
+        return taylor_recurrent(x, precision, n, s, one, cos_square);
 }
+
 
 double taylor_log(double x, double precision, int *n, Status *s) {
         *n = 1;
--- Chizhik_MA/4.e/taylor.h
+++ Chizhik_MA/4.e/taylor.h
@@ -1,4 +1,5 @@
 typedef double (*dFUNC)(double x);
+typedef double (*diFUNC)(double x, int n);
 typedef enum {
 	OK,
 	TOO_LONG,
@@ -6,7 +7,16 @@ typedef enum {
 } Status;
 typedef double (*taylorFUNC)(double x, double precision, int *n, Status *s);
 
+double taylor_recurrent(double x, double precision, int *n, Status *s, dFUNC start_value, diFUNC recurrence);
 double taylor_exp(double x, double precision, int *n, Status *s);
 double taylor_sin(double x, double precision, int *n, Status *s);
 double taylor_cos(double x, double precision, int *n, Status *s);
 double taylor_log(double x, double precision, int *n, Status *s);
+
+double one(double x);
+double linear(double x, int n);
+
+double id(double x);
+double sin_square(double x, int n);
+
+double cos_square(double x, int n);
