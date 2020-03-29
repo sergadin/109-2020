c0229690c8bbce811ea89f42ac60217cc9160ca6
ponomar4643 <kkkitya@yandex.ru>
2020-03-29 19:48:35 +0300

    Решение задачи 1.1

---
+++ Ponomar_AE/1.1/decision.1.1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include "decision.h"
+
+double dcsn_f_x(double X_1, double X_2, double E, RRF func)
+{
+        double left = X_1, mid = (X_1+X_2)/2, right = X_2, f_left, f_mid, f_right, cur_length = X_2-X_1;
+
+        if((*func)(left)*(*func)(right) >= 0)
+                return 10000;
+
+        while(cur_length > E)
+        {
+                mid = (left+right)/2;
+                f_mid = (*func)(mid);
+                if(f_mid*f_right <= 0)
+                {
+                        left = mid;
+                        f_left = f_mid;
+                }
+                else
+                {         
+                        right = mid;
+                        f_right = f_mid;
+                }
+                printf("%lf %lf\n", left, right);
+                cur_length = right-left;
+        }
+
+        return (left+right)/2;
+}
+
---
+++ Ponomar_AE/1.1/decision.1.1.h
@@ -0,0 +1,3 @@
+typedef double (*RRF)(double x);
+double dcsn_f_x(double X_1, double X_2, double E, RRF func);
+
