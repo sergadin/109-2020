f456b76fc50ae92ad6a70e3c0f7d338ec9e68d94
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-30 22:30:40 +0300

    Add files via upload

--- Gorev_VA/2.2/trap_sum.c
+++ Gorev_VA/2.2/trap_sum.c
@@ -1,5 +1,7 @@
 #include "trap_sum.h"
 
+#define eps 0.000001
+
 double trap_sum(double (*f) (double), double a, double b, int N)
 {
 	double S = 0, x1, x2;
@@ -11,4 +13,21 @@ double trap_sum(double (*f) (double), double a, double b, int N)
 		x1 = x2;
 	}
 	return S;
+}
+
+double interal(double (*f) (double), double a, double b, int* Error)
+{
+	double I = 0, I1 = 0;
+	int N = 100;
+	for (int i = 2; i <= 10; i++, N *= 10, I1 = I)
+	{
+		I = trap_sum(f, a, b, N);
+		if (((I - I1) < eps) && ((I1 - I) < eps))
+		{
+			*Error = 0;
+			return I;
+		}
+	}
+	*Error = 1;
+	return I;
 }
