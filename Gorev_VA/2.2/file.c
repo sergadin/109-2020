c07e9c7b7d364346b707fcbdfd03e3c1862c0c73
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-30 19:39:12 +0300

    Update file.c

--- Gorev_VA/2.2/file.c
+++ Gorev_VA/2.2/file.c
@@ -39,14 +39,15 @@ int main(void)
 	printf("Integrating with trapeziodal rule...\n");
 	printf("f(x) = x at [0, 1]\n");
 	printf(" N = 100:   %f\n N = 1000:  %f\n N = 10000: %f\n", trap_sum(X, 0, 1, 100), trap_sum(X, 0, 1, 1000), trap_sum(X, 0, 1, 10000));
-	printf(" actual:    %f\n", 0.5)
+	printf(" actual:    %f\n", 0.5);
 
 	printf("f(x) = x^2 at [-1, 2]\n");
 	printf(" N = 100:   %f\n N = 1000:  %f\n N = 10000: %f\n", trap_sum(SQR, -1, 2, 100), trap_sum(SQR, -1, 2, 1000), trap_sum(SQR, -1, 2, 10000));
-	printf(" actual:    %f\n", 3.0)
+	printf(" actual:    %f\n", 3.0);
 
 	printf("f(x) = x^6 - x^4 - 2x + 1 at [0, 2]\n");
 	printf(" N = 100:   %f\n N = 1000:  %f\n N = 10000: %f\n", trap_sum(F1, 0, 2, 100), trap_sum(F1, 0, 2, 1000), trap_sum(F1, 0, 2, 10000));
-	printf(" actual:    %f\n", 9.8857142857)
+	printf(" actual:    %f\n", 9.8857142857);
+	
 	return 0;
 }
