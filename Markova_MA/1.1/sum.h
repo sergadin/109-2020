30e9d28202a87a271de0fabd22eae36b6676aa0f
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-03-30 15:14:59 +0300

    у

--- Markova_MA/1.1/sum.h
+++
@@ -1,9 +0,0 @@
-#define T 10000000
-typedef double(*RRFUN)(double x);
-
-double root(RRFUN f, double a, double b, double eps);
-double abcf(RRFUN f, double x);
-double f1 (double x);
-double f2 (double x);
-double f3 (double x);
-double f4 (double x);
