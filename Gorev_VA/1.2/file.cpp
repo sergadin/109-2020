a7822d49eff2ae136aae3ded0032cb0afd08aa1c
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-30 23:19:53 +0300

    Add files via upload

---
+++ Gorev_VA/1.2/file.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <сmath>
+#include "root.h"
+
+double F1(double x);
+double F1(double x)
+{
+	return x*x - 1.0;
+}
+
+double F2(double x);
+double F2(double x)
+{
+	return 1.0/256.0 * x*x*x*x - 4.0;
+}
+
+double F3(double x);
+double F3(double x)
+{
+	return 1.0/(x*x*x) - 2.0;
+}
+
+double F4(double x);
+double F4(double x)
+{
+	return x*x*x*x - x*x;
+}
+
+double Arctg(double x);
+double Arctg(double x)
+{
+	return atan(x);
+}
+
+int main(void)
+{
+	int* Error;
+	double R;
+	Error = (int*)malloc(1 * sizeof(int));
+	
+	*Error = 0;
+	R = root(F1, 0.0, 2.0, Error);
+	printf("f(x) = x^2 - 1 at [0, 2]\n");
+	if (*Error)
+		printf(" Error\n");
+	else
+		printf(" calculated root: %f\n actual root:     %f\n", R , 1.0);
+	
+	R = root(F2, -2.0, -6.0, Error);
+	printf("f(x) = 1/256 x^4 - 4 at [-2, -6]\n");
+	if (*Error)
+		printf(" Error\n");
+	else
+		printf(" calculated root: %f\n actual root:     %f\n", R, -5.6568542495);
+	
+	R = root(F3, 0.5, 2.0, Error);
+	printf("f(x) = 1/x^3 - 2 at [0.5, 2]\n");
+	if (*Error)
+		printf(" Error\n");
+	else
+		printf(" calculated root: %f\n actual root:     %f\n", R, 0.7937005293);
+	
+	R = root(F4, -0.75, 0.75, Error);
+	printf("f(x) = x^4 - x^2 at [-0.75, 0.75]\n");
+	if (*Error)
+		printf(" Error\n");
+	else
+		printf(" calculated root: %f\n actual root:     %f\n", R, 0.0);
+	
+	/*R = root(sin, -5, 5, Error);
+	printf("f(x) = arctg(x) at [-5, 5]\n");
+	if (*Error)
+		printf(" Error\n");
+	else
+		printf(" calculated root: %lf\n actual root:     %f\n", R, 0.0);*/
+		
+	printf("%f\n", atan(3.3));
+	
+	return 0;
+}
