5b4826473626ef1cca74a1fadd6c61cf3042e5fc
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-30 18:57:46 +0300

    Add files via upload

--- Gorev_VA/2.2/file.c
+++ Gorev_VA/2.2/file.c
@@ -7,7 +7,7 @@ double trap_sum(double (*f) (double), double a, double b, int N)
 	x1 = a;
 	for (int i = 1; i <= N; i++)
 	{
-		x2 = (a * (double(i) / double(N)) + b * (double(N - i) / double(N)));
+		x2 = (a * ((double)i / (double)N) + b * ((double)(N - i) / (double)N));
 		S += 0.5 * (f(x2) + f(x1)) / double(N);
 	}
 	if (a > b)
