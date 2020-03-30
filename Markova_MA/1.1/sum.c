782d953434ae00a1dbcd70b3523ad3f9a1aad745
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-03-30 20:38:51 +0300

    k

--- Markova_MA/1.1/sum.c
+++ Markova_MA/1.1/sum.c
@@ -12,9 +12,8 @@ double abcf(RRFUN f, double x)
 		return (*f)(x);
 	}
 }
-void root(RRFUN f, double a, double b, double eps)
+double root(RRFUN f, double a, double b, double eps);
 {
-	int check = 0;
 	double m = (a + b)/2;
 	while(b - a < eps)
 	{
@@ -27,13 +26,6 @@ void root(RRFUN f, double a, double b, double eps)
 			b = (*f)(m);
 		}
 		m = (a + b)/2;
-		check++;
-		if (check > T)
-		{
-			printf("FAULT");
-			break;
-			return;
-		}
 	}
-	printf("Answer = %e\nfunction value = %e\nIterations = %d\n", m, absf(f, m), check);	
+	return m;
 }
--- Markova_MA/1.1/test_sum.c
+++ Markova_MA/1.1/test_sum.c
@@ -5,22 +5,11 @@ double f1 (double x)
 {
 	return x*x + 5*x - 6;
 }
-double f2 (double x)
-{
-	return x*x + 1;
-}
-double f3 (double x)
-{
-	return x*x*x;
-}
-double f4 (double x)
-{
-	return x*x + 5*x;
-}
+
 int main(void)
 {
-	double a = -4, b = 2, eps = 0,0001;
-	if(f1(a)*f1(b) >= 0 || f2(a)*f2(b) >= 0 || f3(a)*f3(b) >= 0 || f4(a)*f4(b) >= 0)
+	double a = -4, b = 2, eps = 0,0001, m;
+	if(f1(a)*f1(b) >= 0)
 	{
 		printf("FAULT");
 	}
@@ -31,31 +20,8 @@ int main(void)
 		a = b;
 		b = temp;
 	}
-	root(f1, a, b, eps);
-	if(f2(a) > 0)
-	{
-		double temp;
-		temp = a;
-		a = b;
-		b = temp;
-	}
-	root(f2, a, b, eps);
-	if(f3(a) > 0)
-	{
-		double temp;
-		temp = a;
-		a = b;
-		b = temp;
-	}
-	root(f3, a, b, eps);
-	if(f4(a) > 0)
-	{
-		double temp;
-		temp = a;
-		a = b;
-		b = temp;
-	}
-	root(f4, a, b, eps);
+	m = root(f1, a, b, eps);
+	printf("Answer = %e\nfunction value = %e\n", m, absf1(f, m));
 	return 0;
 }
 
