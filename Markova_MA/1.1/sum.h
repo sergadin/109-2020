a2ddbcf74658b4420631bfd64dd3731f6d80692d
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-03-30 20:42:36 +0300

    k

--- Markova_MA/1.1/sum.h
+++ Markova_MA/1.1/sum.h
@@ -1,9 +1,5 @@
-#define T 10000000
 typedef double(*RRFUN)(double x);
 
-void root(RRFUN f, double a, double b, double eps);
+double root(RRFUN f, double a, double b, double eps);
 double abcf(RRFUN f, double x);
 double f1 (double x);
-double f2 (double x);
-double f3 (double x);
-double f4 (double x);
