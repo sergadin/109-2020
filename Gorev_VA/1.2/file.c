c676b372dea828b2af499bb59751d12562fb02ac
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-30 16:16:35 +0300

    Add files via upload

--- Gorev_VA/1.2/file.c
+++ Gorev_VA/1.2/file.c
@@ -5,7 +5,7 @@
 double derivative(double (*f)(double), double x);
 double derivative(double (*f)(double), double x)
 {
-	return ((*f)(x + eps/2) - (*f)(x)) / (eps/2);
+	return ((*f)(x + eps / 2.0) - (*f)(x)) / (eps / 2.0);
 }
 
 double root(double (*f)(double), double a, double b);
@@ -19,24 +19,24 @@ double root(double (*f)(double), double a, double b)
 		b = c;
 	}
 	c = a;
-	if ((b - a) < (eps / 2))
+	if ((b - a) < (eps))
 		return c;
 	while (1)
 	{
-		printf("%f\n", derivative(f, b));
+		//printf("%f\n", derivative(f, b));
 		c = b - (*f)(b) / derivative(f, b);
-		printf("b %f\n", c);
+		//printf("b %f\n", c);
 		if ((c > b) || (c < a))
 		{
 			c = a - (*f)(a) / derivative(f, a);
-			printf("a %f\n", c);
+			//printf("a %f\n", c);
 			if (((b - a) < (eps)) || ((c - a) < (eps)))
 				break;
 			else
 				a = c;
 		}
 		else
-			if (((b - a) < (eps / 2)) || ((b - c) < (eps / 2)))
+			if (((b - a) < (eps)) || ((b - c) < (eps)))
 				break;
 			else
 				b = c;
@@ -47,13 +47,13 @@ double root(double (*f)(double), double a, double b)
 double one(double x);
 double one(double x)
 {
-	return 1 + 0*x;
+	return 1.0 + 0.0*x;
 }
 
 double func(double (*f)(double));
 double func(double (*f)(double))
 {
-	return 2 * (*f)(1);
+	return 2.0 * (*f)(1.0);
 }
 
 double F1(double x);
@@ -71,13 +71,13 @@ double F2(double x)
 double F3(double x);
 double F3(double x)
 {
-	return 1.0/(x*x) - 2.0;
+	return 1.0/(x*x*x) - 2.0;
 }
 
 int main(void)
 {
-	//printf("%f\n", root(F1, 0, 2));
-	printf("%f\n", root(F2, -6, -2));
-	//printf("%f\n", root(F3, 1 / 2, 2));
+	printf("%f\n", root(F1, 0.0, 2.0));
+	printf("%f\n", root(F2, -6.0, -2.0));
+	printf("%f\n", root(F3, 0.5, 2.0));
 	return 0;
 }
