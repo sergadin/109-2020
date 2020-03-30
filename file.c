a592273be686a28c094922b3da310f745f0c7d7b
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-30 13:06:09 +0300

    Add files via upload

---
+++ file.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define eps 0.000001
+
+double derivative(double (*f)(double), double x);
+double derivative(double (*f)(double), double x)
+{
+	return ((*f)(x + eps / 100) - (*f)(x)) / (eps / 100);
+}
+
+/*double root(double (*f)(double), double a, double b);
+double root(double (*f)(double), double a, double b)
+{
+	double der = 0;
+	double c;
+	if (a > b)
+	{
+		der = a;
+		a = b;
+		b = der;
+	}
+	c = a;
+	while (((b - a) >= eps) || (b - c) >= eps)
+	{
+		der = derivative(f, b);
+		c = b - (*f)(
+	}
+}*/
+
+double one(double x);
+double one(double x)
+{
+	return 1 + 0*x;
+}
+
+double func(double (*f)(double));
+double func(double (*f)(double))
+{
+	return 2 * (*f)(1);
+}
+
+double SQR(double x);
+double SQR(double x)
+{
+	return x*x;
+}
+
+
+
+int main(void)
+{
+	printf("%f\n", derivative(SQR, 2));
+	return 0;
+}
