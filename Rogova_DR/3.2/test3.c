bcd2596a27c3eb1f3558ee6cfe6d66b95cb0cf2d
Dasha <dasha975@yandex.ru>
2020-03-31 21:39:28 +0300

    task 3

--- Rogova_DR/3.2/test3.c
+++ Rogova_DR/3.2/test3.c
@@ -4,9 +4,9 @@
 
 
 
-double line(double x);
-double sqr(double x);
-double zer(double x);
+double cube(double x);
+double somef(double x);
+double cosin(double x);
 
 
 
@@ -16,11 +16,11 @@ double zer(double x);
 int main(void)
 {
 	int i, num = 3;
-	double res, eps = 0.001;
-	double tran[] = {1, 1, 0};
-	RRF funcs[] = {line, sqr, zer};
-	double a = 1;
-	double b = 2;
+	double res, eps = 0.00000001;
+	double tran[] = {-1, -1, -2};
+	RRF funcs[] = {cosin, cube, somef};
+	double a = -1;
+	double b = 4;
 	for(i = 0; i < num; i++)
 	{
 		res = mini(a, b, eps, funcs[i]);
@@ -30,20 +30,20 @@ int main(void)
 
 
 
-double line(double x)
+double cosin(double x)
 {
-	return x;
+	return cos(x);
 }
 
 
-double sqr(double x)
+double cube(double x)
 {
-	return x*x;
+	return x*x*x;
 }
 
 
 
-double zer(double x)
+double somef(double x)
 {
-	return 0*x;
+	x*x - 2;
 }
