1d810790b9b75b7c1f92145312512e0fedc1be55
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-31 22:16:19 +0300

    Add files via upload

--- Gorev_VA/3.2/file.c
+++ Gorev_VA/3.2/file.c
@@ -31,13 +31,13 @@ double Min(double(*f) (double), double a, double b, int* Error)
 		{
 			a = x0;
 			x0 = x1;
-			q = 1; // Г­Г  Г±Г«ГҐГ¤ГіГѕГ№ГҐГ¬ ГёГ ГЈГҐ Г­ГіГ¦Г­Г® Г±Г·ГЁГІГ ГІГј Гµ1
+			q = 1; // на следующем шаге нужно считать х1
 		}
 		else
 		{
 			b = x1;
 			x1 = x0;
-			q = 0; // Г­Г  Г±Г«ГҐГ¤ГіГѕГ№ГҐГ¬ ГёГ ГЈГҐ Г­ГіГ¦Г­Г® Г±Г·ГЁГІГ ГІГј Гµ0
+			q = 0; // на следующем шаге нужно считать х0
 		}
 
 		if ((b - a) < eps)
@@ -58,6 +58,7 @@ double Min(double(*f) (double), double a, double b, int* Error)
 		f1 = f(x1);
 		f2 = f(x2);
 		x1 = -0.5 * (x2 * x2 * f0 - x0 * x0 * f2 + x0 * x0* f1 - x1 * x1 * f0 + x1 * x1 * f2 - x2 * x2 * f1) / (f2 * x0 - f0 * x2 + f0 * x1 - f1 * x0 + f1 * x2 - f2 * x1);
+		printf("  %f\n", x1);
 		if ((x1 >= a) && (x1 <= b))
 		{
 			*Error = 0;
