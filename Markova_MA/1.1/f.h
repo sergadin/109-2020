0a00fe7505c5abf2df75caadc3c1a77c16ae809e
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-03-31 15:51:04 +0300

    Update f.h

--- Markova_MA/1.1/f.h
+++ Markova_MA/1.1/f.h
@@ -1,6 +1,6 @@
 typedef enum { ROOT_OK = 0, ROOT_NOT } ErrorCode;
 double ab (double m);
-double root(double (*f)(double), double a, double b, double eps);
+double root(double (*f)(double), double a, double b, double eps, ErrorCode *perr);
 double f1 (double x);
 double f2 (double x);
 double f3 (double x);
