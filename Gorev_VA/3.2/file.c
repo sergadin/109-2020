a393ec794ff81022f75f06ab280ed7cef2b14584
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-03-31 22:31:27 +0300

    Add files via upload

--- Gorev_VA/3.2/file.c
+++ Gorev_VA/3.2/file.c
@@ -25,7 +25,7 @@ double Min(double(*f) (double), double a, double b, int* Error)
 		else
 			x0 = b - (b - a) * (sqrt(5) - 1) * 0.5;
 
-		printf("%f  %f\n", x0, x1);
+		printf("  %f %f %f %f\n", a, x0, x1, b);
 
 		if ((*f)(x0) >= (*f)(x1))
 		{
@@ -57,7 +57,7 @@ double Min(double(*f) (double), double a, double b, int* Error)
 		f0 = f(x0);
 		f1 = f(x1);
 		f2 = f(x2);
-		x1 = -0.5 * (x2 * x2 * f0 - x0 * x0 * f2 + x0 * x0* f1 - x1 * x1 * f0 + x1 * x1 * f2 - x2 * x2 * f1) / (f2 * x0 - f0 * x2 + f0 * x1 - f1 * x0 + f1 * x2 - f2 * x1);
+		x1 = -0.5 * (x2 * x2 * f0 - x0 * x0 * f2 + x0 * x0 * f1 - x1 * x1 * f0 + x1 * x1 * f2 - x2 * x2 * f1) / (f2 * x0 - f0 * x2 + f0 * x1 - f1 * x0 + f1 * x2 - f2 * x1);
 		printf("  %f %f\n", x1, b - a);
 		if ((x1 >= a) && (x1 <= b))
 		{
