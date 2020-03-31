4b37f3e129aab0331294e7c3a336cd1f8bd51506
Dasha <dasha975@yandex.ru>
2020-03-31 23:38:40 +0300

    task 3

--- Rogova_DR/3.2/test3.c
+++ Rogova_DR/3.2/test3.c
@@ -16,10 +16,10 @@ double cosin(double x);
 int main(void)
 {
 	int i, num = 3;
-	double res, eps = 0.00000001;
-	double tran[] = {-1, -1, -2};
+	double res, eps = 0.001;
+	double tran[] = {-1, 0, -19.9};
 	RRF funcs[] = {cosin, cube, somef};
-	double a = -1;
+	double a = 0;
 	double b = 4;
 	for(i = 0; i < num; i++)
 	{
@@ -45,5 +45,5 @@ double cube(double x)
 
 double somef(double x)
 {
-	x*x - 2;
+	return (x*x*x*x*x - 3*x*x*x*x);
 }
