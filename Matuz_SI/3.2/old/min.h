c980234950dd142f9379a0dd6b0a3bb3eb54aa18
fludaw <fludaw@mail.ru>
2020-04-01 15:34:03 +0300

    :(
    :(

--- Matuz_SI/3.2/old/min.h
+++
@@ -1,7 +0,0 @@
-typedef double(*RRfun)(double x);
-
-void Min_Search_Golden_Section(RRfun f, double* a, double* b, double eps);
-double parabolic_approx(double x1, double x2, double x3, RRfun f);
-
-double module(double x);
-int stop(double x0, double x1, double eps);
--- Matuz_SI/3.2/very old/min.h
+++
@@ -1,6 +0,0 @@
-typedef double(*RRfun)(double x);
-
-void Min_Search_Golden_Section(RRfun f, double* a, double* b, double eps);
-
-double module(double x);
-int stop(double x0, double x1, double eps);
