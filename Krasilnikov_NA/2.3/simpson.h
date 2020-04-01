857e4451427274e0ee7e49218dc9f5aabec78291
kolorono <62987548+kolorono@users.noreply.github.com>
2020-04-01 18:37:57 +0300

    Update simpson.h
    Чуточку изменил названия

--- Krasilnikov_NA/2.3/simpson.h
+++ Krasilnikov_NA/2.3/simpson.h
@@ -1,4 +1,4 @@
 typedef double(*RRFUN)(double x);
 
-double simpson(double a, double b, double epsilon, RRFUN ffunc);
-double integrate(double a, double b, int n, RRFUN ffunc);
+double simpson(double a, double b, int n, RRFUN function);
+double integrate(double a, double b, double epsilon, RRFUN function);
