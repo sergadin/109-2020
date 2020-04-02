dd9e7d11e6ce0f5c027e225c3f35c1e0225004e3
kolorono <kolorono@yandex.ru>
2020-04-02 21:53:04 +0300

    Добавил структуру

--- Krasilnikov_NA/2.3/simpson.h
+++ Krasilnikov_NA/2.3/simpson.h
@@ -2,9 +2,9 @@ typedef double(*RRFUN)(double x);
 
 struct result
 {
-  double rofi //result of integration
-  int n
-}
+  double rofi; //result of integration
+  int n;
+};
 
 double simpson(double a, double b, int n, RRFUN function);
-double integrate(double a, double b, double epsilon, RRFUN function);
+struct result integrate(double a, double b, double epsilon, RRFUN function);
