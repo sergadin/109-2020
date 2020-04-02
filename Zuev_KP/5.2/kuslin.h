b7774cd92519e3ce5ef80ea19a8514cf78e8d1c7
ki-zuev <kizuev02@gmail.com>
2020-04-02 18:28:17 +0300

    Решение задачи 5.2

--- Zuev_KP/5.2/kuslin.h
+++ Zuev_KP/5.2/kuslin.h
@@ -1,2 +1,27 @@
-#include <iostream>
+#include"iostream"
+using namespace std;
 
+class klin
+{
+	private:
+		int n;
+		double *x;
+		double *y;
+	public:
+		klin(int dlin, double *a, double *b)
+		{
+			n = dlin;
+			x = (double*) malloc(n *sizeof(double));
+			y = (double*) malloc(n *sizeof(double));
+			for(int i = 0; i < n; i++)
+			{
+				x[i] = a[i];
+				y[i] = b[i];
+			}
+		}
+		double interpolation(double x0)
+		{
+			double y0 = 0;
+			return y0;
+		}
+};
--- Zuev_KP/5.2/test52.cpp
+++ Zuev_KP/5.2/test52.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include "kuslin.h"
 
+using namespace std;
 
-int main(void)
+int main()
 {
-	int n = 5;
-	double *x = (double*) malloc(n *sizeof(double));
-	double *y = (double*) malloc(n *sizeof(double));
-	double result = 0;
-	double c[] = {8, -1.01002, -1, 0};
-
-
+	int n0 = 5;
+	double *x = (double*) malloc(n0 *sizeof(double));
+	double *y = (double*) malloc(n0 *sizeof(double));
+	double x0 = 9;
+	double y0[] = {0};
+	for(int i = 0; i < n0; i++)
+	{
+		x[i] = i;
+		y[i] = i;
+	}
+	klin l(n0, x, y);
+	y0[0] = l.newPoint(x0);
+	for(int i = 0; i < 1; i++)
+	{
+		cout<< "" << y0[i] <<endl;
+	}
 	return 0;
 }
-
-
