df615c0333264f0ba10f8acfded5feb20b07a870
ki-zuev <kizuev02@gmail.com>
2020-04-02 16:15:07 +0300

    Решение задачи 5.2

---
+++ Zuev_KP/5.2/test52.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "kuslin.h"
+
+
+int main(void)
+{
+	int n = 5;
+	double *x = (double*) malloc(n *sizeof(double));
+	double *y = (double*) malloc(n *sizeof(double));
+	double result = 0;
+	double c[] = {8, -1.01002, -1, 0};
+
+
+	return 0;
+}
+
+
