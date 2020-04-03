dba632b78dafff1bb30304fc2cc936c243d706d9
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-04-03 22:58:45 +0300

    Add files via upload

--- Gorev_VA/4.sin/file.c
+++ Gorev_VA/4.sin/file.c
@@ -16,27 +16,27 @@ int main(void)
 	if (*Error)
 		printf(" Error\n actual value:     %f\n", sin(0));
 	else
-		printf(" calculated value: %f\n actual value:     %f", S, sin(0));
+		printf(" calculated value: %f\n actual value:     %f\n", S, sin(0));
 
 	S = Sin(1, 0.00001, Error);
 	printf("x = 1, eps = 10^-5:\n");
 	if (*Error)
 		printf(" Error\n actual value:     %f\n", sin(1));
 	else
-		printf(" calculated value: %f\n actual value:     %f", S, sin(1));
+		printf(" calculated value: %f\n actual value:     %f\n", S, sin(1));
 
 	S = Sin(100, 0.00001, Error);
 	printf("x = 100, eps = 10^-5:\n");
 	if (*Error)
 		printf(" Error\n actual value:     %f\n", sin(100));
 	else
-		printf(" calculated value: %f\n actual value:     %f", S, sin(100));
+		printf(" calculated value: %f\n actual value:     %f\n", S, sin(100));
 
 	S = Sin(100, 0.001, Error);
 	printf("x = 100, eps = 10^-2:\n");
 	if (*Error)
 		printf(" Error\n actual value:     %f\n", sin(100));
 	else
-		printf(" calculated value: %f\n actual value:     %f", S, sin(100));
+		printf(" calculated value: %f\n actual value:     %f\n", S, sin(100));
 
 }
