cbc003ec55c5e70616f6ba13deec53563397ee69
ponomar4643 <kkkitya@yandex.ru>
2020-04-04 00:23:31 +0300

    Решение задачи 3.2

--- Ponomar_AE/3.2/minimum.c
+++ Ponomar_AE/3.2/minimum.c
@@ -3,9 +3,11 @@
 #include <stdio.h> 
 #include "minimum.h"
 
+#define MIN(a,b) ((a)<(b) ? (a) : (b))
+
 double minimum(double xa, double xb, double E, RRFUN func)
 {
-	double x, minimum, x1, f_x1, x2, f_x2, f_xa, f_xb, f_x;
+	double x_minimum, x, minimum, x1, f_x1, x2, f_x2, f_xa, f_xb, f_x;
 	double zolotoe_chislo = (1 + sqrt(5))/2;
 	while (fabs(xa-xb) >= E)
 	{
@@ -22,8 +24,22 @@ double minimum(double xa, double xb, double E, RRFUN func)
 			xb = x2;
 		}
 	}
+
 	x = (xa+xb)/2;
-	minimum = (*func)(x);
+	f_x = (*func)(x);
+	f_xa = (*func)(xa);
+	f_xb = (*func)(xb);
+
+	x_minimum = x - 0.5*((x-xa)*(x-xa)*(f_x-f_xb)-(x-xb)*(x-xb)*(f_x-f_xa))/((x-xa)*(f_x-f_xb)-(x-xb)*(f_x-f_xa));
+
+	if ((f_xa > f_x) && (f_xb > f_x))
+	{
+		minimum = (*func)(x_minimum);
+	}
+	else
+	{
+		minimum = MIN(f_xa, f_xb);
+	}
 
 	return minimum;
 }
