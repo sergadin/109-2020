45a8ecbcfaa423d77623ae038a604e0ee4157528
kolorono <62987548+kolorono@users.noreply.github.com>
2020-04-04 00:27:29 +0300

    Чуточку поправил

--- Krasilnikov_NA/1.2/Newton.c
+++ Krasilnikov_NA/1.2/Newton.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
+#include <math.h>
 #include"Newton.h"
 
-struct result calculation(double a, double b, double epsilon, RRFUN function);
+struct result calculation(double a, double b, double epsilon, RRFUN function, ErrorCode *error);
 {
-  double length = (b - a);
+  double A;
   struct result answer;
+  *error = INT_OK;
   answer.iterations = 0;
   if ((*function)(a) * (*function)(b) > 0)
   {
     answer.rofc = 0;
+    *error = CALC_NEOK;
+  }
+  if (fabs(*function)(a) <= epsilon)
+  {
+    answer.rofc = a;
     return answer;
   }
-  while (length > epsilon)
+  if (fabs(*function)(b) <= epsilon)
   {
-    length = length/2;
-    if ((*function)(a) = 0)
-    {
-      answer.rofc = a;
-      return answer;
-    }
-    if ((*function)(b) = 0)
-    {
-      answer.rofc = b;
-      return answer;
-    }
-    a += length;
-    if ((*function)(a) * (*function)(b) > 0)
-    {
-      a -= length;
-      b -= length;
-    }
-    answer.iterations++;
+    answer.rofc = b;
+    return answer;
   }
-  answer.rofc = (a + b)/2;
-  return answer;
+  if ((second_derivative(function, a, epsilon)) * (*function)(a) > 0)
+  {
+    A = a;
+  }
+  else
+  {
+    A = b;
+  }
+  
+
+}
+double derivative(RRFUN function, double x, double h)
+{
+  return ((*function)(x + h) - (*function)(x))/(h)
+}
+double second_derivative(RRFUN function, double x, double h)
+{
+  return ((*function)(x + 2 * h) - 2 * (*function)(x + h) + (*function)(x))/(h * h)
 }
