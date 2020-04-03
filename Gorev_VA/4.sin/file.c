abe7d794e8075a2c2ac7af65ac94c275adea52cb
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-04-04 01:55:05 +0300

    Update file.c

--- Gorev_VA/4.sin/file.c
+++ Gorev_VA/4.sin/file.c
@@ -11,28 +11,28 @@ int main(void)
 
 	S = Sin(0, 0.000001, E);
 	printf("x = 0, eps = 10^-6:\n");
-	if (E = ERROR_OF_INT)
+	if (E == ERROR_OF_INT)
 		printf(" Error\n actual value:     %f\n", sin(0));
 	else
 		printf(" calculated value: %f\n actual value:     %f\n", S, sin(0));
 
 	S = Sin(1, 0.000001, E);
 	printf("x = 1, eps = 10^-6:\n");
-	if (E = ERROR_OF_INT)
+	if (E == ERROR_OF_INT)
 		printf(" Error\n actual value:     %f\n", sin(1));
 	else
 		printf(" calculated value: %f\n actual value:     %f\n", S, sin(1));
 
 	S = Sin(10, 0.000001, E);
 	printf("x = 10, eps = 10^-6:\n");
-	if (E = ERROR_OF_INT)
+	if (E == ERROR_OF_INT)
 		printf(" Error\n actual value:     %f\n", sin(10));
 	else
 		printf(" calculated value: %f\n actual value:     %f\n", S, sin(10));
 
 	S = Sin(-10, 0.000001, E);
 	printf("x = -10, eps = 10^-6:\n");
-	if (E = ERROR_OF_INT)
+	if (E == ERROR_OF_INT)
 		printf(" Error\n actual value:     %f\n", sin(-10));
 	else
 		printf(" calculated value: %f\n actual value:     %f\n", S, sin(-10));
