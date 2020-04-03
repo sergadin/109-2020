e7fc3d3b06ed10d6edadb8a11e2bd4974b6f1892
ponomar4643 <kkkitya@yandex.ru>
2020-04-03 17:59:20 +0300

    Решение задачи 2.1

--- Ponomar_AE/2.1/integral.c
+++ Ponomar_AE/2.1/integral.c
@@ -3,16 +3,36 @@
 #include <stdlib.h>
 #include "integral.h"
 
+#define max(a,b) ((a)>(b) ? (a) : (b))
+#define MAX(a,b,c) (max(max((a), (b)), (c)))
+
 double integral(RRF func, double X_a, double X_b, double E, ErrorCode *error_code)
 {
-	double result = 0, dx = (X_b-X_a)*E;
-	for (int i = 0; i < 1/E; i++)
-       	{
-		result += func(X_a + dx * (i + 0.5));
-	}
-	 
-	result = result * dx;
+	int n = 2;
+	double result1 = 0, result2 = 0, x, dx = (X_b - X_a)/n;
 	
-	return result;
+	for (int i = 0; i < n; i++) 
+	{
+		x = X_a + (i * 0.5) * dx;
+		result2 += func(x) * dx;
+	}
+	n = n * 2;
+
+	while ((fabs(result1-result2))/3 >= (E*MAX(1, result1, result2)))
+	{
+		result1 = result2;
+		result2 = 0;
+		dx = (X_b - X_a)/n;
+
+		for (int i = 0; i < n; i++) 
+		{
+			x = X_a + (i + 0/5) * dx;
+			result2 += func(x) * dx;
+		}
+
+		n = n * 2;
+	}
+
+	return result2;
 }
 
