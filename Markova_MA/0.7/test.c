e903d3d7a70e2d5de1756c983aa5e51f019ca938
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-04-04 15:38:15 +0300

    Update test.c

--- Markova_MA/0.7/test.c
+++ Markova_MA/0.7/test.c
@@ -4,12 +4,13 @@
 #include <math.h>
 long double f (long double x)
 {
-	return x*x - 3*x - 4;
+	return 1/(x*x - 3*x - 4);
 }
 
 int g (long double x, long double y)
 {
-	if(x == y)
+	double eps = 0.01;
+	if((x - y) < eps && (- x + y) < eps)
 	{
 		return 0;
 	}
