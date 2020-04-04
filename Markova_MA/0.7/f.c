46b4a3caa2d3f8804a6fb51b4cbc2c4ed84c2ce0
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-04-04 14:48:15 +0300

    g

--- Markova_MA/0.7/f.c
+++ Markova_MA/0.7/f.c
@@ -7,10 +7,12 @@
 void a (double (*f)(double), int (*g)(double, double), double x)
 {
 	double ans = (*f)(x), check = 0;
+	printf("%e", ans);
 	while(((*g)(ans, (*f)(ans)) > 0) || ((*g)(ans, (*f)(ans)) < 0))
 	{
 		check++;
 		ans = (*f)(ans);
+		printf(", %e", ans);
 		if (check > T)
 		{
 			printf("error");
