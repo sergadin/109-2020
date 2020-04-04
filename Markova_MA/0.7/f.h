5896a0d7df1e1eb66d1c3f78731ebc5659b08169
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-04-04 15:19:52 +0300

    Update f.h

--- Markova_MA/0.7/f.h
+++ Markova_MA/0.7/f.h
@@ -1,5 +1,5 @@
 #define T 30
-double f (double x);
-int g (double x, double y);
+double f (long double x);
+int g (long double x, long double y);
 
 void a (long double (*f)(long double), int (*g)(long double, long double), double x);
