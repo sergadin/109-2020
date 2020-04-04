264cde3e3d4cb10c28f4a0773fdb628682d1bcf8
kolorono <kolorono@yandex.ru>
2020-04-04 18:59:14 +0300

    Eбрал лишнее

--- Krasilnikov_NA/4.log/Taylor.h
+++ Krasilnikov_NA/4.log/Taylor.h
@@ -1,5 +1,4 @@
-typedef double(*RRFUN)(double x);
 typedef enum {LOG_OK=0, LOG_NEOK} ErrorCode;
 
-double series(double x, double epsilon, RRFUN function, ErrorCode *error);
+double series(double x, double epsilon, ErrorCode *error);
  
