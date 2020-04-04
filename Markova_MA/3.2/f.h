95b271d8498b6ebcc7e2f0712a4ad8bffb88dc3d
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-04-04 13:00:40 +0300

    e

--- Markova_MA/3.2/f.h
+++ Markova_MA/3.2/f.h
@@ -3,5 +3,7 @@ double ab (double m);
 typedef enum { ER = 1, OK = 0 } ErrorCode;
 
 double gold(double (*f)(double), double a, double b, double eps, ErrorCode *perr);
-double f(double x);
-
+double f1 (double x);
+double f2 (double x);
+double f3 (double x);
+double f4 (double x);
--- Markova_MA/3.2/test.c
+++ Markova_MA/3.2/test.c
@@ -5,21 +5,84 @@
 
 #include "f.h"
 
-double f(double x){
-    return x*x*x;
+double f1 (double x)
+{
+	return x*sin(x);
 }
-
+double f2 (double x)
+{
+	return x;
+}
+double f3 (double x)
+{
+	return x*x*x;
+}
+double f4 (double x)
+{
+	return (x - 5)*(x - 6)*(x - 7);
+}
 
 int main(void){
     double a = -1, b = 8, eps = 0.001, x;
     ErrorCode err = OK;
-    x = gold(f, a, b, eps, &err);
-	printf("Точка минимума: %e\n", x);
-	if(err)
+    m = gold(f1, a, b, &err, eps);
+	
+	printf("f1 ");
+	switch(err)
 	{
-		printf("error");
+	case INT_NOT:
+        printf("FAULT\n");
+		break;
+	case INT_ND:
+		printf("NO DATA\n");
+		break;
+	case INT_OK:
+		printf("Answer = %e\n", m);
 	}
-	else{
-		printf("Точка минимума: %e\n", x);
+	
+
+	printf("f2 ");
+	m = gold(f2, a, b, &err, eps);
+	
+	switch(err)
+	{
+	case INT_NOT:
+        printf("FAULT\n");
+		break;
+	case INT_ND:
+		printf("NO DATA\n");
+		break;
+	case INT_OK:
+		printf("Answer = %e\n", m);
+	}
+	
+	printf("f3 ");
+	m = gold(f3, a, b, &err , eps);
+	
+	switch(err)
+	{
+	case INT_NOT:
+        printf("FAULT\n");
+		break;
+	case INT_ND:
+		printf("NO DATA\n");
+		break;
+	case INT_OK:
+		printf("Answer = %e\n", m);
+	}
+	
+	printf("f4 ");
+	m = gold(f4, a, b, &err, eps);
+	
+	switch(err)
+	{
+	case INT_NOT:
+        printf("FAULT\n");
+		break;
+	case INT_ND:
+		printf("NO DATA\n");
+		break;
+	case INT_OK:
+		printf("Answer = %e\n", m);
 	}
 }
