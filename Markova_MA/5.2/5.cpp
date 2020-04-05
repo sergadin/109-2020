135dde68f2d9edf066699d60577f13832e8e47f7
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-04-06 01:35:23 +0300

    Update 5.cpp

--- Markova_MA/5.2/5.cpp
+++ Markova_MA/5.2/5.cpp
@@ -9,7 +9,7 @@ int main()
 	try
 	{	
 		int n = 10; 
-		double x[5] = {1, 2, 2.4, 3, 4, 5.6, 7, 8, 9, 10};
+		double x[10] = {1, 2, 2.4, 3, 4, 5.6, 7, 8, 9, 10};
 		double x0 = 6;
 		double y0[2] = {0, 0};
 		double true_ans[2] = {216, 5.93989};
