587e816c54102f937b6a81ee199208f0f30827f2
Sergey Afonin <serg@msu.ru>
2020-04-06 00:32:00 +0300

    Move files from top-level directory into their proper location

--- solve.h
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"test.h"
-
-int solve(double a, double b, double eps, double*res, double(*func)(double));
--- test.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-//static int count=0;
-double func(double x);
-double func1(double x);
-double func2(double x);
-double func3(double x);
-int get_count(void);
