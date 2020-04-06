cbd25d49ea5bf155ddc5ff71acbc2112cb0e826b
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-04-06 15:56:54 +0300

    Update sum.c

--- BondarevNS/1.3/sum.c
+++ BondarevNS/1.3/sum.c
@@ -6,7 +6,8 @@ double findRoot(double a, double b, double epsilon, RRFUN f)
 {
 	while(fabs(b - a) > epsilon)
 	{
-	
+		a = b - (b - a) * (*f)(b) / ((*f)(b) - (*f)(a));
+		b = a + (a - b) * (*f)(a) / ((*f)(a) - (*f)(b));
 	}
 
 	return b;
