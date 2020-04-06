a0767c13505b1efd71d206757bb764fe9d4f19e7
KandaurovaVasilisa <magnit6520@gmail.com>
2020-04-06 11:54:07 +0300

    поправлена обработка ошибок к 1.3

--- Kandaurova_VB/1.3/chord.c
+++ Kandaurova_VB/1.3/chord.c
@@ -11,15 +11,17 @@ double mod(double x){
 double chord(double a, double b, RRfun fun, double eps, ErrorCode *perr){
     double x1 = a, x2 = b, x0, k = 0;
     *perr = I_OK;
-    if((a > b)||((b - a) < eps)) 
-        *perr = I_NO;
+    if((a > b)) 
+        *perr = I_NOSIGM;
+    if((b - a) < eps) 
+        *perr = I_SMALL;
     while(mod(x2 - x1) > eps) {
         x0 = x2 - (x2 - x1) / ((*fun)(x2) - (*fun)(x1)) * (*fun)(x2);
         x1 = x2;
         x2 = x0;
         k++;
         if(k > 10000) {
-            *perr = I_NO;
+            *perr = I_MUCH;
             break;        
         }
     }
