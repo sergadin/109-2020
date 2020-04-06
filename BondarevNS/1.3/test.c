6570989c5698330b9e162a4067de6c18f3a17238
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-04-07 01:13:27 +0300

    Update test.c

--- BondarevNS/1.3/test.c
+++ BondarevNS/1.3/test.c
@@ -7,7 +7,7 @@ double f3 (double x);
 
 double f1 (double x)
 {
-	return  x + 2;
+	return  x - 1;
 }
 
 double f2 (double x)
@@ -24,9 +24,9 @@ double f3 (double x)
 int main (void)
 {
 double res, epsilon = 0.001;
-double  otvet [] = {-2, 0, 0};
-double a = -3;
-double b = 1;
+double  otvet [] = {1, 0, 0};
+double a = -2;
+double b = 0;
 RRFUN f[] = {f1, f2, f3 };
                  
 	for(int i = 0; i < 3; i++)
