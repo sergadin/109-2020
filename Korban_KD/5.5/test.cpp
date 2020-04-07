2d880a3a84bf861044de3dbb437320c0508bf506
kir1200 <kir120067@gmail.com>
2020-04-07 01:37:36 +0000

    5.5 fixed

--- Korban_KD/5.5/test.cpp
+++ Korban_KD/5.5/test.cpp
@@ -45,7 +45,7 @@ int main()
         x[i] = left + i*h;
     }
     
-    cout << "test on linear functions" << "\n";
+    cout << "test on linear functions" << "\n\n";
     
     for (int i = 0; i < 3; i++)
     {
@@ -56,23 +56,23 @@ int main()
         }
         else
         {
-            cout << "resul: linear function " << data.get_k() << "x + " << data.get_b << "\n";
+            cout << "resul: linear function " << data.get_k() << "x + " << data.get_b() << "\n";
             cout << "actual linear function " << k[i] << "x + " << b[i] << "\n";
             cout << "resul: value in f("<< x0 <<")=" << data.get_value(x0) << "\n";
-            cout << "actual value in f("<< x0 <<")=" << foo[i](x0) << "\n";
+            cout << "actual value in f("<< x0 <<")=" << foo[i](x0) << "\n\n";
         }
     }
     
     cout << "test on some function (x^3)" << "\n";
     
-    res = data.aprox(foo[4], x, n);
+    res = data.aprox(f4, x, n);
         if(res < 0)
         {
             printf("determinat was too small unable to solve\n");
         }
         else
         {
-            cout << "resul: linear function "<<data.get_k() << "x + " << data.get_b << "\n";
+            cout << "resul: linear function "<<data.get_k() << "x + " << data.get_b() << "\n";
             cout << "resul: value in f("<< x0 <<")=" <<data.get_value(x0) << "\n";
         }
     return 0;
