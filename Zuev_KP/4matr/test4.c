3cc408a4fc474867b51a3715a0ee7da7d2bbd849
ki-zuev <kizuev02@gmail.com>
2020-04-18 23:35:19 +0300

    Решение задачи 4

--- Zuev_KP/4matr/test4.c
+++ Zuev_KP/4matr/test4.c
@@ -2,17 +2,21 @@
 #include <math.h>
 #include <stdlib.h>
 #include "clin.h"
+#define MAX(a, b) (((a) > (b))?(a):(b))
+#define MAX1(a, b, c) MAX(a, MAX(b, c))
+double modul(double x);
 
 int main(void)
 {
 	int i, j, n_rows = 3, n_cols = 4;
-	double current;
+	double current, e = 0.1;
 	double **mas = malloc(n_rows* sizeof(double));
 	double *x1 = malloc(n_rows* sizeof(double));
+	double b[] = {0, -1, 2};
 	FILE *input1;
 	int o[n_rows];
 	for ( i = 0; i < n_rows; i++ )
-			o[i] = i;
+		o[i] = i;
 	if((input1 = fopen("input1.txt", "r")) == NULL)
 	{
 		return -1;
@@ -41,6 +45,17 @@ int main(void)
 			}
 		}
 	}
+	for(i = 0; i < n_rows; i++)
+	{
+		if (modul(x1[i] - b[i]) < e*MAX1(x1[i], b[i], 1))
+		{
+			printf("пройден\n");
+		}
+		else
+		{
+			printf("не пройден\n");
+		}
+	}
 	for(int row = 0; row < n_rows; row ++)
 	{
 		free(mas[row]);
@@ -50,34 +65,10 @@ int main(void)
 	return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+double modul(double x)
+{
+	if (x < 0)
+		return -x;
+	else 
+		return x;
+}
