28439c3c1139efed53a108004f6d68b39ac8c126
ki-zuev <kizuev02@gmail.com>
2020-04-18 19:43:05 +0300

    Решение задачи 4

--- Zuev_KP/4matr/clin.c
+++ Zuev_KP/4matr/clin.c
@@ -2,6 +2,7 @@
 #include"stdlib.h"
 #include"math.h"
 #include"clin.h"
+static double modul(double x);
 
 double* clin(double **matrix2d, int n_rows, int n_cols)
 {
@@ -10,31 +11,41 @@ double* clin(double **matrix2d, int n_rows, int n_cols)
 	n = n_rows;
 	for ( k = 0; k < n; k++ )
 	{
-		if ( fabs( matrix2d[k] [k] ) < 0.0001 )
-			{
-				printf( "Система не имеет единственного решения" );
-				return ( 0 );
-			}
+		if (modul(matrix2d[k] [k]) < 0.0001 )
+		{
+			printf( "infinity" );
+			return 0;
+		}
 		for ( j = n; j >= k; j-- )
-			matrix2d[k] [j] /= matrix2d[k] [k];
-		for ( i = k + 1; i < n; i++ )
-			for ( j = n; j >= k; j-- )
-				matrix2d[i] [j] -= matrix2d[k] [j] * matrix2d[i] [k];
-
+			matrix2d[k] [j] = matrix2d[k] [j] / matrix2d[k] [k];
+		for ( i = k+1; i < n; i++ )
+		{
+			for ( j = n; j >= k; j--)
+			{
+			matrix2d[i] [j] = matrix2d[i] [j]-matrix2d[k] [j] * matrix2d[i] [k];
+			}
+		}	
 	}
-	for ( i = 0; i < n; i++ )
-{
-printf( "%d\n", i );
-		x[i] = matrix2d[i] [n];
-					
-}
+	for ( i = 1; i < n; i++)
+	{
+		x[i] = matrix2d[i][n];
+	}
+	x[0] = matrix2d[0][n];
 	for ( i = n - 2; i >= 0; i-- )
+	{	
 		for ( j = i + 1; j < n; j++ )
-{
-			x[i] -= x[j] * matrix2d[i] [j];
+		{
+			x[i] = x[i]-x[j] * matrix2d[i] [j];
 
-}
+		}
+	}
 	return x;
 }
 
-
+static double modul(double x)
+{
+	if (x < 0)
+		return -x;
+	else 
+		return x;
+}
--- Zuev_KP/4matr/test4.c
+++ Zuev_KP/4matr/test4.c
@@ -8,14 +8,14 @@ int main(void)
 	int i, j, n_rows = 3, n_cols = 4;
 	double current;
 	double **matrix2d = malloc(n_rows* sizeof(double));
-	double *x;
-	FILE *input;
-	int otv[n_rows];
+	double *x1;
+	FILE *input1;
+	int o[n_rows];
 	for ( i = 0; i < n_cols; i++ )
-{
-		otv[i] = i;
-}
-	if((input = fopen("input.txt", "r")) == NULL)
+	{
+		o[i] = i;
+	}
+	if((input1 = fopen("input1.txt", "r")) == NULL)
 	{
 		return -1;
 	}
@@ -27,19 +27,22 @@ int main(void)
 	{
 		for(int j = 0; j < n_cols; j ++)
 		{
-			fscanf(input, "%lf", &current);
+			fscanf(input1, "%lf", &current);
 			matrix2d[i][j] = current;
 		}
 	}
-	x = clin(matrix2d, n_rows, n_cols);
-	printf( "Ответ:\n" );
+	x1 = clin(matrix2d, n_rows, n_cols);
 	for ( i = 0; i < n_rows; i++ )
+	{
 		for ( j = 0; j < n_rows; j++ )
-			if ( i == otv[j] )
+		{
+			if ( i == o[j] )
 			{
-				printf( "%lf\n", x[j] );
+				printf( "%lf\n", x1[j] );
 				break;
 			}
+		}
+	}
 	for(int row = 0; row < n_rows; row ++)
 	{
 		free(matrix2d[row]);
