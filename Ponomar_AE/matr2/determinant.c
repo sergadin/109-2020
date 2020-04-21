e4cf8f64d6c1c697e8cae06b88a731a119a61a8e
ponomar4643 <kkkitya@yandex.ru>
2020-04-21 18:29:33 +0300

    Решение задачи 2 (матрицы)

--- Ponomar_AE/matr2/determinant.c
+++ Ponomar_AE/matr2/determinant.c
@@ -6,12 +6,12 @@
 double determinant_matrix(double *matrix, int n, double E) 
 {
 	double factor, change, determinant = 1, sign = 1;
-	int string, i, j = 0, a1 = 0, a2, a3;
+	int string;
 
-	for (j = 0; j < n; j++) 
+	for (int j = 0; j < n; j++) 
 	{
 		string = j;
-		for (i = j + 1; i < n; i++) 
+		for (int i = j + 1; i < n; i++) 
 		{
 			if (fabs(matrix(n, i, j)) > fabs(matrix(n, string, j))) 
 			{
@@ -21,22 +21,22 @@ double determinant_matrix(double *matrix, int n, double E)
 
 		if (string != j) 
 		{
-			for (a1 = 0; a1 < n; a1++) 
+			for (int a = 0; a < n; a++) 
 			{
-				change = (matrix(n, j, a1));
-				matrix(n, j, a1) = matrix(n, string, a1);
-				matrix(n, string, a1) = change;
+				change = (matrix(n, j, a));
+				matrix(n, j, a) = matrix(n, string, a);
+				matrix(n, string, a) = change;
 			}
 
 			sign *= -1;
 		}
 
-		for (a2 = j + 1; a2 < n; a2++) 
+		for (int a = j + 1; a < n; a++) 
 		{
-			factor = matrix(n, a2, j)/matrix(n, j, j);
-			for (a3 = j; a3 < n; a3++) 
+			factor = matrix(n, a, j)/matrix(n, j, j);
+			for (int b = j; b < n; b++) 
 			{
-				matrix(n, a2, a3) -= factor*matrix(n, j, a3);
+				matrix(n, a, b) -= factor*matrix(n, j, b);
 			}
 		}
 
