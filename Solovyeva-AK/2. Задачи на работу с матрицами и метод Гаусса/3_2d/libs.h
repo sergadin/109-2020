228900aa3e46c8b07176a77ee7bccfafd3c6b2a8
Solovyeva-AK <adknm@yandex.ru>
2020-04-21 18:53:14 +0300

    3

--- Solovyeva-AK/2. Задачи на работу с матрицами и метод Гаусса/3_2d/libs.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-double deter(int n, double **matrix_now);
-void reverse(int n, double **matrix_now, double **new);
--- Solovyeva-AK/2. Задачи на работу с матрицами и метод Гаусса/4_1d/libs.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-void gauss_m(int n, double *matrix_now, double *x);
