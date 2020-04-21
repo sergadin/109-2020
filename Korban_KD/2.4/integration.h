0495a23fea286bb7ec66f82a21d103d5b1fb935e
kir1200 <kir120067@gmail.com>
2020-04-21 23:15:21 +0000

    fixed .h

--- Korban_KD/2.4/integration.h
+++ Korban_KD/2.4/integration.h
@@ -1,7 +1,5 @@
 typedef double (*dndFUNC)(double x);
-#define NUMBER 4
-#define STEP 2
-#define MAX_IT 1e10
+
 
 double part_sum(dndFUNC f, double a, double b); 
 double gaus(dndFUNC f, double a, double b, int n);
