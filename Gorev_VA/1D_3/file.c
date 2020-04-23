3412c1f76dba91f1ddb5f5fae21326876c9cd315
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-04-23 23:31:22 +0300

    Add files via upload

--- Gorev_VA/1D_3/file.c
+++ Gorev_VA/1D_3/file.c
@@ -26,5 +26,76 @@ double *read_matrix(int m, int n, FILE* input)
 
 int main(void)
 {
+	double *matr;
+	int N = 3;
+	ErrorCode *Error;
+	FILE *input;
+	Error = (ErrorCode*)malloc(sizeof(ErrorCode));
+
+
+	if ((input = fopen("input1.txt", "r")) == NULL)
+	{
+		return -1;
+	}
+	matr = read_matrix(N, N, input);
+	printf("A = \n");
+	print_matrix(matr, N, N);
+	printf("\n");
+	inverse(matr, N, Error);
+	if (*Error == OK)
+	{
+		printf("A^-1 = \n");
+		print_matrix(matr, N, N);
+		printf("\n");
+	}
+	if (*Error == MATR_IS_SINGULAR)
+		printf("Matrix is singular\n\n");
+	fclose(input);
+	free(matr);
+
+
+	if ((input = fopen("input2.txt", "r")) == NULL)
+	{
+		return -1;
+	}
+	matr = read_matrix(N, N, input);
+	printf("A = \n");
+	print_matrix(matr, N, N);
+	printf("\n");
+	inverse(matr, N, Error);
+	if (*Error == OK)
+	{
+		printf("A^-1 = \n");
+		print_matrix(matr, N, N);
+		printf("\n");
+	}
+	if (*Error == MATR_IS_SINGULAR)
+		printf("Matrix is singular\n\n");
+	fclose(input);
+	free(matr);
+
+
+	if ((input = fopen("input3.txt", "r")) == NULL)
+	{
+		return -1;
+	}
+	matr = read_matrix(N, N, input);
+	printf("A = \n");
+	print_matrix(matr, N, N);
+	printf("\n");
+	inverse(matr, N, Error);
+	if (*Error == OK)
+	{
+		printf("A^-1 = \n");
+		print_matrix(matr, N, N);
+		printf("\n");
+	}
+	if (*Error == MATR_IS_SINGULAR)
+		printf("Matrix is singular\n\n");
+	fclose(input);
+	free(matr);
+
+
+	free(Error);
 	return 0;
 }
