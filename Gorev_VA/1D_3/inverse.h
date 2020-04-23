8d4169c5c7b85caf7a2587e6546f50bb1aabc3c3
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-04-23 22:48:59 +0300

    Add files via upload

--- Gorev_VA/1D_3/inverse.h
+++ Gorev_VA/1D_3/inverse.h
@@ -9,88 +9,54 @@ double Abs(double x)
 	return -x;
 }
 
-double *centr_sym(double *matr, int n);
-double *centr_sym(double *matr, int n)
+void centr_sym(double *matr, int n);
+void centr_sym(double *matr, int n)
 {
 	double *T;
 	T = (double*)malloc(n * n * sizeof(double));
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			T[i * n + j] = matr[(n - 1 - i) * n + (n - 1 - j)];
-	return T;
+			T[i * n + j] = matr[i * n + j];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			matr[i * n + j] = T[(n - 1 - i) * n + (n - 1 - j)];
+	free(T);
 }
 
-double *swap(double *matr, int m, int n, int i1, int i2);
-double *swap(double *matr, int m, int n, int i1, int i2)
+void swap(double *matr, int n, int i1, int i2);
+void swap(double *matr, int n, int i1, int i2)
 {
 	double *A;
-	if (i1 == i2)
-		return matr;
-	if (i1 > i2)
+	A = (double*)malloc(sizeof(double));
+	for (int j = 0; j < n; j++)
 	{
-		A = (double*)malloc(sizeof(double));
-		A[0] = i1;
-		i1 = i2;
-		i2 = A[0];
-		free(A);
+		A[0] = matr[i1 * n + j];
+		matr[i1 * n + j] = matr[i2 * n + j];
+		matr[i2 * n + j] = A[0];
 	}
-	A = (double*)malloc(m * n * sizeof(double));
-
-	for (int i = 0; i < i1; i++)
-		for (int j = 0; j < n; j++)
-			A[i * n + j] = matr[i * n + j];
-	for (int j = 0; j < n; j++)
-		A[i1 * n + j] = matr[i2 * n + j];
-	for (int i = i1 + 1; i < i2; i++)
-		for (int j = 0; j < n; j++)
-			A[i * n + j] = matr[i * n + j];
-	for (int j = 0; j < n; j++)
-		A[i2 * n + j] = matr[i1 * n + j];
-	for (int i = i2 + 1; i < m; i++)
-		for (int j = 0; j < n; j++)
-			A[i * n + j] = matr[i * n + j];
-
-	return A;
+	free(A);
 }
 
-double *multiply(double *matr, int m, int n, int i1, double c);
-double *multiply(double *matr, int m, int n, int i1, double c)
+void multiply(double *matr, int n, int i1, double c);
+void multiply(double *matr, int n, int i1, double c)
 {
-	double *A;
-	A = (double*)malloc(m * n * sizeof(double));
-	for (int i = 0; i < i1; i++)
-		for (int j = 0; j < n; j++)
-			A[i * n + j] = matr[i * n + j];
 	for (int j = 0; j < n; j++)
-		A[i1 * n + j] = c * matr[i1 * n + j];
-	for (int i = i1 + 1; i < m; i++)
-		for (int j = 0; j < n; j++)
-			A[i * n + j] = matr[i * n + j];
-	return A;
+		matr[i1 * n + j] = c * matr[i1 * n + j];
 }
 
-double *plus_str(double *matr, int m, int n, int i1, int i2, double c);
-double *plus_str(double *matr, int m, int n, int i1, int i2, double c)
+void plus_str(double *matr, int n, int i1, int i2, double c);
+void plus_str(double *matr, int n, int i1, int i2, double c)
 {
-	double *A;
-	A = (double*)malloc(m * n * sizeof(double));
-	for (int i = 0; i < i1; i++)
-		for (int j = 0; j < n; j++)
-			A[i * n + j] = matr[i * n + j];
 	for (int j = 0; j < n; j++)
-		A[i1 * n + j] = matr[i1 * n + j] + c * matr[i2 * n + j];
-	for (int i = i1 + 1; i < m; i++)
-		for (int j = 0; j < n; j++)
-			A[i * n + j] = matr[i * n + j];
-	return A;
+		matr[i1 * n + j] = matr[i1 * n + j] + c * matr[i2 * n + j];
 }
 
 void diag(double *A, double *A_dop, int n, ErrorCode *Error);
 void diag(double *A, double *A_dop, int n, ErrorCode *Error)
 {
 	*Error = OK;
-	double *B, *B_dop;
-	B = A, B_dop = A_dop;
+	/*double *B, *B_dop;
+	B = A, B_dop = A_dop;*/
 	for (int k = 0; k < (n - 1); k++)
 	{
 		*Error = MATR_IS_SINGULAR;
@@ -99,11 +65,11 @@ void diag(double *A, double *A_dop, int n, ErrorCode *Error)
 			if (Abs(A[i * n + k]) >= eps)
 			{
 				*Error = OK;
-				A_dop = swap(A_dop, n, n, k, i);
-				A = swap(A, n, n, k, i);
+				swap(A_dop, n, k, i);
+				swap(A, n, k, i);
 
-				A_dop = multiply(A_dop, n, n, k, 1.0 / A[k * n + k]);
-				A = multiply(A, n, n, k, 1.0 / A[k * n + k]);
+				multiply(A_dop, n, k, 1.0 / A[k * n + k]);
+				multiply(A, n, k, 1.0 / A[k * n + k]);
 				break;
 			}
 		}
@@ -112,8 +78,8 @@ void diag(double *A, double *A_dop, int n, ErrorCode *Error)
 			for (int i = k + 1; i < n; i++)
 				for (int j = k; j < n; j++)
 				{
-					A_dop = plus_str(A_dop, n, n, i, k, -A[i * n + k] / A[k * n + k]);
-					A = plus_str(A, n, n, i, k, -A[i * n + k] / A[k * n + k]);
+					plus_str(A_dop, n, i, k, -A[i * n + k] / A[k * n + k]);
+					plus_str(A, n, i, k, -A[i * n + k] / A[k * n + k]);
 				}
 		}
 		else
@@ -122,16 +88,18 @@ void diag(double *A, double *A_dop, int n, ErrorCode *Error)
 	for (int k = 0; k < n; k++)
 		if (Abs(A[k * n + k]) < eps)
 			*Error = MATR_IS_SINGULAR;
-	for (int i = 0; i < n; i++)
+	/*for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 		{
 			B_dop[i * n + j] = A_dop[i * n + j];
 			B[i * n + j] = A[i * n + j];
 		}
+	free(A);
+	free(A_dop);*/
 }
 
-double *inverse(double *A, int n, ErrorCode *Error);
-double *inverse(double *A, int n, ErrorCode *Error)
+void inverse(double *A, int n, ErrorCode *Error);
+void inverse(double *A, int n, ErrorCode *Error)
 {
 	double *A_dop;
 	A_dop = (double*)malloc(n * n * sizeof(double));
@@ -143,10 +111,15 @@ double *inverse(double *A, int n, ErrorCode *Error)
 				A_dop[i * n + j] = 0;
 
 	diag(A, A_dop, n, Error);
-	if (*Error == MATR_IS_SINGULAR)
-		return A_dop;
-	A = centr_sym(A, n);
-	A_dop = centr_sym(A_dop, n);
-	diag(A, A_dop, n, Error);
-	return centr_sym(A_dop, n);
+	if (*Error != MATR_IS_SINGULAR)
+	{
+		centr_sym(A, n);
+		centr_sym(A_dop, n);
+		diag(A, A_dop, n, Error);
+		centr_sym(A_dop, n);
+	}
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			A[i * n + j] = A_dop[i * n + j];
+	free(A_dop);
 }
