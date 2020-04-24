baa6d1fdfd5df20e50bcf21d60607f6c6e9f6771
Александр <alexandr30032014@mail.ru>
2020-04-24 18:11:59 +0300

    Переименование переменных

--- Dolgikh_AA/Mat1/task.c
+++ Dolgikh_AA/Mat1/task.c
@@ -4,27 +4,26 @@
 
 int rank(double *data, int n_rows, int n_cols)
 {
-	int k = 0, fail = 0, i = 0, j = 0;
+	int curr_row = 0, non_zero_not_found = 0, i = 0, j = 0;
 	double max = 0;
 	int maxrow, maxcol;
 
-	while(!fail && k < n_rows)
+	while(!non_zero_not_found && curr_row < n_rows)
 	{
 		printmat(data, n_rows, n_cols);
 		fprintf(stdout,"\n");
 		
-		k++;
-		fail = 1;
+		curr_row++;
+		non_zero_not_found = 1;
 		max = 0;
 		//Ищем максимальный по модулю элемент
 		for(i = 0; i < n_cols; i++)
 		{
-			for(j = k-1; j < n_rows; j++)
+			for(j = curr_row-1; j < n_rows; j++)
 			{
-				if(!(fabs(EL(data, n_cols, j, i)) < eps))
-					fail = 0;
 				if(fabs(EL(data, n_cols, j, i)) > fabs(max))
 				{
+					non_zero_not_found = 0;
 					max = EL(data, n_cols, j, i);
 					maxrow = j;
 					maxcol = i;
@@ -32,26 +31,26 @@ int rank(double *data, int n_rows, int n_cols)
 			}
 		}
 
-		if(fail)
+		if(non_zero_not_found)
 		{
-			k--;
+			curr_row--;
 		}
 		else
 		{
-			swaprows(data, n_cols, k, maxrow);
-			swapcols(data, n_rows, n_cols, k, maxcol);
+			swaprows(data, n_cols, curr_row, maxrow);
+			swapcols(data, n_rows, n_cols, curr_row, maxcol);
 			//Обнуляем все нижние элементы столбца
-			for(j = k; j < n_rows; j++)
+			for(j = curr_row; j < n_rows; j++)
 			{
-				if(!(fabs(EL(data, n_cols, j, k-1)) < eps))
+				if(!(fabs(EL(data, n_cols, j, curr_row-1)) < eps))
 				{
-					plusrows(data, n_cols, j, k-1, (-EL(data, n_cols, j, k-1)) / EL(data, n_cols, k-1, k-1));
+					plusrows(data, n_cols, j, curr_row-1, (-EL(data, n_cols, j, curr_row-1)) / EL(data, n_cols, curr_row-1, curr_row-1));
 				}
 			}
 		}
 	}
 
-	return k;
+	return curr_row;
 }
 
 void swaprows(double *data, int n_cols, int row1, int row2)
