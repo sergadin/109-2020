a5010cc7fd6ed48f1f040ee8f449043ade17728b
Solovyeva-AK <adknm@yandex.ru>
2020-04-26 20:16:30 +0300

    8

---
+++ Solovyeva-AK/3. Обработка текстового файла/8/string8.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+char *read_string(FILE *in) {
+    char buf[PATH_MAX];
+    buf[0] = 0;
+    char *s = fgets(buf, PATH_MAX, in);
+    if(s) {
+        int len = strlen(s);
+        char *res = malloc(len + 1);
+        strcpy(res, s);
+        return res;
+    }
+    return NULL;
+}
+
+
+int main(int argc, char *argv[]){
+    FILE *inp = fopen("input.txt", "r");
+    FILE *out = fopen("out.txt", "w");
+    char *start, *end;
+    if (argc == 1) {
+        start = malloc(2);
+        end = malloc(2);
+        start[0] = '"';
+        start[1] = '\0';
+        end[0] = '"';
+        end[1] = '\0';
+        printf("start quote: %s\nend quote: %s\n", start, end);
+        printf("if you need other - enter %cquotes%c in command line arguments\n", '"', '"');
+        
+    } else if (argc == 2) {
+        start = argv[1];
+        end = argv[1];
+    } else if (argc == 3) {
+        start = argv[1];
+        end = argv[2];
+    } else {
+        printf("Must be no more than 3 arguments\n");
+        return 0;
+    }
+    char *begin_quotes, *end_quotes, *str, tmp;
+    while(str = read_string(inp)){
+        begin_quotes = strstr(str, start);
+        if (begin_quotes){
+            char k = begin_quotes[0];
+            begin_quotes[0] = '\0';
+            fprintf(out, "%s", str);
+            begin_quotes[0] = k;
+            begin_quotes++;
+            end_quotes = strstr(begin_quotes, end);
+            if(end_quotes) {
+                end_quotes += strlen(end);
+                fprintf(out, "%s", end_quotes);
+            } else {
+                begin_quotes--;
+                fprintf(out, "%s", begin_quotes);
+            }
+        } else {
+            fprintf(out, "%s", str);
+        }
+        free(str);
+    }
+    if (argc == 1) {
+        free(start);
+        free(end);
+    }
+    
+    fclose(inp);
+    fclose(out);
+    return 0;
+}
