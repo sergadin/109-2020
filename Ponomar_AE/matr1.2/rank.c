a4d53969c00ff19ae11999011b470f3c1762abdd
ponomar4643 <kkkitya@yandex.ru>
2020-04-28 00:54:08 +0300

    Решение задачи 1 (матрицы)

--- Ponomar_AE/matr1.2/rank.c
+++ Ponomar_AE/matr1.2/rank.c
@@ -16,8 +16,8 @@ int rank_matrix(double **matrix, int n, int m, double E)
 
 	while((presence_nonzero == 1) && (curr_string < min(n, m)))
 	{
-                presence_nonzero = 0;
-                max = 0;
+		presence_nonzero = 0;
+		max = 0;
 		for(int i = curr_string; i < n; i++)
 		{
 			for(int j = curr_string; j < m; j++)
@@ -34,37 +34,36 @@ int rank_matrix(double **matrix, int n, int m, double E)
 
 		if (presence_nonzero == 1)
 		{
-	                for(int j = curr_string; j < m; j++)
-	                {
-		                double change = matrix[curr_string][j];
-		                matrix[curr_string][j] = matrix[max_string][j];
-		                matrix[max_string][j] = change;
-	                }
-                        for(int i = curr_string; i < n; i++)
-	                {
-		                double change = matrix[i][curr_string];
-		                matrix[i][curr_string] = matrix[i][max_column];
-		                matrix[i][max_column] = change;
-	                }
+
+			double *change = matrix[curr_string];
+			matrix[curr_string] = matrix[max_string];
+			matrix[max_string] = change;
+
+			for(int i = curr_string; i < n; i++)
+			{
+				double change = matrix[i][curr_string];
+				matrix[i][curr_string] = matrix[i][max_column];
+				matrix[i][max_column] = change;
+			}
 
 			for(int i = curr_string + 1; i < n; i++)
 			{
 				if(fabs(matrix[curr_string][curr_string]) > E)
 				{
-                                        double factor = matrix[i][curr_string] / matrix[curr_string][curr_string];
-                                     	for(int j = curr_string; j < m; j++)
-	                                {
-                                                matrix[i][j] -= matrix[curr_string][j] * factor;
-                                                                      
-                                        }
+					double factor = matrix[i][curr_string] / matrix[curr_string][curr_string];
+					for(int j = curr_string; j < m; j++)
+					{
+						matrix[i][j] -= matrix[curr_string][j] * factor;
+
+					}
 				}
 			}
-                        
+
 			curr_string++;
 		}
 	}
 
-        return curr_string;
+	return curr_string;
 }
 
 
