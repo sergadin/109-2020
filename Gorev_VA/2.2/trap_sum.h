0598ad9b58e6ba147e7dc9a5ec3958dafbcde444
gorevvadim <gorev.vadim.al1234567890sobaka@gmail.com>
2020-04-28 12:23:21 +0300

    Переименовано дофига всего

--- Gorev_VA/2.2/trap_sum.h
+++
@@ -1,5 +0,0 @@
-#define eps 0.000001
-#define pi 3.141592653589793238462643
-
-double trap_sum(double (*f) (double), double a, double b, int N);
-double integral(double (*f) (double), double a, double b, int* Error);
--- Gorev_VA/3.2/min_value.h
+++
@@ -1,3 +0,0 @@
-#define eps 0.000001
-
-double min_value(double(*f) (double), double a, double b, int* Error);
