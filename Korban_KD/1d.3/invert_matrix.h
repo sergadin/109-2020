6e7603729146c9e2e2f85ebb815ad29e58a67a24
kir1200 <kir120067@gmail.com>
2020-04-28 00:39:31 +0000

    added 1d.3

---
+++ Korban_KD/1d.3/invert_matrix.h
@@ -0,0 +1,9 @@
+int invert_matrix (int n, double * a, double *b);
+double find_max_abs_redused(int n, double *a, int start,int *max_i,int *max_j);
+ 
+enum INVERT_MATRIX
+{
+    DETERMINANT_0 = 0,
+    OK,
+};
+ 
---
+++ Korban_KD/1d.3/main.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "matr.h"
+#include "invert_matrix.h"
+#include "../lib/exmath.h"
+
+enum RETURN_CODES
+{
+    INPUT_ERROR = -3,
+    MEMORY_ERROR,
+    READ_ERROR,
+};
+
+int main(int argc, char **argv)
+{
+    double *a, *b, *c;
+    int n, m, k, res;
+    char *name = 0;
+    clock_t time_start;
+    
+    
+    if( !(argc==3 || argc==4) || (n = atoi(argv[1]))<=0 || (m = atoi(argv[2]))<=0 )
+    {
+        fprintf(stderr, "Usage: %s (matrix n x n) [n] [m] [file] (test mode w/o file ) (m is number of printed rows)\n", argv[0]);
+        return INPUT_ERROR;
+    }
+
+    if( argc==4 )
+    {
+        name = argv[3];
+    }
+
+    if( !(a = malloc(n*n*sizeof(double))) )
+    {
+        fprintf(stderr, "Memory ERROR!\n");
+        return MEMORY_ERROR;
+    }
+    
+    if( !(b = malloc(n*n*sizeof(double))) )
+    {
+        fprintf(stderr, "Memory ERROR!\n");
+        return MEMORY_ERROR;
+    }
+    
+    if( !(c = malloc(n*n*sizeof(double))) )
+    {
+        fprintf(stderr, "Memory ERROR!\n");
+        return MEMORY_ERROR;
+    }
+
+    if( name )
+    {
+        int res_read;
+
+        res_read = read_matrix(a, n, n, name);
+
+        if( res_read!=MATR_SUCCESS )
+        {
+            switch( res_read )
+            {
+                case MATR_ERR_OPEN:
+                    fprintf(stderr, "ERROR can't open %s\n", name);
+                    break;
+                case MATR_ERR_READ:
+                    fprintf(stderr, "ERROR can't read element from %s\n", name);
+                    break;
+                default:
+                    fprintf(stderr, "Unknown ERROR code: %d\n", res_read);
+            }
+
+            free(a);
+            return READ_ERROR;
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {                
+                if(i==j)
+                    b[i*n + j] = 1;
+                else
+                    b[i*n + j] = 0;
+            }
+        }
+        
+        printf("ROWS AND COLUMS ARE COUNTED FORM 0\n\n");
+        printf("matrix\n");
+        print_matrix(a, n, n);
+
+        time_start = clock();
+    
+        res = invert_matrix(n, a, b);
+    
+        printf("Elapsed %.2lf\n", (double)(clock() - time_start)/CLOCKS_PER_SEC);
+        
+        if(res !=OK)
+        {
+            printf("determinant is 0\n");
+            free(a);
+            free(b);
+            free(c);
+            return 0;
+        }
+        printf("inverted matrix\n");
+        print_matrix(b, n, n);
+        
+        printf("after multiplication\n");
+        read_matrix(a, n, n, name);
+        mult_matrix(a, b, c, n, n, n);
+        print_matrix(c, n, n);
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {                
+                if(i==j)
+                    c[i*n + j] -= 1;
+            }
+        }
+        
+        printf("norm of error: %e\n", norm_matrix(c, n)/norm_matrix(a, n));
+        
+        
+    } 
+    else
+    {
+        for(k = 1; k <= 4; k++)
+        {
+            
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {                
+                    if(i==j)
+                        b[i*n + j] = 1;
+                    else
+                        b[i*n + j] = 0;
+                }
+            }
+            printf("ROWS AND COLUMS ARE COUNTED FORM 0\nk==%d\n", k);
+            init_matrix(a, n, k);
+            printf("matrix\n");
+            printf("after multiplication\n");
+            print_matrix(a, n, m);
+
+            time_start = clock();
+    
+            res = invert_matrix(n, a, b);
+    
+            printf("Elapsed %.2lf\n", (double)(clock() - time_start)/CLOCKS_PER_SEC);
+            
+            if(res !=OK)
+            {
+                printf("determinant is 0\n");
+                free(a);
+                free(b);
+                free(c);
+                return 0;
+            }
+            printf("inverted matrix\n");
+            print_matrix(b, n, n);
+            
+            printf("after multiplication\n");
+            init_matrix(a, n, k);
+            mult_matrix(a, b, c, n, n, n);
+            print_matrix(c, n, n);
+            
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {                
+                    if(i==j)
+                        c[i*n + j] -= 1;
+                }
+            }
+        
+            printf("norm of error: %e\n", norm_matrix(c, n)/norm_matrix(a, n));
+        
+            printf("/////////////////////////////////////////////////////////\n");
+
+        }
+        
+    }
+    
+    free(a);
+    free(b);
+    free(c);
+    
+    return 0;
+} 
