9a8fd197f12a46652ae96558cabeb7160017924f
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-04-29 17:30:26 +0300

    s

--- BondarevNS/1.3/test.c
+++ BondarevNS/1.3/test.c
@@ -7,17 +7,17 @@ double f3 (double x);
 
 double f1 (double x)
 {
-	return  x*x*x +x*x - x;
+	return  x - 1;
 }
 
 double f2 (double x)
 {
-        return x*x;
+        return x*x - 4;
 }
 
 double f3 (double x)
 {
-        return  -x*x+x;
+        return  2*x-5;
 }
 
 
@@ -25,16 +25,17 @@ int main (void)
 {
 double res, epsilon = 0.001;
 double  otvet [] = {1, 2, 2.5};
-double h = 0.0001;
-double x = 0.1;
+double a = 0;
+double b = 3;
 RRFUN f[] = {f1, f2, f3 };
                  
 	for(int i = 0; i < 3; i++)
 	{
-		res = findmin(h, x, epsilon, f[i]);
+		res = findRoot(a, b, epsilon, f[i]);
 		printf("correct:\n%lf\nmy:\n%lf+-%lf\n", otvet[i], res, epsilon);	
 	}
 }
+
 
 
 
--- BondarevNS/3.1/test.c
+++ BondarevNS/3.1/test.c
@@ -7,17 +7,17 @@ double f3 (double x);
 
 double f1 (double x)
 {
-	return  x - 1;
+	return  x*x*x + x*x - x;
 }
 
 double f2 (double x)
 {
-        return x*x - 4;
+        return x*x;
 }
 
 double f3 (double x)
 {
-        return  2*x-5;
+        return  -x*x + x;
 }
 
 
@@ -25,13 +25,13 @@ int main (void)
 {
 double res, epsilon = 0.001;
 double  otvet [] = {1, 2, 2.5};
-double a = 0;
-double b = 3;
+double h = 0.0001;
+double x = 0.1;
 RRFUN f[] = {f1, f2, f3 };
                  
 	for(int i = 0; i < 3; i++)
 	{
-		res = findRoot(a, b, epsilon, f[i]);
+		res = findmin(h, x, epsilon, f[i]);
 		printf("correct:\n%lf\nmy:\n%lf+-%lf\n", otvet[i], res, epsilon);	
 	}
 }
