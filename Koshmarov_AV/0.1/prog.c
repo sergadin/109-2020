e2be90fef8eee874498eb50aad4d01656561285e
koshmanton <63317462+koshmanton@users.noreply.github.com>
2020-04-30 15:58:24 +0300

    Добавлен массив значений переменной

--- Koshmarov_AV/0.1/prog.c
+++ Koshmarov_AV/0.1/prog.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "help.h"
 
 
-double result(pointer_on_func our_func, double point){
-    double znach;
-    znach = our_func(point);
-    return znach;
+double *result(pointer_on_func our_func, double *ans,  double *point, int n){
+    ans = (double*) malloc(sizeof(double) * n);
+    for (int i = 0; i < n; i++){
+        ans[i] = (*our_func)(point[i]);
+    }
+    return ans;
 }
