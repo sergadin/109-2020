06ddfdd05311b9844602e4f062dc86ab23c6407b
koshmanton <63317462+koshmanton@users.noreply.github.com>
2020-04-30 15:59:22 +0300

    Добавлен массив значений переменной

--- Koshmarov_AV/0.1/test.c
+++ Koshmarov_AV/0.1/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "help.h"
 
 #define eps 1e-7
@@ -26,20 +27,24 @@ int main(){
     pointer_on_func mas_func[al] = {zero, line, square, cube};
     double pointers[cp] = {1.15, 0, 15};
     double check_mac[cp][al] = {0, 2.15, 3.3225, 4.520875, 0, 1, 2, 3, 0, 16, 227, 3378};
+    double *po_mas = (double*) malloc(sizeof(double )*cp);
 
     for (int i = 0; i < al; i++){
-        for (int j = 0; j < cp; j++){
-            if (result(*mas_func[i], pointers[j]) - check_mac[j][i] <= eps)
-                printf("%d.Test completed\n", al*i + j);
+        result(*mas_func[i], po_mas, pointers, cp);
+        printf("Test for %d function:\n", i + 1);
+        for (int j = 0; j < cp; j ++) {
+            if(po_mas[j] - check_mac[j][i] <= eps)
+                printf("    %d.Test completed\n", j + 1);
             else {
-                printf("%d.Test not completed!\n", al*i + j);
+                printf("    %d.Test not completed!\n", j + 1);
                 printf("Check your program!\n");
                 return 0;
             }
         }
     }
 
+    printf("\n");
     printf("Your program is good!\n");
+    free(po_mas);
     return 0;
 }
-
