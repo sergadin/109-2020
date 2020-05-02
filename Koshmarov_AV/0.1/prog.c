ad7a55b584a8baef747a249aeddd0b1cc6f78563
koshmanton <63317462+koshmanton@users.noreply.github.com>
2020-05-03 00:01:06 +0300

    Убрал бессмысленное выделение памяти под ans.

--- Koshmarov_AV/0.1/prog.c
+++ Koshmarov_AV/0.1/prog.c
@@ -3,7 +3,7 @@
 
 
 double *result(pointer_on_func our_func, double *ans,  double *point, int n){
-    ans = (double*) malloc(sizeof(double) * n);
+    //ans = (double*) malloc(sizeof(double) * n);
     for (int i = 0; i < n; i++){
         ans[i] = (*our_func)(point[i]);
     }
