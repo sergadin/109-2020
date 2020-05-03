5a3f0d1054f960f15de52d772630db7df41baab1
ki-zuev <kizuev02@gmail.com>
2020-05-03 19:02:13 +0300

    Решение задачи 1

--- Zuev_KP/1stri/test1.c
+++ Zuev_KP/1stri/test1.c
@@ -2,28 +2,24 @@
 #include <math.h>
 #include <stdlib.h>
 #include "zamena.h"
-#define MAX(a, b) (((a) > (b))?(a):(b))
-#define MAX1(a, b, c) MAX(a, MAX(b, c))
-double modul(double x);
 
 int main(void)
 {
-	int i, j;
+	int i, j, n;
 	double current;
-	FILE *input1;
+	FILE *input1, *output;
 	if((input1 = fopen("input1.txt", "r")) == NULL)
 	{
 		return -1;
 	}
-
+	if((output = fopen("output.txt", "r")) == NULL)
+	{
+		return -1;
+	}
+	zamena(input1, output);
+	fclose(input1);
+	fclose(output);
 	return 0;
 }
 
 
-double modul(double x)
-{
-	if (x < 0)
-		return -x;
-	else 
-		return x;
-}
