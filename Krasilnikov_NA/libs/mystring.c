a52554d6fed4a6f9c93444bd873f60e6b4534d47
kolorono <kolorono@yandex.ru>
2020-05-03 23:14:29 +0300

    Работает, но не правильно

--- Krasilnikov_NA/libs/mystring.c
+++ Krasilnikov_NA/libs/mystring.c
@@ -6,10 +6,25 @@
 
 int slength(char *string)
 {
-  int length = 0;
-  while (string[length] != '\0')
-  {
-    length ++;
-  }
-  return length;
+	int length = 0;
+	while (string[length] != '\0')
+	{
+		length ++;
+	}
+	return length;
+}
+
+char *scopy(char *from, char *to)
+{
+	int length = slength(from);
+	if ((*to = (char*)realloc(to, length + 1) == NULL))
+	{
+		return NULL;
+	}
+	for (int i = 0; i < length; i++)
+	{
+		to[i] = from[i];
+	}
+	to[length] = '\0';
+	return to;
 }
