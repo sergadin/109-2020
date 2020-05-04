9968b5ddb6f6966343783621ddff11eb75a36cfd
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-05-04 14:09:50 +0300

    Update sum.c

--- BondarevNS/3.1/sum.c
+++ BondarevNS/3.1/sum.c
@@ -6,11 +6,11 @@ double Findmin(double h, double x, double epsilon, RRFUN f)
 {
 	if (x==0)
 	{ 
-		x = x + 0.1;
+		x = x + 0.001;
 	}
 	while (((f(x+h)-2*f(x)+f(x-h))/(h*h)) <= 0)
 	{ 
-		x = x + 0.1;
+		x = x + 0.001;
 	}
 	double x1;
         x1=x-0.5*h*(f(x+h)-f(x-h))/(f(x+h)-2*f(x)+f(x-h));
