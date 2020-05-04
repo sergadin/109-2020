fbf08a7ca27abc7758d06ec02228613318f30cc3
GlugovskayaYA <62711590+GlugovskayaYA@users.noreply.github.com>
2020-05-04 23:56:34 +0300

    Считывание строк любой длины

--- Glugovskaya_YA/String_7/f.c
+++ Glugovskaya_YA/String_7/f.c
@@ -1,14 +1,16 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "f.h"
 
 int Cut_Strings(const char *fileIn, const char *fileOut, int max_lenght)
 {
 	FILE *FileIn, *FileOut;
-	char buf[LEN];
-	int count = 0; 
-	int lenght; 
+	char *buf;
+	int *Count_El;
+	int count = 0, i, t, j; 
 	int Last_whitespace, Last_line_trans, FlagTBW = 0, Too_big_word = 0;
+	int count_strings, lenght;
 	
 	if(!(FileIn = fopen(fileIn,"r")))
 		return ERROR_OPEN_A;
@@ -16,44 +18,54 @@ int Cut_Strings(const char *fileIn, const char *fileOut, int max_lenght)
 	if(!(FileOut = fopen(fileOut,"w")))
 		return ERROR_OPEN_B ;
 	
+	count_strings = Count_Strings(fileIn);
 	
-	while (fgets(buf, LEN, FileIn))
+	Count_El = (int*) malloc (count_strings * sizeof(int));
+	Count_El_InString(fileIn, Count_El, count_strings);  
+	
+	for (t = 0; t < count_strings; t++)
 	{
+		buf = (char*) malloc (Count_El[t] * sizeof(char));
+		for (j = 0; j < Count_El[t]; j++)
+		{
+			fscanf(FileIn, "%c", &buf[j]); 
+		}
+
 		lenght = strlen(buf);
 		count = 0;
 		Last_whitespace = -1;
 		Last_line_trans = 0;
 		FlagTBW = 0;
-		for (int i = 0; buf[i]; i++)
+		
+		for (i = 0; i < Count_El[t]; i++)
 		{
 			count++;
-			//printf("count: %d i: %d %c\n", count, i, buf[i]);
+	
 			if (buf[i] == ' ')
-			{
 				Last_whitespace = i;
-				//printf("buf[%d]=%c\n", i, buf[i]);
-			}
 	
 			if (count > max_lenght && (Last_whitespace > -1 && Last_whitespace > Last_line_trans))
 			{
-				printf("Count %d:\n", count);
 				if (buf[i+1] != '\n') 
 					buf[Last_whitespace] = '\n';
 				count = i - Last_whitespace;
 				Last_line_trans = i;
 				FlagTBW = 0;
-				printf("Buf: %s\n", buf);
 			}
 			
 			if (count > max_lenght && Last_whitespace <= Last_line_trans && FlagTBW == 0)
 			{
-				//printf("i: %d \n", i);
 				Too_big_word +=1;
 				FlagTBW = 1;
 			}
 		}
-		fprintf(FileOut, "%s", buf);
+		
+		for (j = 0; j < Count_El[t]; j++)
+			fprintf(FileOut, "%c", buf[j]);
+		
+		free(buf);
 	}
+	while (fscanf(FileIn, "%c", &buf[0]) == 1);
 	
 	if (Too_big_word != 0)
 		printf ("There are %d words that are too large, please, choose a longer string length\n", Too_big_word);
@@ -67,6 +79,64 @@ int Cut_Strings(const char *fileIn, const char *fileOut, int max_lenght)
 	}
 	fclose(FileIn);
 	fclose(FileOut);
+	
+	return 0;
+}
+
+int Count_Strings (const char *fileIn)
+{
+	FILE *FileIn;
+	char buf;
+	int count_strings = 0;
+	
+	
+	if(!(FileIn = fopen(fileIn,"r")))
+		return ERROR_OPEN_A;
+	
+	while (fscanf(FileIn, "%c", &buf) == 1)
+		if (buf == '\n')
+			count_strings++;
+	
+	if(!feof(FileIn))
+	{
+		fclose(FileIn);
+		return ERROR_READ_A;
+	}
+	fclose(FileIn);
+	
+	return count_strings;
+}
+
+
+int Count_El_InString(const char *fileIn, int *Count_El, int count_strings)
+{
+	FILE *FileIn;
+	char buf;
+	int i, count = 0;
+	
+	if(!(FileIn = fopen(fileIn,"r")))
+		return ERROR_OPEN_A;
+	
+	for (i = 0; i < count_strings; i++)
+	{	
+		count = 1;
+		fscanf(FileIn, "%c", &buf);
+		
+		while (buf != '\n' && count < MAX_IT)
+		{
+			fscanf(FileIn, "%c", &buf);
+			count++;
+		}
+		Count_El[i] = count;
+	}
+	
+	
+	if(!feof(FileIn))
+	{
+		fclose(FileIn);
+		return ERROR_READ_A;
+	}
+	fclose(FileIn);
 	
 	return 0;
 }
--- Glugovskaya_YA/String_7/f.h
+++ Glugovskaya_YA/String_7/f.h
@@ -2,5 +2,8 @@
 #define ERROR_OPEN_A -1
 #define ERROR_OPEN_B -2
 #define ERROR_READ_A -3
+#define MAX_IT 1e15
 
 int Cut_Strings(const char *fileIn, const char *fileOut, int max_lenght);
+int Count_Strings (const char *fileIn);
+int Count_El_InString(const char *fileIn, int *Count_El, int count_strings);
