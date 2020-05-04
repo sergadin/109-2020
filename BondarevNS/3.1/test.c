8d8f6a122b493fe61e72ba98623577bd447a2a1d
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-05-04 13:27:00 +0300

    Update test.c

--- BondarevNS/3.1/test.c
+++ BondarevNS/3.1/test.c
@@ -23,17 +23,17 @@ int main(void)
 
 
 
-double f1(double x)
+double f1(double x);
 {
 	return  x*x*x + x*x - x;
 }
 
-double f2(double x)
+double f2(double x);
 {
         return x*x;
 }
 
-double f3(double x)
+double f3(double x);
 {
         return  -x*x + x;
 }
