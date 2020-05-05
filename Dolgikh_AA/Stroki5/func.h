f1702554243d196d547acc476d8176f01a0d2ea6
Александр <alexandr30032014@mail.ru>
2020-05-06 02:43:53 +0300

    Строки-5

---
+++ Dolgikh_AA/Stroki5/func.h
@@ -0,0 +1,2 @@
+char *read_long_string(FILE *f);
+void FUNC(FILE *input, int *words, int *minword, int *maxword, int *symbols, int *symbolstable);
--- Dolgikh_AA/Stroki5/stroki.c
+++ Dolgikh_AA/Stroki5/stroki.c
@@ -1,115 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-char *read_long_string(FILE *f)
-{
-	char buf[1024];
-	buf[0] = 0;
-	char *s = fgets(buf, 1024, f);
-	if(s)
-	{
-		int len = strlen(s);
-		char *result = malloc(len + 1);
-		strcpy(result, s);
-		return result;
-	}
-	return NULL;
-}
-
-void FUNC(FILE *input, char *s, int *words, int *minword, int *maxword, int *symbols, int *symbolstable)
-{
-	int curr_word = 0; //это длина текущего слова
-	long int i;
-	int spaceind = 1; //это индикатор пробела в предыдущем символе
-	int symbol; //это код считываемого символа
-	*words = 0; //это счётчик слов
-	*minword = 0; //это минимальная длина слова
-	*maxword = 0; //это максимальная длина слова
-	*symbols = 0; //это счётчик символов
-	long int len;
-	while((s = read_long_string(input)) != NULL)
-	{
-		fprintf(stdout,"%s",s);
-		len = strlen(s);
-		
-
-		for(i = 0; i < len - 1; i++)
-		{
-			symbol = s[i];
-			if(symbol != 32) //Каждый символ кроме пробела заносим в табличку
-			{
-				(*symbols)++;
-				symbolstable[symbol]++;
-				if(spaceind)
-					(*words)++;
-			}
-			if(symbol == 32 || i == len - 2) //Если попался пробел или кончилась строка
-			{
-				if(curr_word > *maxword)
-					*maxword = curr_word;
-				if((*minword == 0) || (curr_word < *minword))
-					*minword = curr_word;
-				spaceind = 1; 
-				curr_word = 0; 
-			}
-			else
-			{
-				spaceind = 0; 
-				curr_word++;
-			}
-		}
-	}	
-
-}
-
-void func(char *s, long int len, int *words, int *minword, int *maxword, int *symbols, int *symbolstable)
-{
-	int curr_word = 0; //это длина текущего слова
-	long int i;
-	int spaceind = 1; //это индикатор пробела в предыдущем символе
-	int symbol; //это код считываемого символа
-
-	*words = 0; //это счётчик слов
-	*minword = 0; //это минимальная длина слова
-	*maxword = 0; //это максимальная длина слова
-	*symbols = 0; //это счётчик символов
-
-	for(i = 0; i < len - 1; i++)
-	{
-		symbol = s[i];
-		if(symbol != 32) //Каждый символ кроме пробела заносим в табличку
-		{
-			(*symbols)++;
-			symbolstable[symbol]++;
-			if(spaceind)
-				(*words)++;
-		}
-		if(symbol == 32 || i == len - 2) //Если попался пробел или кончилась строка
-		{
-			if(curr_word > *maxword)
-				*maxword = curr_word;
-			if((*minword == 0) || (curr_word < *minword))
-				*minword = curr_word;
-			spaceind = 1; 
-			curr_word = 0; 
-		}
-		else
-		{
-			spaceind = 0; 
-			curr_word++; 
-		}
-	}
-}
+#include "func.h"
 
 int main(void)
 {
 	FILE *input;
-	char *s;
 	int i;
 	long int len;
 	int *symbolstable;
 	int words, minword, maxword, symbols;
+	double average;
 	
 	symbolstable = (int *)malloc(256 * sizeof(int));
 	for(i = 0; i < 256; i++)
@@ -117,28 +18,18 @@ int main(void)
 
 	input = fopen("input.txt","r");
 	
-	while((s = read_long_string(input)) != NULL)
-	{
-		fprintf(stdout,"%s",s);
-		len = strlen(s);
-		func(s, len, &words, &minword, &maxword, &symbols, symbolstable);
-		fprintf(stderr, "words: %d min: %d max: %d symbols: %d\n", words, minword, maxword, symbols);
-		for(i = 0; i < 256; i++)
-		{
-			if(symbolstable[i] != 0)
-				fprintf(stdout, "symbol %c: %d times\n", i, symbolstable[i]);
-		}
-	}
 
-	/*FUNC(input, s, &words, &minword, &maxword, &symbols, symbolstable);
-	fprintf(stderr, "words: %d min: %d max: %d symbols: %d\n", words, minword, maxword, symbols);
+	FUNC(input, &words, &minword, &maxword, &symbols, symbolstable);
+	average = (double)(symbols)/(double)(words);
+	fprintf(stderr, "max: %d min: %d average: %g symbols/word\n", maxword, minword, average);
 	for(i = 0; i < 256; i++)
 	{
 		if(symbolstable[i] != 0)
-			fprintf(stdout, "symbol %c: %d times\n", i, symbolstable[i]);
-	}*/
+			fprintf(stdout, "symbol %c: %d times = %g percents\n", i, symbolstable[i], (double)symbolstable[i]/(double)symbols*100);
+	}
+
+
 	fclose(input);
-	free(s);
 	free(symbolstable);
 	return 0;
 }
