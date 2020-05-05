64a280049077d18d5fa4773a988e3b6e1a3c1cd3
fludaw <fludaw@mail.ru>
2020-05-06 01:42:49 +0300

    ( ͡~ ͜ʖ ͡°)
    ( ͡~ ͜ʖ ͡°)

--- Matuz_SI/string-2/replace.c
+++ Matuz_SI/string-2/replace.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "replace.h"
+
+
 
 void read_file(FILE *file, char **data, size_t bufsize, int *array_size)
 {
     int i = 0;
     data[0] = NULL;
-    while ((getline(&data[i], &bufsize, file)) != -1) {
+    while ((getline(&data[i], &bufsize, file)) != -1) 
+    {
         //printf("%s", data[i]);
         data[i + 1] = NULL;
         i++;
@@ -14,8 +18,8 @@ void read_file(FILE *file, char **data, size_t bufsize, int *array_size)
     *array_size = i;
     return;
 }
-void ReplaceWord(const char *s, const char *old,
-                  const char *new)
+
+void ReplaceWord(const char *s, const char *old, const char *new)
 {
     char *result = NULL;
     int i, count = 0;
@@ -47,59 +51,4 @@ void ReplaceWord(const char *s, const char *old,
     result[i] = '\0';
     printf("%s", result);
     free(result);
-}
-
-
-
-int main(void) {
-    FILE *input;//, *output;
-    int size = 1000;
-    size_t bufsize = 32;
-    char **data;
-    data = malloc(bufsize * sizeof(char *));
-
-    char old[] = "bla";
-    char new[] = "cool";
-
-
-    if ((input = fopen("input.txt", "r")) == NULL) {
-        printf("cannot open input file\n");
-        return -1;
-    }
-
-    /*if ((output = fopen("output.txt", "r")) == NULL)
-    {
-        printf("cannot open output file\n");
-        fclose(input);
-        return -1;
-    }*/
-
-    read_file(input, data, bufsize, &size);
-
-    if (size == 0)
-    {
-        printf("nothing to replace cuz empty file");
-        return -1;
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%s", data[i]);
-    }
-    printf("\n////////////////\n");
-    for (int i = 0; i < size; i++)
-    {
-        ReplaceWord(data[i], old, new);
-    }
-
-    printf("\n %d", size);
-    fclose(input);
-    //fclose(output);
-
-    for (int i = 0; i <= size; i++)
-    {
-	free(data[i]);
-    }
-    free(data);
-    return 0;
 }
---
+++ Matuz_SI/string-2/replace.h
@@ -0,0 +1,2 @@
+void read_file(FILE *file, char **data, size_t bufsize, int *array_size);
+void ReplaceWord(const char *s, const char *old, const char *new);
