5dc95f2af52cd988e464cb45014c259516d42a1a
ki-zuev <kizuev02@gmail.com>
2020-05-07 21:49:20 +0300

    Решение задачи 1

--- Zuev_KP/1stri/zamena.c
+++ Zuev_KP/1stri/zamena.c
@@ -7,21 +7,21 @@ char *strcpy(char *t, const char *s);
 
 void zamena(FILE *input1, FILE *output) 
 {
-	int i = 0, j, k = 1, len = 0, n = 0, num1, num2;
+	int i = 0, len = 0;
 	char c;
 	char *s;	
 	while((s = read_string(input1)) != NULL)
 	{
 		len = length(s);
-	        while(s[i] != '\0')
+		while(s[i] != '\0')
 		{
-		         fprintf(output, "%c", s[i]);
-  			 while(s[i] == s[i+1])
-			 {
-				 i++;
-   			 }
-			 i++;
- 		 }
+			fprintf(output, "%c", s[i]);
+			while(s[i] == s[i+1])
+			{
+				i++;
+			}
+			i++;
+		}
 		free(s);
 	}	
 }
@@ -49,27 +49,31 @@ char *strcpy(char *t, const char *s)
 
 char *read_string(FILE *f) 
 {
-	int l = 0, l1 = 0;
+	int l = 0, l1 = 0;	
 	char *result;
 	char buf[1024];
 	buf[0] = 0;	
 	char *s = fgets(buf, 1024, f);
-	if(!s)
+	if(!s) 
 		return NULL;
 	else
 	{
 		l = length(s);
-		result = (char *) malloc((l + 1)*sizeof(char));
-		strcpy(result, s);
-		while((s) && (s[length(s)-1] != '\n'))
+		result = (char*) malloc((l + 1)*sizeof(char));;
+		while(s)
 		{
-			strcpy(result, s);
-			l1 = length(s);
-			l = l+l1;
-			result = (char *) realloc(result, l);
+			strcpy(result+l1, s);
+			if(s[length(s)-1] == '\n') 
+				break;
 			s = fgets(buf, 1024, f);
-		}
+			if (s) 
+			{
+				l1 = length(s);
+				l = l+l1;
+				result = (char*) realloc(result, l);
+			}
+		} 
 		return result;
-	}
+	} 
 }
 
