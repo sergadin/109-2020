6276a4c2cc03c05ddffca6d48af52508f3d6d942
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-05-10 00:01:12 +0300

    Вижу колонны замерших внуков,

--- Gorev_VA/3.9/file.c
+++ Gorev_VA/3.9/file.c
@@ -37,6 +37,7 @@ int INCLUDE(char *progname, char *filename)
 	N = 0;
 	while (fscanf(prog, "%c", &c) == 1)
 	{
+		printf("Just hallo, just how are you?\nJust hallo, just how are you?\nJust hallo, just how are you?\n");
 		// write char to the string
 		if ((strlen(A[N]) + 2) > kNUM2)
 		{
