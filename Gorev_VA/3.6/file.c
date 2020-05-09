0823a3e9eb790f4fcdb4f0e40ea30a5a9af29db8
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-05-10 01:19:31 +0300

    К нему не зарастет народная тропа,

--- Gorev_VA/3.6/file.c
+++ Gorev_VA/3.6/file.c
@@ -1,66 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "sort.h"
 
-#define NUM 1
-
-int INCLUDE(char *progname, char *filename);
-int INCLUDE(char *progname, char *filename)
+void Print(char **A);
+void Print(char **A)
 {
-	FILE *prog;
-	FILE *file;
-	char **A;
-	char c;
-	int N, i;
-	char *incl = 0;
-	int kNUM1, kNUM2;
-
-	//open file progname
-	if ((prog = fopen(progname, "r")) == NULL)
+	int i = 0;
+	while(strlen(A[i]) > 0)
 	{
-		printf("Can't open file' %s\n", progname);
-		return -1;
-	}
-
-	// create string "#include filename"
-	incl = (char*)malloc((strlen("#include ") + strlen(filename) + 1) * sizeof(char));
-	strcpy(incl, "#include ");
-	strcpy(incl + strlen("#include "), filename);
-
-	// create array of strings
-	// A[i] - string in prog or contents of the file
-	A = (char**)malloc(NUM * sizeof(char*));
-	A[0] = (char*)malloc(NUM * sizeof(char));
-	A[0][0] = 0;
-	kNUM1 = kNUM2 = NUM;
-
-	//write all
-	//prog = fopen(progname, "w");
-	i = 0;
-	while (i <= N)
-	{
-		printf("%s", A[i]);
+		printf("%s\n", A[i]);
 		i++;
 	}
-
-	//fclose(prog);
-	i = 0;
-	while (i <= N)
-	{
-		free(A[i]);
-		i++;
-	}
-	free(A);
-	free(incl);
-	return 0;
 }
 
 int main(void)
 {
-	char *filename = "input.txt";
-	char *progname = "programm.txt";
+	char **s;
+	int i = 0;
+	FILE *input;
+	if ((input = fopen("input.txt", "r")) == NULL)
+	{
+		printf("Не удалось открыть файл input.txt\n");
+		return -1;
+	}
+	
+	s = ar_of_words(input);
+	Print(s);
 
-	INCLUDE(progname, filename);
-
+	while (strlen(s[i]) > 0)
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s[i]);
+	free(s);
+	fclose(input);
 	return 0;
 }
