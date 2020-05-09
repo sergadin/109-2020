568fdad50bb5ebf822e58cfd53a7125b860ce816
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-05-09 18:40:41 +0300

    Добавлено дофига всего

--- Gorev_VA/3.9/file.c
+++ Gorev_VA/3.9/file.c
@@ -2,46 +2,129 @@
 #include <stdlib.h>
 #include <string.h>
 
-void INCLUDE(FILE *file, char *filename)
+int INCLUDE(char *progname, char *filename)
 {
-	char *A;
+	FILE *prog;
+	FILE *file;
+	char **A;
 	char c;
-	int i = 0;
-	A = (char*)malloc(sizeof(char));
-	A[0] = 0;
-	fscanf(file, "%c", &c);
-	while(!feof(file))
+	int N, i;
+	char *incl = 0;
+	
+	//open file progname
+	if ((prog = fopen(progname, "r")) == NULL)
 	{
-		//printf("%c", c);
-		A[strlen(A)] = c;
-		A = (char*)realloc(A, (strlen(A) + 2) * sizeof(char));
-		A[strlen(A)] = 0;
-		fscanf(file, "%c", &c);
+		printf("Не удалось открыть файл %s\n", progname);
+		return -1;
 	}
-//	file = fopen(filename, "w");
-	while (i < strlen(A))
+	
+	// create string "#include filename"
+	incl = (char*)malloc((strlen("#include ") + strlen(filename) + 1) * sizeof(char));
+	strcpy(incl, "#include ");
+	strcpy(incl + strlen("#include "), filename);
+	
+	// create array of strings
+	// A[i] - string in prog or contents of the file
+	A = (char**)malloc(sizeof(char*));
+	A[0] = (char*)malloc(sizeof(char));
+	A[0][0] = 0;
+	
+	N = 0;
+	while (fscanf(prog, "%c", &c) == 1)
 	{
-		printf("%c", A[i]);
+		// write char to the string
+		A[N] = (char*)realloc(A[N], (strlen(A[N]) + 2) * sizeof(char));
+		A[N][strlen(A[N]) + 1] = 0;
+		A[N][strlen(A[N])] = c;
+		
+		// if c == '\n' check equal to "#include filename" and create new string
+		if (c == '\n')
+		{
+			// check equal to "#include filename"
+			if (A[N] == strstr(A[N], incl))
+			{
+				// open filename
+				if ((file = fopen(filename, "r")) == NULL)
+				{
+					fclose(prog);
+					printf("Не удалось открыть файл %s\n", file);
+					return -2;
+				}
+				
+				// clear A[N] ("#include filename")
+				free(A[N]);
+				A[N] = (char*)malloc(sizeof(char));
+				A[N][0] = 0;
+				
+				// write contents of the file to the A[N]
+				while (fscanf(file, "%c", &c) == 1)
+				{
+					A[N] = (char*)realloc(A[N], (strlen(A[N]) + 2) * sizeof(char));
+					A[N][strlen(A[N]) + 1] = 0;
+					A[N][strlen(A[N])] = c;
+				}
+				
+				// old A[N] ended with '\n' so we must write it
+				c = '\n';
+				A[N] = (char*)realloc(A[N], (strlen(A[N]) + 2) * sizeof(char));
+				A[N][strlen(A[N]) + 1] = 0;
+				A[N][strlen(A[N])] = c;
+				
+				fclose(file);
+			}
+			
+			// create new string
+			A = (char**)realloc(A, (N + 2) * sizeof(char*));
+			N++;
+			A[N] = (char*)malloc(sizeof(char));
+			A[N][0] = 0;
+		}
+	}
+	
+	// most probably last A[N] don't ended with '\n' so we must check its equal to "#include filename"
+	if (A[N][strlen(A[N]) - 1] != '\n')
+		if (A[N] == strstr(A[N], incl))
+		{
+			if ((file = fopen(filename, "r")) == NULL)
+			{
+				fclose(prog);
+				printf("Не удалось открыть файл %s\n", file);
+				return -2;
+			}
+			
+			free(A[N]);
+			A[N] = (char*)malloc(sizeof(char));
+			A[N][0] = 0;
+			while (fscanf(file, "%c", &c) == 1)
+			{
+				A[N] = (char*)realloc(A[N], (strlen(A[N]) + 2) * sizeof(char));
+				A[N][strlen(A[N]) + 1] = 0;
+				A[N][strlen(A[N])] = c;
+			}
+			fclose(file);
+		}
+	fclose(prog);
+	
+	//write all
+	prog = fopen(progname, "w");
+	i = 0;
+	while (i <= N)
+	{
+		fprintf(prog, "%s", A[i]);
 		i++;
-	}	
-	fclose(file);
-//	fclose(file);
+	}
+	
+	//fclose(prog);
 	free(A);
+	return 0;
 }
 
 int main(void)
 {
-	FILE *file;
-	FILE *programm;
 	char *filename = "input.txt";
-	char **A;
-	if ((file = fopen(filename, "r")) == NULL)
-	{
-		printf("ГЌГҐ ГіГ¤Г Г«Г®Г±Гј Г®ГІГЄГ°Г»ГІГј ГґГ Г©Г« %s\n", filename);
-		return -1;
-	}
+	char *progname = "programm.txt";	
 	
-	INCLUDE(file, filename);
+	INCLUDE(progname, filename);
 	
 	return 0;
 }
