aeb74351529679962b7f63cab9ccfffdb56c330f
Makar Chizhik <makar.chizhik@gmail.com>
2020-05-14 21:18:01 +0300

    Изменена структура программы

--- Chizhik_MA/F.4/test.c
+++ Chizhik_MA/F.4/test.c
@@ -1,70 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ftw.h>
 
-#include "printdiff.h"
-
-#define NOPENFD 10
-#define ARR_EXTENSION 10
-
-char **files1 = NULL;
-char **files2 = NULL;
-
-int len1 = 0, len2 = 0;
-int actual_len1 = 0, actual_len2 = 0;
-
-char ***curr_catalog;
-int *curr_len;
-int *curr_actual_len;
-
-/*
- * fn - колбэк-функция для ftw
- *
- * Параметры:
- * fpath: путь к файлу (или директории)
- * sb: указатель на структуру, содержащую основную информацию о файле
- * typeflag: целочисленное значение, обозначающее тип файла
- *
- * fn динамически выделяет память для массива, находящегося по адресу curr_catalog, если 
- * место под очередное имя файла закончилось, и добавляют в этот массив новое имя 
- * (без пути к файлу, только его имя; поэтому два разных файла с одним и тем же именем будут 
- * отождествлены), если ftw вызвала fn не для директории, а для файла.
- *
- * Возвращаемое значение: в случае ошибки (выделения памяти) - (-1), в остальных случаях - 0.
- */
-
-static int fn(const char *fpath, const struct stat *sb, int typeflag) {
-	char *last_slash;
-
-	if (typeflag == FTW_D) {
-		return 0;
-	}
-
-	if (*curr_len == *curr_actual_len) {
-		if ((*curr_catalog = (char **)realloc(*curr_catalog, (*curr_len + ARR_EXTENSION) * sizeof(char *))) == NULL) {
-			return -1;
-		}
-		*curr_len += ARR_EXTENSION;
-	}
-
-	last_slash = strrchr(fpath, '/');
-
-	if (last_slash != NULL) {
-		if (((*curr_catalog)[*curr_actual_len] = (char *)malloc((strlen(last_slash + 1) + 1) * sizeof(char))) == NULL) {
-			return -1;
-		}
-		strcpy((*curr_catalog)[*curr_actual_len], last_slash + 1);
-	} else {
-		if (((*curr_catalog)[*curr_actual_len] = (char *)malloc((strlen(fpath) + 1) * sizeof(char))) == NULL) {
-			return -1;
-		}
-		strcpy((*curr_catalog)[*curr_actual_len], fpath);
-	}
-
-	(*curr_actual_len)++;
-	return 0;
-}
+#include "getdata.h"
 
 static void print_usage(char *program_name) {
 	fprintf(stderr, "usage: %s dir1 dir2 output\n", program_name);
@@ -86,6 +22,7 @@ static void print_usage(char *program_name) {
 
 int main(int argc, char **argv) {
 	FILE *output;
+	int flag = 0;
 
 	if ((argc < 4) || (argv[1] == "--help")) {
 		print_usage(argv[0]);
@@ -98,49 +35,14 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	curr_catalog = &files1;
-	curr_len = &len1;
-	curr_actual_len = &actual_len1;
-
-	if (ftw(argv[1], fn, NOPENFD) != 0) {
-		fprintf(stderr, "Something went wrong...\n");
-		print_usage(argv[0]);
-		fclose(output);
-		return -1;
-	}
-
-	for (int k = 0; k < *curr_actual_len; k++) {
-
-	}
-
-	curr_catalog = &files2;
-	curr_len = &len2;
-	curr_actual_len = &actual_len2;
-
-	if (ftw(argv[2], fn, NOPENFD) != 0) {
-		fprintf(stderr, "Something went wrong...\n");
-		print_usage(argv[0]);
-
-		for (int i = 0; i < actual_len1; i++) {
-			free(files1[i]);
-		}
-
-		fclose(output);
-		return -1;
-	}
-
-	print_sym_difference(output, files1, argv[1], actual_len1, files2, argv[2], actual_len2);
-
-	for (int i = 0; i < actual_len1; i++) {
-		free(files1[i]);
-	}
-
-	for (int i = 0; i < actual_len2; i++) {
-		free(files2[i]);
-	}
-	free(files1);
-	free(files2);
-
+	flag = get_data(output, argv[1], argv[2]);
 	fclose(output);
+
+	if (flag != 0) {
+		fprintf(stderr, "Something went wrong\n");
+		print_usage(argv[0]);
+		return -1;
+	}
+
 	return 0;
 }
