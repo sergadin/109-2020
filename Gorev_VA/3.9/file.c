b1e479379b83f66d43479b0673ae49631aa13642
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-05-17 23:23:21 +0300

    Add files via upload
    #include <stdio.h>
    #include <stdlib.h>
    #include <string.h>
    
    int INCLUDE(char *progname);
    int INCLUDE(char *progname)
    {
    	FILE *proglen;
    	FILE *progchar;
    	char c = 'A';
    	char *A;
    	int len;
    	int Eof = 0, Ind = 0;
    	
    	// open progname
    	if ((progchar = fopen(progname, "r")) == NULL)
    	{
    		printf("~~~INCLUDE(%s): Can't open file \"%s\"\n", progname, progname);
    		return -1;
    	}
    	if ((proglen = fopen(progname, "r")) == NULL)
    	{
    		fclose(progchar);
    		printf("~~~INCLUDE(%s): Can't open file \"%s\"\n", progname, progname);
    		return -1;
    	}
    	len = 0;
    	while(!Eof)
    	{
    		if (fscanf(proglen, "%c", &c) != 1)
    			Eof = 1;
    		else
    			len++;
    		//printf("%c", c);
    		if ((c == '\n') || Eof)
    		{
    			A = (char*)malloc((len + 1) * sizeof(char));
    			fgets(A, len + 1, progchar);
    			if (strstr(A, "#include ") == A)
    			{
    				if (A[len - 1] == '\n')
    				{
    					A[len - 1] = 0;
    					Ind = 1;
    				}
    				else
    					Ind = 0;
    				if (strcmp(A + strlen("#include "), progname) == 0)
    				{
    					printf("~~~INCLUDE(%s): Loop output: \"#include %s\" in the file \"%s\"\n", progname, progname, progname);
    					fclose(proglen);
    					fclose(progchar);
    					free(A);
    					return -2;
    				}
    				else
    				{
    					if (INCLUDE(A + strlen("#include ")))
    						return -3;
    					if (Ind)
    						printf("\n");
    				}
    			}
    			else
    				printf("%s", A);
    			free(A);
    			len = 0;
    		}
    	}
    	
    	fclose(progchar);
    	fclose(proglen);
    	//fclose(progchar);
    	return 0;
    }
    
    int main(void)
    {
    	char *progname = "programm.txt";
    	if (INCLUDE(progname))
    		printf("~~~ERROR\n");
    	return 0;
    }

--- Gorev_VA/3.9/file.c
+++ Gorev_VA/3.9/file.c
@@ -2,143 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NUM 3
-
-// copy contents of the file filename to empty string str
-char *copy(char *filename);
-char *copy(char *filename)
+int INCLUDE(char *progname);
+int INCLUDE(char *progname)
 {
-	FILE *file;
-	char c;
-	char *str;
-	int kNUM2 = NUM;
-	if ((file = fopen(filename, "r")) == NULL)
+	FILE *proglen;
+	FILE *progchar;
+	char c = 'A';
+	char *A;
+	int len;
+	int Eof = 0, Ind = 0;
+	
+	// open progname
+	if ((progchar = fopen(progname, "r")) == NULL)
 	{
-		printf("Can't open file' %s\n", filename);
-		return 0;
-	}
-	str = (char*)malloc(kNUM2 * sizeof(char));
-	str[0] = 0;
-	while(fscanf(file, "%c", &c) == 1)
-	{
-		if ((strlen(str) + 2) > kNUM2)
-		{
-			kNUM2 += NUM;
-			str = (char*)realloc(str, kNUM2 * sizeof(char));
-		}
-		str[strlen(str) + 1] = 0;
-		str[strlen(str)] = c;
-	}
-	fclose(file);
-	return str;
-}
-
-int INCLUDE(char *progname, char *filename);
-int INCLUDE(char *progname, char *filename)
-{
-	FILE *prog;
-	FILE *file;
-	char **A;
-	char c;
-	int N = 0, i = 0;
-	char *incl = 0;
-	int kNUM1, kNUM2;
-
-	//open file progname
-	if ((prog = fopen(progname, "r")) == NULL)
-	{
-		printf("Can't open file' %s\n", progname);
+		printf("~~~INCLUDE(%s): Can't open file \"%s\"\n", progname, progname);
 		return -1;
 	}
-
-	// create string "#include filename"
-	incl = (char*)malloc((strlen("#include ") + strlen(filename) + 1) * sizeof(char));
-	strcpy(incl, "#include ");
-	strcpy(incl + strlen("#include "), filename);
-
-	// create array of strings
-	// A[i] - string in prog or contents of the file filename
-	kNUM1 = kNUM2 = NUM;
-	A = (char**)malloc(kNUM1 * sizeof(char*));
-	A[0] = (char*)malloc(kNUM2 * sizeof(char));
-	A[0][0] = 0;
-	N = 0;
-	while (fscanf(prog, "%c", &c) == 1)
+	if ((proglen = fopen(progname, "r")) == NULL)
 	{
-		// put char in string
-		if ((strlen(A[N]) + 2) > kNUM2)
+		fclose(progchar);
+		printf("~~~INCLUDE(%s): Can't open file \"%s\"\n", progname, progname);
+		return -1;
+	}
+	len = 0;
+	while(!Eof)
+	{
+		if (fscanf(proglen, "%c", &c) != 1)
+			Eof = 1;
+		else
+			len++;
+		//printf("%c", c);
+		if ((c == '\n') || Eof)
 		{
-			kNUM2 += NUM;
-			A[N] = (char*)realloc(A[N], kNUM2 * sizeof(char));
-		}
-		A[N][strlen(A[N]) + 1] = 0;
-		A[N][strlen(A[N])] = c;
-
-		// if c == '\n' we must check equal to "#include filename" and create new string
-		if (c == '\n')
-		{
-			// check equal to "#include filename"
-			if (A[N] == strstr(A[N], incl))
+			A = (char*)malloc((len + 1) * sizeof(char));
+			fgets(A, len + 1, progchar);
+			if (strstr(A, "#include ") == A)
 			{
-				free(A[N]);
-				A[N] = copy(filename);
-				if (((strlen(A[N]) + 1) % NUM) == 0)
+				if (A[len - 1] == '\n')
 				{
-					A[N] = (char*)realloc(A[N], (strlen(A[N]) + 2) * sizeof(char));
+					A[len - 1] = 0;
+					Ind = 1;
 				}
-				A[N][strlen(A[N]) + 1] = 0;
-				A[N][strlen(A[N])] = '\n';
+				else
+					Ind = 0;
+				if (strcmp(A + strlen("#include "), progname) == 0)
+				{
+					printf("~~~INCLUDE(%s): Loop output: \"#include %s\" in the file \"%s\"\n", progname, progname, progname);
+					fclose(proglen);
+					fclose(progchar);
+					free(A);
+					return -2;
+				}
+				else
+				{
+					if (INCLUDE(A + strlen("#include ")))
+						return -3;
+					if (Ind)
+						printf("\n");
+				}
 			}
-
-			//create new string
-			if ((N + 2) > kNUM1)
-			{
-				kNUM1 += NUM;
-				A = (char**)realloc(A, kNUM1 * sizeof(char*));
-			}
-			N++;
-			kNUM2 = NUM;
-			A[N] = (char*)malloc(kNUM2 * sizeof(char));
-			A[N][0] = 0;
+			else
+				printf("%s", A);
+			free(A);
+			len = 0;
 		}
 	}
 	
-	// most probably there is not '\n' at the end of the last string
-	if (strlen(A[N]) > 0)
-		if ((A[N][strlen(A[N]) - 1] != '\n') && (A[N] == strstr(A[N], incl)))
-		{
-			free(A[N]);
-			A[N] = copy(filename);
-		}
-
-	//write all
-	//prog = fopen(progname, "w");
-	i = 0;
-	while (i <= N)
-	{
-		printf("%s", A[i]);
-		i++;
-	}
-	
-	i = 0;
-	while (i <= N)
-	{
-		free(A[i]);
-		i++;
-	}
-	free(A);
-	free(incl);
-	fclose(prog);
+	fclose(progchar);
+	fclose(proglen);
+	//fclose(progchar);
 	return 0;
 }
 
 int main(void)
 {
-	char *filename = "input.txt";
 	char *progname = "programm.txt";
-
-	INCLUDE(progname, filename);
-
+	if (INCLUDE(progname))
+		printf("~~~ERROR\n");
 	return 0;
 }
 
