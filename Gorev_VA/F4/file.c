feb03bbaec481326eb27504f2a3ab6e32c564e67
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-05-21 19:31:06 +0300

    Update file.c
    #include <stdio.h>
    #include <stdlib.h>
    #include <ftw.h>
    
    int func(const char *fpath, const struct stat *sb, int flag);
    int func(const char *fpath, const struct stat *sb, int flag)
    {
    	printf("%s\n", fpath);
    	return 0;
    }
    
    int main(void)
    {
    	ftw("dir1", func, 20);
    	return 0;
    }

--- Gorev_VA/F4/file.c
+++ Gorev_VA/F4/file.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <ftw.h>
 
+int func(const char *fpath, const struct stat *sb, int flag);
+int func(const char *fpath, const struct stat *sb, int flag)
+{
+	printf("%s\n", fpath);
+	return 0;
+}
+
 int main(void)
 {
-	
+	ftw("dir1", func, 20);
 	return 0;
 }
