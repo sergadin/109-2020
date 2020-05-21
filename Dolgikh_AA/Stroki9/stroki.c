12e71e2e334433a4219190d4dff2ca7a703a01c8
Александр <alexandr30032014@mail.ru>
2020-05-21 19:24:24 +0300

    fix

--- Dolgikh_AA/Stroki9/stroki.c
+++ Dolgikh_AA/Stroki9/stroki.c
@@ -9,40 +9,39 @@ int main()
 {
 	FILE *original_file; //это наш главный файл
 	FILE *include_file; //это наш файл, который мы инклюдим
-	char **strings_of_original_file; //это массив строк главного файла
 	char **output_strings; //это массив строк, которые мы будем выводить
 	int i = 0, j;
-	char shablon[8] = "#include"; 
+	char shablon[10] = "#include ";
 	char *include_file_name; //это строчка, куда будет записываться имя файла, который нужно заинклюдить
-	int include_not_found;
+	int include_found;
 	char *s;
 
-	strings_of_original_file = malloc(MAX_SIZE * sizeof(char *));
 	output_strings = malloc(MAX_SIZE * sizeof(char *));
 
 	original_file  = fopen("input.txt","r"); //открываем главный файл
+	
 	while((s = read_long_string(original_file)) != NULL) //пока читаются строки из файла
 	{
-		strings_of_original_file[i] = malloc((strlen(s)+1) * sizeof(char));
-		strings_of_original_file[i] = strcpy(strings_of_original_file[i],s); //считавшуюся строку записываем в массив
-		free(s);
-
-		include_not_found = strncmp(strings_of_original_file[i],shablon,8); //эта переменная показывает, нашли ли мы строку, начинающуюся с #include
-		if(!include_not_found) //если нашли, то...
+		include_found = !strncmp(s,shablon,9); //эта переменная показывает, нашли ли мы строку, начинающуюся с #include
+		if(include_found) //если нашли, то...
 		{
-			include_file_name = malloc((strlen(strings_of_original_file[i]) - 9) * sizeof(char)); //выделяем память под имя инклюдируемого файла
-			for(j = 0; j < strlen(strings_of_original_file[i]) - 10; j++)
+			printf("include found\n");
+			include_file_name = malloc((strlen(s) - 9) * sizeof(char)); //выделяем память под имя инклюдируемого файла
+			for(j = 0; j < strlen(s) - 10; j++)
 			{
-				include_file_name[j] = strings_of_original_file[i][j+9]; //записываем в специальную строку имя файла, который нужно заинклюдить
+				include_file_name[j] = s[j+9]; //записываем в специальную строку имя файла, который нужно заинклюдить
 			}
-			include_file_name[strlen(strings_of_original_file[i])-10] = 0;
+			include_file_name[strlen(s)-10] = 0;
+			printf("include file name = %s\n",include_file_name);
 			include_file = fopen(include_file_name,"r"); //открываем инклюдируемый файл
-			free(strings_of_original_file[i]); //освобождаем строчку главного файла 
+			free(s);
 			free(include_file_name); //освобождаем место под имя файла
 			while((s = read_long_string(include_file)) != NULL) //пока читаются строки из инклюдируемого файла
 			{
 				output_strings[i] = malloc((strlen(s)+1) * sizeof(char)); 
 				output_strings[i] = strcpy(output_strings[i], s); //записываем их в массив
+				printf("String %d\n",i+1);
+				printf("%s",output_strings[i]);
 				free(s);
 				i++; //увеличиваем счётчик строк
 			}
@@ -50,9 +49,11 @@ int main()
 		}
 		else
 		{
-			output_strings[i] = malloc((strlen(strings_of_original_file[i])+1) * sizeof(char));
-			output_strings[i] = strcpy(output_strings[i], strings_of_original_file[i]); //строку из массива строк главного файла записываем в массив выводимых строк
-			free(strings_of_original_file[i]); //освобождаем строчку главного файла
+			output_strings[i] = malloc((strlen(s)+1) * sizeof(char));
+			output_strings[i] = strcpy(output_strings[i], s); //строку из массива строк главного файла записываем в массив выводимых строк
+			free(s); //освобождаем строчку главного файла
+			printf("String %d\n",i+1);
+			printf("%s",output_strings[i]);
 			i++; //увеличиваем счётчик строк
 		}
 	}
@@ -69,7 +70,6 @@ int main()
 	{
 		free(output_strings[j]);
 	}
-	free(strings_of_original_file);
 	free(output_strings); //всех освобождаем
 	return 0;
 }
