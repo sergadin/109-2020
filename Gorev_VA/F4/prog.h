9a1f8dcf778b5db3901e32467d850cba2846465f
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-05-23 02:34:15 +0300

    Update prog.h
    struct chain // реализация списка файлов
    {
    	char *name;         // имя файла 
    	struct chain *next; // указатель на следующий файл в списке
    	                    // если следующего нет, то 0
    	struct chain *prev; // указатель на предыдущий файл в списке
    	                    // если предыдущего нет, то 0
    };
    
    char *file_name(const char *filename);
    int write_in_filelist(const char *filename, struct chain *filelist);
    int find_file_in_list(char *short_filename, struct chain *filelist);
    int func_for_ftw(const char *fpath, const struct stat *sb, int flag);
    int dif();

--- Gorev_VA/F4/prog.h
+++ Gorev_VA/F4/prog.h
@@ -1,2 +1,14 @@
-char *dir1;
-char *dir2;
+struct chain // реализация списка файлов
+{
+	char *name;         // имя файла 
+	struct chain *next; // указатель на следующий файл в списке
+	                    // если следующего нет, то 0
+	struct chain *prev; // указатель на предыдущий файл в списке
+	                    // если предыдущего нет, то 0
+};
+
+char *file_name(const char *filename);
+int write_in_filelist(const char *filename, struct chain *filelist);
+int find_file_in_list(char *short_filename, struct chain *filelist);
+int func_for_ftw(const char *fpath, const struct stat *sb, int flag);
+int dif();
