253caf8c3d86b08c98a2a984286d7e0d8b9cef84
fludaw <fludaw@mail.ru>
2020-05-24 17:04:33 +0300

    :))
    :))

--- Matuz_SI/files-3/files.c
+++ Matuz_SI/files-3/files.c
@@ -1,80 +1,31 @@
-/** (это всё, как я хотела бы, чтобы это всё работало)
- ** Задача:
- **
- **     Для двух заданных каталогов напечатать список файлов,
- **     имеющих совпадающие имена
- **
- ** Параметры:
- **     int argc (argument count) - кол-во аргументов
- **     char *argv[] (argument value) - значения аргументов,
- **                             переданных при запуске (массив аргументов)
- ** Входные данные:
- **     2 аргумента: две заданные директории
- **     остальное мы с программой не признаём
- **
- ** Результат программы:
- **     будет напечатан список файлов с совпадаюшими именами
- **
- **
- ** Метод:
- **     использование функции ftw() из ftw.h
- **     (рекурсивно спускается по иерархии каталогов,
- **     находящихся в path)
- **
- */
-
-
-
-
-//#include <stdio.h>
-/*i thought to use files instead of global array*/
-
 #include <stdlib.h>
-
+#include <stdio.h>
 #include <string.h>
-
 #include <ftw.h>
 #include <sys/stat.h>
 
-//#define N 1024
-//#define NAME_MAX 255
 
-
-/*i found this in one smart book*/
-int list(const char *name, const struct stat *status, int type)
-{
-    if(type == FTW_NS)
-        return 0;
-
-    if(type == FTW_F)
-        printf("0%3o\t%s \n", status->st_mode&0777, name);
-
-    return 0;
-}
-
-/** Функция get_filenames
- **
- ** Цель:
- **     получить именя файлов из path
- **
- **
- ** Как делает:
- **     strrchr находит последнее включение '/', получая "/название файла"
- **
- **     потом какими-то шаманскими операциями было бы неплохо сохранить куда-то
- **
- **
- **/
+char *data;
+int len = 1;
 
 static int get_filenames(const char *fpath, const struct stat *status, int tflag)
 {
     if (tflag == FTW_F)
     {
+//printf("3\n");
         char *temp;
-        if (temp = strrchr(fpath, '/'))
+        if (strrchr(fpath, '/') != NULL)
         {
-            printf("%s \n", temp);
+            temp = strrchr(fpath, '/');
+	    //printf("%ld\n", strlen(temp));
+            data = realloc(data, len + strlen(temp)+1);
+            //printf("%ld\n", len + strlen(temp) + 1); 
+//printf("2\n");
+	    strcpy(data + len, temp); 
+            data[len + strlen(temp)] = ' ';
+            len += (strlen(temp)+1);          
         }
+//printf("1\n");
         
     }
     return 0;
@@ -82,34 +33,50 @@ static int get_filenames(const char *fpath, const struct stat *status, int tflag
 
 
 
-int main(int argc, char *argv[])
+static int compare_filenames(const char *fpath, const struct stat *status, int tflag)
 {
-    if (argc == 1)
-	ftw(".", get_filenames, 1);
+    if (tflag == FTW_F)
+    {   
+        char *temp;
+        char *filename;
+        filename = malloc(2 * sizeof(char));
+        filename[0] = ' ';
+        if (temp = strrchr(fpath, '/'))
+        { 
+	    //printf("00%s\n", temp);
+	    if (strchr(temp, '.') != NULL)
+	    {
+		//printf("01%s \n", strchr(temp, '.'));
+		filename = realloc(filename, strlen(temp) - strlen(strchr(temp, '.'))+2+1);
+            // 1 - space in the beginning, 1 - space in the end, 1 - /0
+                for (int k = 1; k < (strlen(temp) - strlen(strchr(temp, '.'))+1); k++)
+		     filename[k] = temp[k-1];
+                //filename[strlen(temp) - strlen(strchr(temp, '.')) + 1] = ' ';
+		filename[strlen(temp) - strlen(strchr(temp, '.')) + 1] = '\0';
+                if (strstr(data, filename))
+		     printf("11%s \n", filename); 
+             }
+        }
+        free(filename);
+    }
     return 0;
 }
 
 
-/* Для меня про ftw()
- *
- * int fn(const char *fpath, const struct stat sb, int typeflag)
- * int ftw(const char *dirpath, int (*fn),int nopenfd);
- *
- * The argument nopenfd should be in the range [1, {OPEN_MAX}]
- * The nopenfd argument shall specify the maximum number of directory streams or
- * file descriptors or both available for use by ftw() while traversing the tree.
- * When ftw() returns it shall close any directory streams and file descriptors
- * it uses not counting any opened by the application-supplied fn function.
- *
- * If the function pointed to by fn returns a nonzero value,
- * ftw() stops its tree traversal and returns whatever value
- * was returned by the function pointed to by fn().
- *
- * FTW_F    The object is a  file
- * FTW_D    ,,    ,,   ,, ,, directory
- * FTW_DNR  ,,    ,,   ,, ,, directory that could not be read
- * FTW_SL   ,,    ,,   ,, ,, symbolic link
- * FTW_NS   The object is NOT a symbolic link and is one for
- *          which stat() could not be executed
- *
- * */
+int main(int argc, char *argv[])
+{
+    data = malloc(2 * sizeof(char));
+    data[0] = ' ';
+    if (argc == 3)
+    {
+	ftw(argv[1], get_filenames, 10);
+        data = realloc(data, len+1);
+        data[len] = '\0';
+        printf("%s \n", data); 
+        ftw(argv[2], compare_filenames, 10);
+    }
+    else
+	printf("smth wrong");
+    free(data);
+    return 0;
+}
