d1ae22ff20fa16efa7c2a9b83cf4f313dd69f02c
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-05-25 01:20:59 +0300

    Update f.h

--- Markova_MA/matrix1D/f.h
+++ Markova_MA/matrix1D/f.h
@@ -13,7 +13,7 @@ enum
 
 void ved_null(double *a, int n, int m, int ii, int jj, double max);
 void change_column(double *a, int n, int m, int i, int j);
-double eps = 1e-16;
+
 
 //void print_matrix(double *a, int m, int n);
 void ERROR_RE(int ret);
