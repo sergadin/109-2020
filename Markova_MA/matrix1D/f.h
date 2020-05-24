1a5572c8cb1b1526a615e67a19746426245e8302
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-05-25 01:01:42 +0300

    Update f.h

--- Markova_MA/matrix1D/f.h
+++ Markova_MA/matrix1D/f.h
@@ -15,8 +15,7 @@ void ved_null(double *a, int n, int m, int ii, int jj, double max);
 void change_column(double *a, int n, int m, int i, int j);
 double eps = 1e-16;
 
-int read_matrix(double *a, int m, int n, const char *name);
-void print_matrix(double *a, int m, int n);
-void ERROR_RE(int ret, char *name);
+//void print_matrix(double *a, int m, int n);
+void ERROR_RE(int ret);
 
 int rank(double *a, int n, int m);
