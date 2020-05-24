93ae542959a5b71b48d4aa97913f4e1b4352d9ae
Makar Chizhik <makar.chizhik@gmail.com>
2020-05-24 19:09:00 +0300

    Черновик I.2

--- Chizhik_MA/I.2/test.c
+++ Chizhik_MA/I.2/test.c
@@ -11,6 +11,7 @@
 #define EPS 1e-6
 static RootStatus r_s;
 static IntStatus i_s;
+static double alpha;
 
 static double f(double t) {
 	return 1 / sqrt(1 + pow(t, 4));
@@ -37,6 +38,10 @@ static void print_usage(const char *program_name) {
 	fprintf(stderr, "alpha: value of the variable alpha in the equation\n");
 }
 
+static double fn(double x) {
+	return integral_imp(x) - 1 - 0.1 * alpha;
+}
+
 /*
  * Программу следует вызывать по меньшей мере с одним дополнительным параметром:
  * argv[1]: значение alpha.
@@ -47,12 +52,12 @@ static void print_usage(const char *program_name) {
  */
 
 int main(int argc, char **argv) {
-	int alpha;
+	double root;
 	double min_x = -M_PI/2;
 	double max_x = M_PI/2;
 
 	if (argc >1) {
-		alpha = atoi(argv[1]);
+		alpha = atof(argv[1]);
 	} else {
 		print_usage(argv[0]);
 		return -1;
@@ -60,6 +65,11 @@ int main(int argc, char **argv) {
 
 	fprintf(stdout, "Integral of dz/(sqrt(pow(cos(z), 4) + pow(sin(z), 4))) from 0 to PI/2 equals to %lf\n", integral_imp(M_PI/2));
 	fprintf(stdout, "Integral of dz/(sqrt(pow(cos(z), 4) + pow(sin(z), 4))) from 0 to -PI/2 equals to %lf\n", integral_imp(-M_PI/2));
-	fprintf(stdout, "%d\n", atoi(argv[1]));
+	root = find_root(fn, min_x, max_x, EPS, &r_s);
+	if (r_s == OK_ROOT) {
+		fprintf(stdout, "Root of the equation integral(c) = 1 + (%lf) is %lf\n", 0.1 * alpha, tan(root));
+	} else {
+		fprintf(stdout, "I'm sorry! The equation integral_imp(c) = 1 + (%lf) does't have a root\n", 0.1 * alpha);
+	}
 	return 0;
 }
