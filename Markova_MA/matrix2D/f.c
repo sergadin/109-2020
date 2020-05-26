fe5a2231cd0cf25d70c3a5aec675dfb151796a19
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-05-26 21:15:49 +0300

    Update f.c

--- Markova_MA/matrix2D/f.c
+++ Markova_MA/matrix2D/f.c
@@ -4,7 +4,7 @@
 void print_matrix(double *a, int n, int m) {
 	int i, j;
 	for (i = 0; i < n; ++i) {
-		for (j = 0; j < m; ++j)
+		for (j = n; j < m; ++j)
 			printf("%10.3e ", a[i*m + j]);
 		printf("\n");
 	}
