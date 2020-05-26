d005eda5b05904ed74af3e5eb1bac1c097e98548
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-05-26 20:11:26 +0300

    Update test.c

--- Markova_MA/matrix2D/test.c
+++ Markova_MA/matrix2D/test.c
@@ -2,7 +2,7 @@
 #include "f.h"
 
 int main() {
-	int n = 4, m = 2*n, res, i,j, t,  err = SUCCESS;
+	int n = 3, m = 2*n, res, i,j, t,  err = SUCCESS;
 	double *a, eps = 1e-16;
 	FILE *fp;
 	if (!(a = (double *)malloc(sizeof(double) * n * m) )) {
