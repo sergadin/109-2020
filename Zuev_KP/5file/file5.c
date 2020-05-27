4bfc57bb5e2cb6b01ee211135cc36e04feb74117
ki-zuev <kizuev02@gmail.com>
2020-05-28 00:09:29 +0300

    Решение задачи 5

--- Zuev_KP/5file/file5.c
+++ Zuev_KP/5file/file5.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdint.h> // для int64_t
+#include <inttypes.h> // для правильного вывода int64_t в printf
 
-static const char *type;
-int64_t filesize(const char* file_name);
+static int n;
+
+int filesize(const char* file_name);
 int cmp(const void *a, const void *b);
 
 int cmp(const void *a, const void *b) 
@@ -13,18 +16,16 @@ int cmp(const void *a, const void *b)
      return *(int*)a - *(int*)b;
 }
 
-//qsort(a, n, sizeof(int), cmp );
-
-int64_t filesize(const char* file)
+int filesize(const char* file_name)
 {
-	long size = 0;
-	FILE* f = fopen(file, "rb"); //"rb" открывает двоичный файл для чтения
+	int size = 0;
+	FILE* f = fopen(file_name, "rb"); //"rb" открывает двоичный файл для чтения
 	if(f == NULL)
 	{
 		size = -1;
 	}
 	else
-	{
+{
 		while(getc(f) != EOF)
 			size++;
 		fclose(f);
@@ -34,23 +35,32 @@ int64_t filesize(const char* file)
 
 static int check(const char *file, const struct stat *sb, int flag) 
 {
+	int i;
+	int size;
+	int *a;	
+	a = (int*) malloc(n * sizeof(int));
 	if (flag == FTW_F) 
 	{
-			long size;
 			size = filesize(file);
-			printf("%ld\n", size);
-			strrchr(file, '/');
-			printf("%s\n", file);
+			n = n+1;
+			a = realloc(a, (n)* sizeof(int *));
+			a[n-1] = size;
 	}
+	qsort(a, n, sizeof(int), cmp);
+    	for(i = 0 ; i < n; i++) 
+	{
+        	printf("%d\n", a[i]);
+    	}
 	return 0;         
 }
 
 int main(int argc, char *argv[]) 
 {
+	int i;
 	if (argc == 2) 
 	{
-		type = argv[1];
 		ftw(argv[1], check, 50);
 	}  
 	return 0;
 }
+
