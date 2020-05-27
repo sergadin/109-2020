d7ff91a1d58b28d42450d1db38db17606c473236
KandaurovaVasilisa <magnit6520@gmail.com>
2020-05-27 18:27:28 +0300

    ))

--- Kandaurova_VB/zachet1/file.h
+++ Kandaurova_VB/zachet1/file.h
@@ -1,5 +1,4 @@
-#define EL(m, n, i, j) \
-	((m)[(n) * (i) + j])
-
 char *readstring(FILE *input);
 int find(char **matrix, int n, int m);
+void fun(char **matrix, int n, int m, int k);
+void print(char **matrix, int n, int m, FILE *f);
