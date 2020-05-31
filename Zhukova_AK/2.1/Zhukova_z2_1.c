7503e2f62a8e6a76342045d725dcb0627440a495
Жукова Анастасия <nastia02072016@gmail.com>
2020-05-31 20:47:19 +0000

    Решение задачи 2.1

---
+++ Zhukova_AK/2.1/Zhukova_z2_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "Zhukova_z1_2.h"
+//эта фунция вычисляет интеграл на отрезке
+//по составной формуле прямоугольников с s отрезками
+//условия на функцию func:
+//она должна быть определена на всём отрезке
+double findint (double( * func) (double ), double a, double b, int s)
+{
+
+	double eps;
+        double intf = 0; //интеграл функции func
+	double x;
+
+	x = a;
+	eps = (b - a)/s; //шаг
+
+//	printf ("eps=%f a=%f b=%f\n", eps, a, b);
+
+        while ((int)((b - x)/eps) > 0)
+        {
+
+                intf = (func(x))*eps + intf; //вычисление площади прямоугольника
+                x = x + eps;
+
+//              printf ("intf=%f  x=%f\n", intf, x);
+        }
+
+        return intf;
+
+}
