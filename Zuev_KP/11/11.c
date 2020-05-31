fec72ca293c7c4839f5d994d664d32d482fa1c5a
ki-zuev <kizuev02@gmail.com>
2020-05-31 22:33:29 +0300

    Решение задачи 11

--- Zuev_KP/11/11.c
+++ Zuev_KP/11/11.c
@@ -4,6 +4,51 @@
 #include "11.h"
 static double simp(double a, double b, double ep, RRFUN ffunc);
 
+double delenie(double a, double b, double ep, RRFUN1 func, ErrorCode *error_code)
+{
+	double kon1 = a, ser = 0, kon2 = b, res1 = 0, ser1 = 0, res2 = 0;
+	double dlina = b - a;
+	ser = (a + b)/2;
+	res1 = (*func)(kon1);
+	res2 = (*func)(kon2);
+	*error_code = SF_OK;
+	if(res1*res2 >= 0) 
+	{
+		*error_code = SF_ERROR;
+		return 0;
+	}
+	while(dlina > ep)
+	{
+		ser = (kon1 + kon2)/2;
+		ser1 = (*func)(ser);
+		if(ser1*res1 <= 0)
+		{
+			kon2 = ser;
+			res2 = ser1;
+		}
+		if (ser1*res1 > 0)
+		{
+			kon1 = ser;
+			res1 = ser1;
+		}
+		dlina = kon2 - kon1;
+	}
+	return (kon1 + kon2)/2;
+}
+
+double integrate(double a, double b, double ep, RRFUN ffunc)
+{
+	int n = 2;
+	double i1 = simp(a, b, n, ffunc), i2 = simp(a, b, 2*n, ffunc);
+	while ((n > 0) && (modul(i2 - i1) > ep) && (n < 67108864))
+	{                                              
+		i1 = simp(a, b, n, ffunc);
+		n = 2*n;
+		i2 = simp(a, b, n, ffunc);
+	}
+	return i2;
+}
+
 static double simp(double a, double b, double n, RRFUN ffunc)
 {
 	int i;
@@ -21,19 +66,6 @@ static double simp(double a, double b, double n, RRFUN ffunc)
 	}
 	res = (h/3)*(2*res1 + 4*res2 + (*ffunc)(a, b) + (*ffunc)(b, b));
 	return res;
-}
-
-double integrate(double a, double b, double ep, RRFUN ffunc)
-{
-	int n = 2;
-	double i1 = simp(a, b, n, ffunc), i2 = simp(a, b, 2*n, ffunc);
-	while ((n > 0) && (modul(i2 - i1) > ep) && (n < 67108864))
-	{                                              
-		i1 = simp(a, b, n, ffunc);
-		n = 2*n;
-		i2 = simp(a, b, n, ffunc);
-	}
-	return i2;
 }
 
 double modul(double x)
--- Zuev_KP/11/11.h
+++ Zuev_KP/11/11.h
@@ -1,5 +1,7 @@
 typedef double(*RRFUN)(double t, double x);
+typedef double(*RRFUN1)(double b);
+typedef enum {SF_OK=0, SF_ERROR} ErrorCode;
 
 double modul(double x);
 double integrate(double a, double b, double n, RRFUN ffunc);
-
+double delenie(double a, double b, double ep, RRFUN1 func, ErrorCode *error_code);
