3660070b70fd497468f2eec239bcc320c33889f0
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-06-01 17:22:46 +0300

    Update test.c

--- Markova_MA/z/test.c
+++ Markova_MA/z/test.c
@@ -27,11 +27,21 @@ char *read_string(FILE *in) {
     } 
     return NULL;
 }
-
+char *cop(const char *f, char *t)
+{
+	int len;
+	len = slen(f);
+	for (int i = 0; i <= len; i++)
+	{
+		t[i] = f[i];
+	}
+	return t;
+}
 //int eq(char *a, char *b)
 
 int main() {
 	char *str, *w = NULL;
+	char temp1;
 	int len, i = 0, word = 0;
 	FILE *inp, *out;
     if ((inp = fopen("input.txt", "r")) == NULL) {
@@ -44,26 +54,32 @@ int main() {
         return 0;
     }
 	
-	while (str = read_string(inp)) 
-	{
-		if (str[0] == '\n') 
-			printf("0");
-		len = strlen(str);
-		w = str[0];
-		for(i = 1;i < len;i++)
+	str = read_string(inp)) 
+	if (str[0] == '\n' || strlen(str) == 1)
+	{			
+		printf("0");
+		return 0;
+	}
+	len = strlen(str);
+	w = malloc(len);
+	w = cop(str, w);
+	//for(i = 1;i < len - 1;i++)
+	//{
+		temp1 = str[0];
+		/*for(j = 0; j < len; j++)
 		{
-			if(str[i] == 'A'+ i)
-			{
-				w = str[i];
-			}
-		}
+			temp2 = 
+			str[j + 1] = str[i];
+			tmp = 
+		}*/
+	//}
 		//i = 2;
 		//m = str[i];
 		//putchar(str[i]);
-		//printf("%s\n", m);
+		printf("%c\n", temp1);
 		printf("%s\n", w);
 		//printf("\n");
 		free(str);
-	}
+
 	return 0;
 }
