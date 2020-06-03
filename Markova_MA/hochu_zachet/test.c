1106aab19a6ca1d601a351299a327006ceaba8cd
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-06-03 22:43:03 +0300

    Update test.c

--- Markova_MA/hochu_zachet/test.c
+++ Markova_MA/hochu_zachet/test.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
+#define T 1000
  /**
     ** Позиция (индекс) минимального элемента, значение которого больше 
 	**среднего арифметического элементов строки, минимальна (в других строках такой элемент стоит правее)
@@ -20,6 +22,7 @@
     **   Элементы массива a не изменяют свой порядок.
     **
     */
+typedef enum { INT_OK = 0, INT_NOT, INT_ND } ErrorCode;
 double alpha;
 double i (double (*f)(double), double a, double b, int n);
 double i (double (*f)(double), double a, double b, int n)
@@ -33,8 +36,8 @@ double i (double (*f)(double), double a, double b, int n)
 		}
 	return (2*I2 + (*f)(a) + (*f)(b))*h/2;
 }
-double integral(double (*f)(double), double a, double b, double eps);
-double integral(double (*f)(double), double a, double b, double eps)
+double integral(double (*f)(double), double a, double b, double eps, ErrorCode *perr);
+double integral(double (*f)(double), double a, double b, double eps, ErrorCode *perr)
 {
 	double I1 = 0, I2 = 0, state;
 	int check = 0, n = 128;
@@ -46,7 +49,7 @@ double integral(double (*f)(double), double a, double b, double eps)
 	I1 = i(f, a, b, n);
 	I2 = i(f, a, b, 2*n);
 	n *= 2;
-	while(ab(I1 - I2) > eps && check < T)
+	while(fabs(I1 - I2) > eps && check < T)
 	{
 		check++;
 		n *= 2;
@@ -62,20 +65,20 @@ double integral(double (*f)(double), double a, double b, double eps)
 	*perr = INT_OK;
 	return I2;
 }
-double root(double (*f)(double), double a, double b, double eps);	
-double root(double (*f)(double), double a, double b, double eps)
+double root(double (*f)(double, ErrorCode), double a, double b, double eps);	
+double root(double (*f)(double, ErrorCode), double a, double b, double eps)
 {
 	double m = (a + b)/2;
 	if(perr != NULL)
 	{
         *perr = ROOT_OK;
-		if((*f)(a) * (*f)(b) > 0)
+		if((*f)(a, perr) * (*f)(b, perr) > 0)
 		{
 			*perr = ROOT_NOT;
 			return m; 
 		}
 	}
-	if((*f)(a) > 0)
+	if((*f)(a, perr) > 0)
 	{
 		double temp;
 		temp = a;
@@ -84,13 +87,13 @@ double root(double (*f)(double), double a, double b, double eps)
 	}
 	while(fabs(b - a) < eps)
 	{
-		if((*f)(m) < 0)
+		if((*f)(m, perr) < 0)
 		{
-			a = (*f)(m);
+			a = (*f)(m, perr);
 		}
 		else
 		{
-			b = (*f)(m);
+			b = (*f)(m, perr);
 		}
 		m = (a + b)/2;
 	}
@@ -101,14 +104,15 @@ double f (double x)
 {
 	return sqrt(x + 1/(x*x + 1));
 }
-double f1 (double x);
-double f1 (double x)
+double f1 (double x, perr);
+double f1 (double x, perr)
 {
 	return integral(f, 1, x) - alpha*x;
 }
 int main() {
-	int a = 2, b = 10,;
+	int a = 2, b = 10;
 	double res = 0;
+	ErrorCode perr;
 	//FILE *inp;
 	/*if (!(inp = fopen("input.txt","r")) )
 	{
@@ -133,8 +137,9 @@ int main() {
 		return -1; 
 	}*/
 	//printf("kyky\n");
-	res = root(f1, a, b, m, eps);
-	printf("%lf\n", res);
+	res = root(f1, a, b, m, eps, perr);
+	if (perr == INT_OK)
+		printf("%lf\n", res);
 	//fclose(outp);
 	return 0;
 }
