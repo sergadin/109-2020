6a7ad2094487d8e97773e255c4f9006ce1423e6f
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-06-03 22:48:37 +0300

    Update test.c

--- Markova_MA/hochu_zachet/test.c
+++ Markova_MA/hochu_zachet/test.c
@@ -22,7 +22,7 @@
     **   Элементы массива a не изменяют свой порядок.
     **
     */
-typedef enum { INT_OK = 0, INT_NOT, INT_ND } ErrorCode;
+
 double alpha;
 double i (double (*f)(double), double a, double b, int n);
 double i (double (*f)(double), double a, double b, int n)
@@ -36,8 +36,8 @@ double i (double (*f)(double), double a, double b, int n)
 		}
 	return (2*I2 + (*f)(a) + (*f)(b))*h/2;
 }
-double integral(double (*f)(double), double a, double b, double eps, ErrorCode *perr);
-double integral(double (*f)(double), double a, double b, double eps, ErrorCode *perr)
+double integral(double (*f)(double), double a, double b, double eps);
+double integral(double (*f)(double), double a, double b, double eps)
 {
 	double I1 = 0, I2 = 0, state;
 	int check = 0, n = 128;
@@ -65,20 +65,15 @@ double integral(double (*f)(double), double a, double b, double eps, ErrorCode *
 	*perr = INT_OK;
 	return I2;
 }
-double root(double (*f)(double, ErrorCode), double a, double b, double eps);	
-double root(double (*f)(double, ErrorCode), double a, double b, double eps)
+double root(double (*f)(double), double a, double b, double eps);	
+double root(double (*f)(double), double a, double b, double eps, )
 {
 	double m = (a + b)/2;
-	if(perr != NULL)
+	if((*f)(a) * (*f)(b) > 0)
 	{
-        *perr = ROOT_OK;
-		if((*f)(a, perr) * (*f)(b, perr) > 0)
-		{
-			*perr = ROOT_NOT;
-			return m; 
-		}
+		return m; 
 	}
-	if((*f)(a, perr) > 0)
+	if((*f)(a) > 0)
 	{
 		double temp;
 		temp = a;
@@ -87,13 +82,13 @@ double root(double (*f)(double, ErrorCode), double a, double b, double eps)
 	}
 	while(fabs(b - a) < eps)
 	{
-		if((*f)(m, perr) < 0)
+		if((*f)(m) < 0)
 		{
-			a = (*f)(m, perr);
+			a = (*f)(m);
 		}
 		else
 		{
-			b = (*f)(m, perr);
+			b = (*f)(m);
 		}
 		m = (a + b)/2;
 	}
@@ -104,38 +99,18 @@ double f (double x)
 {
 	return sqrt(x + 1/(x*x + 1));
 }
-double f1 (double x, perr);
-double f1 (double x, perr)
+double f1 (double x);
+double f1 (double x)
 {
 	return integral(f, 1, x) - alpha*x;
 }
 int main() {
 	int a = 2, b = 10;
 	double res = 0;
-	ErrorCode perr;
-	//FILE *inp;
-	/*if (!(inp = fopen("input.txt","r")) )
-	{
-		printf("ERROR\n");
-		return -1;
-	}*/
-	/*if ((outp = fopen("output.txt", "w")) == NULL) 
-	{
-        printf("ERROR\n");
-        fclose(inp);
-        return -1;
-    }*/
-	if (scanf("%d", &alpha) != 1) {
-		fclose(inp);
+	if (scanf("%lf", &alpha) != 1) {
 		//err = ERROR_READ;
 		return -1;
-		//break;
 	}
-	/*if (err != SUCCESS) {
-		ERROR_RE(err);
-		free(a);
-		return -1; 
-	}*/
 	//printf("kyky\n");
 	res = root(f1, a, b, m, eps, perr);
 	if (perr == INT_OK)
