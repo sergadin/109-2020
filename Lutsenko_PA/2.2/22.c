cc01a7cb928879365210edc4ede7b564312df5a1
lutsenkoPA <63011581+lutsenkoPA@users.noreply.github.com>
2020-06-05 17:11:51 +0300

    почти

--- Lutsenko_PA/2.2/22.c
+++ Lutsenko_PA/2.2/22.c
@@ -7,18 +7,44 @@
 
 
 
-double  sum (double x1,double x2,double n,RRFUN f)
+double  sum (double x1,double x2,double eps, int n,RRFUN f)
 {         
-           double h,result;
-          h = fabs(x2 - x1)/(n);
-          int i;
-   result += ((*f)(x1) + (*f)(x2)) * h / 2;
-   for (i = 1; i < n; ++i)
-       {
-      result += f(x1 + i * h) * h;
-       }
-   return result;
+   double h,prev =0 ,curr = 0;
+   h = fabs(x2 - x1)/n;
           
+   prev += ((*f)(x1) + (*f)(x2)) * h / 2; //посчитала с разбиением h
+   for (int i = 1; i < n; ++i)
+   {
+      prev += f(x1 + i * h) * h;
+   }
+    
+    h = h/2;
+    
+    curr += ((*f)(x1) + (*f)(x2)) * h / 2; //посчитала с разбиением h/2
+    for (int i = 1; i < n; ++i)
+    {
+       curr += f(x1 + i * h) * h;
+    }
+          
+    
+    while( fabs(prev - curr)/3 >= eps)
+    {  //уменьшаю разбиение если нет нужной точности
+        h /= 2;
+        n *= 2;
+        prev = curr;
+        curr = 0;
+        
+        curr += ((*f)(x1) + (*f)(x2)) * h / 2;
+        for (int i = 1; i < n; ++i)
+        {
+           curr += f(x1 + i * h) * h;
+        }
+    
+        
+
+    }
+    
+    return curr;
 }
 
 
