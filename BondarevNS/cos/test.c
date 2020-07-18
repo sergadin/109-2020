0a90f00a28899098770f81eea16dcc726c7d85c7
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-07-19 01:19:46 +0300

    ee

--- BondarevNS/cos/test.c
+++ BondarevNS/cos/test.c
@@ -8,12 +8,12 @@ int main(void)
 	int i;
 	double r_res;
 	double eps = 0.5;
-	double result;
+	double res;
 	double x_mas[] = {pi/2 , 0, pi/3, pi/4 };
 
 		for(i = 0; i < 4; i++){
 		r_res = cos(x_mas[i]);	
-		result = Taylor_cos(x_mas[i], eps);
+		res = Taylor(x_mas[i], eps);
 		printf("правильный ответ:\n%lf\nответ:\n%lf\n eps: %lf\n", r_res, result, eps);	
 		printf("меньше?=%d\n", fabs(r_res - result) < eps);
 		}
