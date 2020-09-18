c2b724debcc7efa51d3cc4d60e1f6cf82f5c64ce
Saralpov <62976588+Saralpov@users.noreply.github.com>
2020-09-18 19:02:12 +0300

    zachet1

---
+++ Saralpov_IA/zachet1/task.h
@@ -0,0 +1,8 @@
+#define EL(m, n_cols, row, col) \
+	((m)[(n_cols) * (row) + col])
+
+int find_row(double *data, int n_rows, int n_cols);//Функция, чтобы находить максимальный элемент матрицы
+
+void printmat(double *data, int n_rows, int n_cols,int t);//Функция чтобы печатать матрицу
+
+
