bf4acbbc72c0fa97af35e4b85f2f09eb4b187fbb
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-09-18 21:41:35 +0300

    блокнот

--- BondarevNS/zachet3/process.c
+++ BondarevNS/zachet3/process.c
@@ -3,22 +3,59 @@
 
 #include "process.h"
 
-#define MATR(n, i, j) matrix[(n) * (i) + (j)]
+#define MATR(m, i, j) matrix[(m) * (i) + (j)]
 
 void read_matrix(double *matrix, int n, int m, FILE *fin, ErrorCode *materr)
 {
-	*materr = MATRIX_OK;
-	
-		for(int i = 0; i< m * n; i++)
+		*materr = MATRIX_OK;
+		for (int i = 0; i < m * n; i++)
+	{
+	if ((fscanf(fin, "%lf", &matrix[i]) != 1))
+	{
+		*materr = DATAERR;
+		break;
+	}
+	}
+
+}
+void founding_and_transformation(double *matrix, int n, int m)
+{
+	int keyrow = -1;
+	double strsum, minsum = 0, cursum = 0, memory;
+	for (int i = 0; i < n; i++)
+	{
+		strsum = 0;
+		cursum = 0;
+		for (int j = 0; j < m; j++)
 		{
-			if((fscanf(fin, "%lf", &matrix[i]) != 1))
-			{
-				*materr = DATAERR;
-				break;
-			}
+			strsum += MATR(m, i, j);
 		}
-	
-	
+	for (int j = 0; j < m; j++)
+	{
+	if (MATR(m, i, j) > strsum/m)
+		{
+			cursum += MATR(m, i, j);
+		}
+	}
+	if ((cursum < minsum) || (i == 0))
+	{
+		minsum = cursum;
+		keyrow = i;
+	}
+}
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < m - i - 1; j++)
+		{
+		if(MATR(m, keyrow, j) <= MATR(m, keyrow, (j + 1)))
+			{
+			memory = MATR(m, keyrow, (j + 1));
+			MATR(m, keyrow, (j + 1)) = MATR(m, keyrow, j);
+			MATR(m, keyrow, j) = memory;
+			}
+		}	
+	}	
+
 }
 
 void print_matrix(double *matrix, int n, int m, FILE *fout)
@@ -28,55 +65,14 @@ void print_matrix(double *matrix, int n, int m, FILE *fout)
 		for (int j = 0; j < m; j++)
 		{
 			fprintf(fout, "%lf", MATR(m, i, j));
-			if(j != m -1)
+			if (j != m - 1)
 			{
 				fprintf(fout, " ");
 			}
 		}
-			if (i != n - 1)
-			{
-				fprintf (fout, "\n");		
-			}
-	}
-}
-
-void foundandtrans(double *matrix, int n, int m)
-{
-	int kr = -1, strcount = 0;
-	double strsum, minsum = 0, cursum = 0, memory;
-	for (int i = 0; i<n; i++)
-	{
-		strsum = 0;
-		cursum = 0;
-		for (int j = 0; j < m; j++)
+		if (i != n - 1)
 		{
-			strsum += MATR(m, i, j);
-		}
-		for (int j = 0; j<m; j++)
-		{
-			if (MATR(m, i, j)>strsum/m)
-			{
-				cursum += MATR(m, i, j);
-				
-			}
-		}
-		if ((i ==0) || (cursum < minsum))
-		{
-			minsum = cursum;
-			kr = i;
-		}
-	
-	}
-	for(int i=0; i<m; i++)
-	{
-		for (int j =0; j< m - i - 1; j++)
-		{
-			if(MATR(m, kr, j) <= MATR(m, kr, (j + 1)))
-			{
-				memory = MATR(m, kr, (j+1));
-				MATR(m, kr, (j+1)) = MATR(m, kr, j);
-				MATR(m, kr, j) = memory;
-			}
+			fprintf (fout, "\n");
 		}
 	}
 }
--- BondarevNS/zachet3/process.h
+++ BondarevNS/zachet3/process.h
@@ -1,5 +1,5 @@
 typedef enum {MATRIX_OK = 0, DATAERR} ErrorCode;
 
-void read_matrix(double *matrix, int n,int m, FILE *fin, ErrorCode *materr);
-void foundandtrans(double *matrix, int n, int m);
+void read_matrix(double *matrix, int n, int m, FILE *fin, ErrorCode *materr);
+void founding_and_transformation(double *matrix, int n, int m);
 void print_matrix(double *matrix, int n, int m, FILE *fout);
