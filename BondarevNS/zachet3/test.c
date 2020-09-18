19c5f3c4f9f3db242fd38f7b4742fcb80117ecd3
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-09-18 21:31:08 +0300

    Update test.c

--- BondarevNS/zachet3/test.c
+++ BondarevNS/zachet3/test.c
@@ -9,19 +9,18 @@ int main(void)
 	int n, m;
 	double *matrix;
 	ErrorCode materr;
-	
 	if ((fin = fopen("input.txt", "r")) == NULL)
-	{
-		printf("Input file error");
-		return -1;
-	}
+		{
+			printf("Input file error");
+			return -1;
+		}
 	if ((fout = fopen("output.txt", "w")) == NULL)
 	{
 		printf("Output file error");
 		fclose(fin);
 		return -1;
 	}
-	if ((fscanf(fin, "%d %d", &n, &m) != 2) || (n<=1) || (m<=1))
+	if ((fscanf(fin, "%d %d", &n, &m) != 2) || (n <= 1) || (m <= 1))
 	{
 		fprintf(fout, "Invalid data");
 		fclose(fin);
@@ -30,25 +29,24 @@ int main(void)
 	}
 	if ((matrix = (double *) malloc(n * m * sizeof(double))) == NULL)
 	{
-		fprintf(fout, "Allocation Error");
+		fprintf(fout, "Allocation error");
 		fclose(fin);
 		fclose(fout);
 		return -1;
 	}
 	read_matrix(matrix, n, m, fin, &materr);
 	if (materr == DATAERR)
-	{	
-			fprintf(fout, "Invalid dATA");
-			fclose(fin);
-			fclose(fout);
-			free(matrix);
-			return -1;		
-	}
-	printf("Input file error");
-	foundandtrans(matrix, n, m);
+		{
+		fprintf(fout, "Invalid data");
+		fclose(fin);
+		fclose(fout);
+		free(matrix);
+		return -1;
+		}
+	founding_and_transformation(matrix, n, m);
 	print_matrix(matrix, n, m, fout);
-	fclose(fout);
 	fclose(fin);
+	fclose(fout);
 	free(matrix);
 	return 0;
 }
