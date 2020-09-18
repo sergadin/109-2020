d3cd7a4454de00cb13f316ded4fd70ddffbd4a21
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-09-18 20:22:30 +0300

    zachet

---
+++ BondarevNS/zachet3/test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "process.h"
+
+int main(void)
+{
+	FILE *fin, *fout;
+	int n, m;
+	double *matrix;
+	ErrorCode materr;
+	if ((fin = fopen("input.txt", "r")) == NULL)
+	{
+		printf("Input file error");
+		return -1;
+	}
+	if ((fout = fopen("output.txt", "r")) == NULL)
+	{
+		printf("Output file error");
+		fclose(fin);
+		return -1;
+	}
+	if (fscanf(fin, "%d %d", &n, &m) != 2) || ((n<=1) || (m<=1))
+	{
+		fprintf(fout, "Invalid data");
+		fclose(fin);
+		fclose(fout);
+		return -1;
+	}
+	if ((matrix = (double *) malloc(n * m * sizeof(double))) == NULL)
+	{
+		fprintf(fout, "Allocation Error");
+		fclose(fin);
+		fclose(fout);
+		return -1;
+	}
+	read_matrix(matrix, n, m, fin, &materr)
+	if (materr == DATAERR)
+	{	
+			fprintf(fout, "Invalid dATA");
+			fclose(fin);
+			fclose(fout);
+			free(matrix);
+			return -1;		
+	}
+	foundandtrans(matrix, n, m);
+	print_matrix(matrix, n, m, fout);
+	fclose(fout);
+	fclose(fin);
+	free(matrix);
+	return 0;
+}
