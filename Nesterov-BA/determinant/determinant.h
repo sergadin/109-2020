6e0409c52726bb3f0f40f2485f403b3281efca5c
Nesterov-BA <nesterov.boris123@gmail.com>
2020-09-30 11:19:57 +0300

    Решение матриц
    .

---
+++ Nesterov-BA/determinant/determinant.h
@@ -0,0 +1,6 @@
+#define eps 0.0000001
+
+double ABS(double x);
+void SwapRows(double *matrix, int n, int row1, int row2);
+void PlusRow(double *matrix, int n, int row1, int row2, double c);
+double Det(double *matrix, int n);
