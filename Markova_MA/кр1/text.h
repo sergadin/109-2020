3bb7a9604bbe31e7899988fbc985357524102bd5
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-10-01 01:59:21 +0300

    w

--- Markova_MA/кр1/text.h
+++ Markova_MA/кр1/text.h
@@ -4,26 +4,6 @@
 #include <math.h>
 using namespace std;
 
-class queue {
-    private:
-        int n;
-        int size;
-        double *array;
-        
-    public:
-        queue();
-        queue(int new_size);
-        queue operator +(const queue &other);
-        void print() const;
-        int checkVoid() const;
-        int checkfull() const;
-        int CheckSize() const;
-        void resize(int new_size);
-        void push(double n);
-        double pop();
-        void clean();
-        ~queue();
-};
 
 
 class UserException {
