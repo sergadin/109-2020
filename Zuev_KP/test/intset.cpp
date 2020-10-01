b5a529c4e5b9148ba65c66f0500f433605ffbdb0
ki-zuev <kizuev02@gmail.com>
2020-10-01 13:35:55 +0300

    Контрольная

--- Zuev_KP/test/intset.cpp
+++ Zuev_KP/test/intset.cpp
@@ -35,42 +35,42 @@ void IntSet::exc(int item)
         if ((item > b_) || (item < a_))
                 throw IntSetError(-1, std::string( "нет"));
         for(int k = 0; k < n_; k++)
-                if elements_[k] = righ[;
-        n_ = right.n;
-        elements_[n_++] = item;
-}
+                if elements_[k]= righ[
+        n_ = right.n
+        elements_[n_++] = item
+
 
 IntSet & IntSet::operator=(const IntSet & right)
-{
+
         if(this == &right)
-        {
-                return *this;
-        }
-        delete[] elements_;
-        elements_ = new int[size_ = right.size_];
+        
+                return *this
+       
+        delete[] elements_
+        elements_ = new int[size_ = right.size_]
         for(int k = 0; k < size_; k++)
-                elements_[k] = right.elements_[k];
-        n_ = right.n_;
-        return *this;
-}
+                elements_[k] = right.elements_[k]
+        n_ = right.n_
+        return *this
+
 
 IntSet & operator*=(IntSet &left, const IntSet& right)
-{
-        return left;
-}
+
+        return left
+
 
 IntSet operator* (const IntSet &left, const IntSet& right)
-{
 
-}
+
+
 
 std::ostream & operator<<(std::ostream &os, const IntSet &q)
-{
-        os << "IntSet of size " << q.size_ << " with " << q.n_ << " items:\n";
+
+        os << "IntSet of size " << q.size_ << " with " << q.n_ << " items:\n"
         for(int k = 0; k < q.n_; k++)
-        {
-                os << "\t" << q.elements_[k] << "\n";
-        }
-        return os;
-}
+        
+                os << "\t" << q.elements_[k] << "\n"
+        
+        return os
+
 
