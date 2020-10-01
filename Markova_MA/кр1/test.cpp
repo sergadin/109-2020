5424765fa18a0eca17b56aba52ea1628176f10e2
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-10-01 16:45:08 +0300

    Update test.cpp

--- Markova_MA/кр1/test.cpp
+++ Markova_MA/кр1/test.cpp
@@ -5,10 +5,12 @@
 int main()
 {
 	ZZ a(3,6), b(4,9);
+	int temp = 5;
 	cout << "a"<< endl;
 	//a.print();
 	//b.print();
-	a.push_(5);
+	a.push_(temp);
+	cout << "0";
 	//a.print();
 	/*if(b.isEmpty())
 	{
