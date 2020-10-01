c6b8b6b41b651f92e48148873b2f632ecbbaff6d
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-10-01 11:33:42 +0300

    Update intset.cpp

--- Gorev_VA/kr/intset.cpp
+++ Gorev_VA/kr/intset.cpp
@@ -7,5 +7,10 @@ intset::intset(int maxval, int minval)
 	size_ = 0;
 	maxval_ = maxval;
 	minval_ = minval;
-	//std::cout << maxval_ << ", " << minval_ << endl;
+	std::cout << maxval_ << ", " << minval_ << endl;
+}
+
+intset::intset(intset& IS)
+{
+	return *this;
 }
