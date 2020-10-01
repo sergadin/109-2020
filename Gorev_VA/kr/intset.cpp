eacf766283aca2aec3fe3227dfe7000ea89a2b3f
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-10-01 11:23:31 +0300

    Add files via upload

---
+++ Gorev_VA/kr/intset.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "intset.h"
+
+intset::intset(int maxval, int minval)
+{
+	intset;
+	size_ = 0;
+	maxval_ = maxval;
+	minval_ = minval;
+	std::cout << maxval_ << ", " << minval_ << endl;
+}
---
+++ Gorev_VA/kr/intset.h
@@ -0,0 +1,12 @@
+class intset
+{
+	private:
+		int *ar_;
+		int size_;
+		int maxval_;
+		int min_val;
+	public:
+		intset(int maxval, int minval);
+		~intset();
+		intset(intset IS);
+};
