d8c8511e252f3042f25aec0bad73c268ea2494fc
Makar Chizhik <makar.chizhik@gmail.com>
2020-10-01 13:51:47 +0300

    Некоторые тесты

--- Chizhik_MA/year2/sets/test.cpp
+++ Chizhik_MA/year2/sets/test.cpp
@@ -4,12 +4,33 @@
 using namespace std;
 
 int main() {
+	//First test
 	try {
-		IntSet big(0, 100);
 		IntSet small(-100, -200);
 	} catch(IntSetException& e) {
 		cerr << e.message() << endl;
-		return -1;
   	}
+	//Second test
+	try {
+		IntSet A(-200, 0);
+		A.add_element(5);
+	} catch(IntSetException& e) {
+		cerr << e.message() << endl;
+	}
+	//Third test
+	try {
+		IntSet B(0, 4);
+		B.min();
+		B.max();
+	} catch(IntSetException& e) {
+		cerr << e.message() << endl;
+	}
+	//Fourth test
+	IntSet A = IntSet(0, 50);
+	for (int i = 0; i < 45; i += 2) {
+		A.add_element(i);
+	}
+	A = A;
+	cout << "Minimum of A is " << A.min() << " and maximum of A is " << A.max() << endl;
 	return 0;
 }
