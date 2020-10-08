08da2a17cae2e30c88d82af2f546a0496fe096a1
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-10-08 14:56:24 +0300

    Add files via upload

--- Gorev_VA/kr_08_10/test.cpp
+++ Gorev_VA/kr_08_10/test.cpp
@@ -5,16 +5,15 @@
 int main(void)
 {
     Clock C(2, 0);
-    Clock CC(C);
 
-    /*try
+    try
     {
         nextClock(nextClock(C), C);
     }
     catch (ClockError &err)
     {
         std::cout << "EXEPTION: " << err.get_reason() << "\n";
-    }*/
+    }
 
     return 0;
 }
