cfb14fc302829382dbd8c0877211e5c6674105ed
KIr1200 <58104464+KIr1200@users.noreply.github.com>
2020-10-08 16:22:19 +0300

    kr

--- Korban_KD/3semester/kr2/test.cpp
+++ Korban_KD/3semester/kr2/test.cpp
@@ -6,20 +6,30 @@ using namespace std;
 int main()
 {
     vector_clock A(2, 0), B(2, 1);
-    B.local_add(2);
+    B.local_add(3);
     A.local_add();
     cout << A << endl;
-    cout << A << endl;
+    cout << B << endl;
     try
     {
         A.receve(B);
         cout << A << endl;
         cout << (A < B) << endl;
+		A.local_add();
+		cout << A << endl;
+		cout << (A < B) << endl;
         
     }
 	catch(vector_clock_Exception & err)
 	{
 		cout << err.message() << endl;
 	}
+	
+	for(int i = 0; i < 1000000000000000; i++)
+	{
+		i -= 2;
+	}
+	
+	
     return 0;
 }
