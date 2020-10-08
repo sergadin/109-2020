64a53a1d1131906cb852e14d847d879722fef668
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-10-08 14:36:01 +0300

    Add files via upload

--- Gorev_VA/kr_08_10/clock.cpp
+++ Gorev_VA/kr_08_10/clock.cpp
@@ -21,7 +21,7 @@ Clock nextClock(const Clock &prevClock, const Clock &sendingClock)
 	Clock C(prevClock.N_, prevClock.num_);
 	C.mark_ = new int[prevClock.N_];
 	for (int i = 0; i < C.N_; i++)
-		C.mark_[i] = prevClock.mark[i];
+		C.mark_[i] = prevClock.mark_[i];
 	C.mark_[C.num_]++;
 
 	if (C.N_ != sendingClock.N_)
