503bcecc6744bc51f1f7021d123de779cdff6a1e
mariamrkv <62586292+mariamrkv@users.noreply.github.com>
2020-10-08 15:40:55 +0300

    Update test.cpp

--- Markova_MA/kr/test.cpp
+++ Markova_MA/kr/test.cpp
@@ -6,17 +6,16 @@ int main()
 {
 	ZZ a1(1,3), a2(1,3), a3(1,3), b1(2,3), b2(2,3), b3(2,3);
 	a1.name(1);
-	/*a2.name(2);
+	a2.name(2);
 	a3.name(3);
 	b1.name(1);
 	b2.name(2);
 	b3.name(3);
-	/*a2.message(b2);
-	*/
+	a2.message(b2);
 	a1.print();
-	/*a2.print();
+	a2.print();
 	a3.print();
-	*/
+	
 	
 	return 0;
 }
