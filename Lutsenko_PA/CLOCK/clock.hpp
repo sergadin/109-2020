09d9a9b8902f9f2dea7dca7960bbd2d183272065
lutsenkoPA <63011581+lutsenkoPA@users.noreply.github.com>
2020-10-08 15:27:21 +0300

    ...

---
+++ Lutsenko_PA/CLOCK/clock.hpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class VClockError
+{
+    
+private:
+    int code_;
+    std::string reason_;
+    
+public:
+    VClockError(int code, const std::string& reason)
+    {
+        code_ = code;
+        reason_ = reason;
+    }
+    
+    const std::string& get_reason() const { return reason_; }
+};
+  class VClock
+
+   {
+   private:
+       
+       int *coordinates_;
+       int n_;  // размерность
+       int k_;  //
+      
+       VClock () {}
+   public:
+       
+       VClock( int n_, int k_);
+       
+       ~VClock();
+       //печать координат
+       void PrintCoordinates();
+       
+       //Сравнение двух меток.
+       friend  void operator==( const VClock & left, const VClock & right); //Сравнение двух меток.
+       // Подпись локального события.
+       void Local();
+       // Подпись события, соответсвующего получению сообщения с меткой m
+       void Global(const VClock & m);
+    
+       
+    
+   };
