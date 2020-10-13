5add9a93de2cedc700262c5b2bb154ac374912a7
ki-zuev <kizuev02@gmail.com>
2020-10-13 21:31:26 +0300

    F

--- Zuev_KP/F/test.cpp
+++ Zuev_KP/F/test.cpp
@@ -7,24 +7,14 @@ int main()
 {
 	try
 	{
-		Vector q0(5, 0);
-		Vector q1(5, 1);
-		Vector q2(5, 2);
-		Vector q3(5, 3);
-		Vector q4(5, 4);
-		q0.loc();
-		q0.loc();
-		q0.loc();
-		q0.loc();
-		q4.rec(q0);
-		std::cout << q0 << endl;
+		Stack q0(5, 0);
+		Stack q1(5, 1);
+		Stack q2(5, 2);
+		Stack q3(5, 3);
+		Stack q4(5, 4);
 		std::cout << q4 << endl;
-		if (q0 <= q4)
-		{
-			cout << "true " << std::endl;
-		}
 	}
-	catch(VectorError &err)
+	catch(StackError &err)
 	{
 		std::cout << err.get_reason() << std::endl;
 	}
