f247d7aa7b1fefdd4d4d3bcd6eee49086fd2c137
Solovyeva-AK <adknm@yandex.ru>
2020-10-13 19:59:08 +0300

    Create stack.h

---
+++ Solovyeva-AK/2 курс 1 семестр/Задача F/stack.h
@@ -0,0 +1,41 @@
+#include <string>
+#include <cstdio>
+#include <math.h>
+using namespace std;
+
+typedef struct list{
+    list *next;
+    int data;
+} list;
+
+
+class stack {
+    private:
+        list *head;
+        
+    public:
+        stack();
+        stack(int elem);
+        stack(const stack &other);
+        stack
+        void operator =(const stack &other);
+        bool operator ==(const IntSet &other);
+        stack operator +(const stack &other);
+        void SortStack();//точно ли нужна функция сортировки для стека?
+        void print() const;
+        int checkVoid() const;
+        void push(int n);
+        double pop();
+        void clean();
+        ~stack();
+};
+
+class UserException {
+	private:
+		int code_;
+		string message_;
+	public:
+		UserException(int code, string message);
+		string message() const;
+		int code() const;
+};
