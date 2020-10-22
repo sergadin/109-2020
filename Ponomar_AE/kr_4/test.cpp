ec918abf0f9b4570017323955e9d71454f7351fc
ponomar4643 <kkkitya@yandex.ru>
2020-10-22 19:37:57 +0300

    .

--- Ponomar_AE/kr_4/test.cpp
+++ Ponomar_AE/kr_4/test.cpp
@@ -5,25 +5,19 @@ using namespace std;
 
 int main()
 {
+	try{
 	matrix matr1(2, 4);
 	matrix matr2(2, 4);
 	matrix matr3(2, 4);
 
 	for(int i = 1; i < 3; ++i)
 	{
-		matr1.change(i, i, 2);
-		matr1.change(i+2, i, 3);
-		matr1.change(i+4, i, 1);
-		matr1.change(i+6, i, 7);
-		matr2.change(1, i, 1);
-		matr2.change(1, i, 3);
-		matr2.change(1, i, 5);
-		matr2.change(1, i, 7);
-
+		matr1.change(i, i, 1);
+		matr1.change(i+2, i, 2);
+		matr1.change(i+4, i, 3);
+		matr1.change(i+6, i, 4);												
 	}
 
-	cout<<matr1.str()<<endl;
-	cout<<matr1.stolb()<<endl;
 	cout<<matr1;
 	cout<<matr2;
 
@@ -32,5 +26,11 @@ int main()
 
 	podmatrix matr4(&matr1,1,1,1,2);
 	cout << matr4.element(1,1)<<" "<< matr4.element(1,2) << endl;
+
+	}
+		catch(iskl &exc)
+	{
+		std::cout << exc.message() << std::endl;
+	}
 	return 0;
 }
