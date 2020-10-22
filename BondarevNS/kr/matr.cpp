892bc997702ac3cfb9cdfe5131987876bad74904
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-10-22 17:49:04 +0300

    kr

--- BondarevNS/kr/matr.cpp
+++ BondarevNS/kr/matr.cpp
@@ -129,11 +129,11 @@ podmatr::~podmatr()
 	m1 = 0;
 }
 
-double podmatr::get_element(int n, int m) const
+double podmatr::get_element(int n_, int m_) const
 {
-	if((n>n_)||(m>m_)||(n<1)||(m<1))
+	if((n_>n1)||(m_>m1)||(n_<1)||(m_<1))
 	{
 		throw Exception(3, "Incorrect element index");
 	}
-	return pod ->get_element(i+n-1, j+m-1);
+	return pod ->get_element(i+n_-1, j+m_-1);
 }
