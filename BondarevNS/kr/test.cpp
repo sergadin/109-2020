9333310a5190ba0738b7dc4f2efc1c44149a3504
BondarevNS <62707816+BondarevNS@users.noreply.github.com>
2020-10-22 17:21:05 +0300

    Update test.cpp

--- BondarevNS/kr/test.cpp
+++ BondarevNS/kr/test.cpp
@@ -7,11 +7,11 @@ int main()
 	matr A(2, 3);
 	matr B(2, 3);
 	matr C(2, 3);
-	for(int i = 1; i<; ++i)
+	for(int i = 1; i<3; ++i)
 	{
-		A.set_elem(i, i, 1);
-		A.set_elem(i+2, i, 2);
-		A.set_elem(i+4, i, 3);
+		A.set_element(i, i, 1);
+		A.set_element(i+2, i, 2);
+		A.set_element(i+4, i, 3);
 		
 	}
 	cout << A;
@@ -19,8 +19,8 @@ int main()
 	C = B+A;
 	cout << C;
 	cout << A+B+C;
-	submatrix D(&A, 1,1,2,2);
-	cout << D.get_elem(1,1) << " "<< D.get_elem(1,2) << endl;
-	cout << D.get_elem(2,1) << " "<< D.get_elem(2,2) << endl;
+	podmatr D(&A, 1,1,2,2);
+	cout << D.get_element(1,1) << " "<< D.get_element(1,2) << endl;
+	cout << D.get_element(2,1) << " "<< D.get_element(2,2) << endl;
 	return 0;
 }
