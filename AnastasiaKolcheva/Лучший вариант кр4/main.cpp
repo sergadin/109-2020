e26f474f9b609800d9a6ced8d7d56fce80ced095
AnastasiaKolcheva <66263182+AnastasiaKolcheva@users.noreply.github.com>
2020-10-23 21:57:08 +0300

    Add files via upload

---
+++ AnastasiaKolcheva/Лучший вариант кр4/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "matrix.h"
+
+void test();
+
+int main()
+{
+    test();
+    std::cout << "Press Enter to Continue";
+    std::cin.ignore();
+    return 0;
+}
+
+matrix_t<double> createSimpleMatrix()
+{
+    matrix_t<double> matrix = matrix_t<double>(7, 7);
+
+    std::cout << "rows: " << matrix.getRows() << std::endl;
+    std::cout << "columns: " << matrix.getCols() << std::endl;
+
+    for (int index = 0; index < matrix.getRows() * matrix.getCols(); ++index)
+    {
+        matrix.setElement(index / matrix.getCols(), index % matrix.getCols(), index);
+    }
+    std::cout << matrix << std::endl;
+    return matrix;
+}
+
+void test()
+{
+    matrix_t<double> matrix = createSimpleMatrix();
+
+    matrix_t<double> result = matrix + matrix;
+
+    std::cout << result << std::endl;;
+
+    std::cout << result.get(1, 5, 2, 2) << std::endl;
+    std::cout << result.get(1, 1, 2, 2) << std::endl;
+    {
+        std::cout << "result2 start" << std::endl;
+        matrix_t<double> result2 = result.get(1, 5, 2, 2) + result.get(1, 1, 2, 2);
+        std::cout << result2 << std::endl;
+        std::cout << "result2 end" << std::endl;
+    }
+    matrix_t<double> result1 = result;
+    std::cout << result1 << std::endl;
+}
