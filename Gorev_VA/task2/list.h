90d241b6e787ec3196ae3a0e82d9c03a48969e2d
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-11-02 17:36:05 +0300

    Add files via upload

--- Gorev_VA/task2/list.h
+++ Gorev_VA/task2/list.h
@@ -19,18 +19,18 @@ class list
 		~list()
 		{
 			val_ = 0;
-			//if (next_ != 0)
-			//	delete[] next_;
-			//if (prev_ != 0)
-			//	delete[] prev_;
+			if (next_ != 0)
+				delete[] next_;
+			if (prev_ != 0)
+				delete[] prev_;
 		}
-		/*list <T> put_item(const T item)
+		list <T> put_item(const T item)
 		{
 			next_ = new list <int>(item);
 			next_->prev_ = this;
 			next_->next_ = 0;
 			return *next_;
-		}*/
+		}
 
 };
 
