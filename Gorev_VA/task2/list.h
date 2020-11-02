fc783ab708ea66713b00fce84cbc14967f8d2eff
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-11-02 18:32:01 +0300

    Add files via upload

--- Gorev_VA/task2/list.h
+++ Gorev_VA/task2/list.h
@@ -27,7 +27,7 @@ class list
 		}*/
 		~list()
 		{
-			//cout << "NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n"
+			//std::cout << "NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n"
 			//deletelist(*this);
 			if (next_ != 0)
 				delete next_;
@@ -36,9 +36,9 @@ class list
 		}
 		list <T> put_item(const T item)
 		{
-			cout << "NO\n\n\n";
+			std::cout << "NO\n\n\n";
 			list *p = new list <T>(val_);
-			cout << "YES\n\n\n";
+			std::cout << "YES\n\n\n";
 			p->prev_ = prev_;
 			p->next_ = this;
 			if (prev_ != 0)
