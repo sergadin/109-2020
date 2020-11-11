db7b850670eb75cba4eee81b7c6c91d71d693bd1
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-11-11 16:32:07 +0300

    Это надо проверить

--- Gorev_VA/task2/list.cpp
+++ Gorev_VA/task2/list.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include "list.h"
+
 int list::add_item(const T item) // add item to the end of the list
 	// return 1 if item - first created element
 	// return 0 if item - not first created element
