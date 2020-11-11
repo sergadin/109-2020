5eeb5acbf2161ef5cdd873c5aaad10afbdaeff00
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-11-11 16:43:32 +0300

    Update list.cpp

--- Gorev_VA/task2/list.cpp
+++ Gorev_VA/task2/list.cpp
@@ -3,7 +3,7 @@
 
 template <class T>
 
-int list <T>::add_item(const T item) // add item to the end of the list
+int list::add_item(const T item) // add item to the end of the list
 	// return 1 if item - first created element
 	// return 0 if item - not first created element
 {
