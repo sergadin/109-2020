1ea04ebcc003591875743569bea9f0bf35952f84
Жукова Анастасия <nastia02072016@gmail.com>
2020-11-25 21:35:38 +0000

    B

--- Zhukova_AK/B/B_1.h
+++ Zhukova_AK/B/B_1.h
@@ -16,6 +16,21 @@ private:
 	ListItem *first = NULL;
         ListItem *last = NULL;
         ListItem *current = NULL;
+/*
+	ListItem (int n)
+	{
+        	for (int i=0;i<n;i++)
+        	{
+	                ListItem *current = new ListItem;
+                	current->next = NULL;
+        	        current->prev = NULL;
+	                current->data = NULL;
+
+                	go_next();
+        	}
+	}
+*/
+
 
 	ListItem *next (ListItem *element)
 	{
@@ -31,6 +46,8 @@ public:
 
 	void add_first_elem (T value);
         void add_last_elem (T value);
+
+	void change_elem(ListItem *item, T elem);
 
         void del_first_elem ();
         void del_last_elem ();
@@ -51,4 +68,5 @@ public:
 	list();
 	~list();
 
+//	list(int n);
 };
