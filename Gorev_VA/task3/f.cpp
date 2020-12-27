826108e43fd8fd2a90c906222f30768fe7a5084b
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-12-28 00:54:40 +0300

    Update f.cpp

--- Gorev_VA/task3/f.cpp
+++ Gorev_VA/task3/f.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 //using namespace std;
 
-const int M = 4;
+const int M = 2;
 
 template <class T>
 class slot
@@ -35,25 +35,51 @@ class node
 {
 private:
 	slot <T> *cur_;  // array of current slots
-	slot <T> *next_; // array of next slots
-	int n_;    // number of current slots
+	int n_;          // number of current slots
+	node <T> **next_node_;
+	node <T> *prev_node_;
 public:
 	node()
 	{
-		cur_ = new slot<T>[M];
-		next_ = new slot<T>[M + 1];
+		cur_ = new slot<T>[2 * M + 1];
+		next_node_ = 0;
+		prev_node_ = 0;
 		n_ = 0;
 	}
 	~node()
 	{
 		std::cout << "\"~node()\" occured\n";
-		delete[] cur_;
-		delete[] next_;
+		if (cur_ != 0)
+			delete[] cur_;
 		n_ = 0;
+		prev_node_ = 0;
+		if (next_node_ != 0)
+			delete[] next_node_;
 	}
-	int sth()
+	int add_slot(std::string key, T val)
 	{
-		return 0;
+		if (n_ >= (2 * M))
+			return 0;
+		cur_[n_].key_ = key;
+		cur_[n_].val_ = val;
+		n_++;
+		return 1;
+	}
+	void sth()
+	{
+		std::cout << cur_->key_ << " " << cur_->val_ << "\n"; 
+	}
+};
+
+template <class T>
+class B_tree
+{
+private:
+	node <T> *root_;
+public:
+	B_tree()
+	{
+		root_ = new node<T>;
 	}
 	
 };
@@ -61,6 +87,7 @@ public:
 int main(void)
 {
 	node <int> N;
+	N.add_slot("stroka", 4);
 	N.sth();
 	return 0;
 }
