6c5552d8648851d5267e9c23a8586d8b4ac0c214
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2020-12-28 19:22:15 +0300

    Update f.cpp

--- Gorev_VA/task3/f.cpp
+++ Gorev_VA/task3/f.cpp
@@ -33,12 +33,12 @@ public:
 template <class T>
 class node
 {
-private:
+public:
 	slot <T> *cur_;  // array of current slots
 	int n_;          // number of current slots
 	node <T> **next_node_;
 	node <T> *prev_node_;
-public:
+
 	node()
 	{
 		cur_ = new slot<T>[2 * M + 1];
@@ -58,16 +58,24 @@ public:
 	}
 	int add_slot(std::string key, T val)
 	{
-		if (n_ >= (2 * M))
-			return 0;
-		cur_[n_].key_ = key;
-		cur_[n_].val_ = val;
-		n_++;
-		return 1;
+		for (int i = 0; i <= n_; i++)
+			if ((i == n_) || (key < cur_[i].key_))
+			{
+				for (int j = n_ - 1; j >= i; j--)
+				{
+					cur_[j + 1].key_ = cur_[j].key_;
+					cur_[j + 1].val_ = cur_[j].val_;
+				}
+				cur_[i].key_ = key;
+				cur_[i].val_ = val;
+				n_++;
+				return 0;
+			}
 	}
 	void sth()
 	{
-		std::cout << cur_->key_ << " " << cur_->val_ << "\n"; 
+		for (int i = 0; i < n_; i++)
+		std::cout << cur_[i].key_ << " " << cur_[i].val_ << "\n";
 	}
 };
 
@@ -79,7 +87,38 @@ private:
 public:
 	B_tree()
 	{
-		root_ = new node<T>;
+		root_ = new node <T>;
+	}
+	~B_tree()
+	{
+		delete_B_tree(root_);
+	}
+	int delete_B_tree(node <T> *root)
+	{
+		if (root->next_node_ != 0)
+		for (int i = 0; i <= root_->n_; i++)
+			delete_B_tree(root->next_node_[i]);
+		delete root;
+		return 0;
+	}
+	int add_slot_in(std::string key, T val, node <T> *cur_node)
+	{
+		if (cur_node->n_ == 0)
+		{
+			cur_node->add_slot(key, val);
+			return 0;
+		}
+		for (int i = 0; i <= cur_node->n_; i++)
+			if ((i == cur_node->n_) || (key < cur_node->cur_[i]->key_))
+			{
+				if (cur_node->next_node_ == 0)
+					cur_node->add_slot(key, val);
+				else
+				{
+					add_slot_in(key, val, cur_node->next_node[i]);
+				}
+				return 0;
+			}
 	}
 	
 };
@@ -87,7 +126,10 @@ public:
 int main(void)
 {
 	node <int> N;
-	N.add_slot("stroka", 4);
+	N.add_slot("stroka1", 1);
+	N.add_slot("stroka2", 2);
+	N.add_slot("stroka3", 3);
 	N.sth();
+	
 	return 0;
 }
