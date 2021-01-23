77ffbe868eaf740f47f60baba43b9611bd4d0282
kolorono <62987548+kolorono@users.noreply.github.com>
2021-01-23 19:42:56 +0300

    Восстановил удаленные файлы

---
+++ Krasilnikov_NA/Sem3/1.18/Hash.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "List.h"
+
+using namespace std;
+
+int key(string key);
+
+template <typename T>
+class Hash
+{
+public:
+	class pair
+	{
+	public:
+		string key;
+		T value;
+		pair(string k, T val)
+		{
+			key = k;
+			value = val;
+		}
+	};
+
+	List<pair*> table[65536];
+
+	void fill(string filename)
+	{
+		ifstream in(filename);
+		while (!in.eof())
+		{
+			T t;
+			string k, h;
+			in >> k >> h >> t;
+			/*if ((string(typeid(T).name())).find("class") == string::npos)
+			{
+				string cmd = temp.erase(0, number);
+				if (cmd.find(" ") == string::npos) t = stod(cmd);
+				else while (cmd.find(" ") != string::npos)
+				{
+					if ((string(typeid(T).name())).find("int") != string::npos)
+					{
+						T new_m = new int [n];
+						for (int i = 0; i < n - 1; i++) new_m[i] = t[i];
+						new_m[n - 1] = stod(cmd.substr(0, cmd.find(" ")));
+						cmd.erase(0, cmd.find(" "));
+					}
+				}
+				t = stod(temp.erase(0, number));
+			}
+			else t = temp.erase(0, number);*/
+			table[key(k)].push_back(&pair(k, t));
+		}
+	}
+
+
+	T get(string k)
+	{
+		ListItem<pair*>* temp = table[key(k)].begin().cur;
+		for (int i = 0; i < table[key(k)].lenght(); i++)
+		{
+			temp = temp->next;
+			if (temp->data->key == k) return temp->data->value;
+		}
+		throw Exc(1, "Incorrect key");
+	}
+
+	void insert(string k, T v)
+	{
+		table[key(k)].push_back(&pair(k, v));
+	}
+};
+
