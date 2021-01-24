97beb2c407e12b8402d82edb533901b7ce4b55e4
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2021-01-24 10:44:09 +0300

    Update exam-test.cpp

--- Gorev_VA/examen/exam-test.cpp
+++ Gorev_VA/examen/exam-test.cpp
@@ -20,7 +20,7 @@ int main()
     // you may insert your solution here
     printf("\n-\n-\n-\n");
     Node *root1 = Copy(root, 1000);
-    PrintGraph(stdout, root1);
+    //PrintGraph(stdout, root1);
              
     DeleteGraph(root, maxId);
     //DeleteGraph(root1, maxId + 1000);
