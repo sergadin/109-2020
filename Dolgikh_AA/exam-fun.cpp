88d97aca82ab57ffced70eda1ae0dcc1e9667c7f
Alexandr <62654721+DAMonix@users.noreply.github.com>
2021-01-24 09:25:17 +0300

    Add files via upload

---
+++ Dolgikh_AA/exam-fun.cpp
@@ -0,0 +1,115 @@
+//h file
+#ifndef EXAM_TEST 
+#define EXAM_TEST
+
+#include <stdio.h>
+#include <new>
+
+
+// graph node structure
+struct Node
+{
+    int id;       // id of this vertex
+    Node* left, * right;  // pointers to siblings
+
+    // simplest constuctor
+    Node() { left = right = nullptr; }
+};
+
+Node * GetNode (Node * p, int id)
+{
+   Node *q;
+   if (!p) return nullptr;
+   if (p->id == id) return p;
+   q = GetNode(p->left, id); 
+   return (q)? q : GetNode(p->right, id); 
+}
+
+Node * ReadGraph(char * fname, int &maxId)
+{
+    // file format (list of edges, left or right):
+    //  parent_id   L/R   child_id   
+
+    // the function does not check id of vertices for coincidence, 
+    // i.e., the file should be correct
+    // parent nodes should precede their siblings
+
+    for (char *s = fname; *s; ++s) if (*s=='\n' || *s=='\r') *s = 0;
+    FILE *f = fopen(fname,"r");
+    if (!f) {
+        fprintf(stderr, "cannot open %s\n",fname);
+        return nullptr;
+    } 
+    Node *root = nullptr;
+    Node *p, *q;
+    int id, parid;
+    char dir[8];
+    maxId = -1;
+    while (fscanf(f, "%d%s%d", &parid, dir, &id) == 3) {
+       printf("%d %s %d\n", parid, dir, id); 
+       if (!root) {
+          root = new Node;
+          root->id = parid;
+       }
+       p = GetNode(root, parid);
+       if (!p) break;
+       if (dir[0] == 'L' && p->left) break;
+       if (dir[0] == 'R' && p->right) break;
+       q = GetNode(root, id);
+       if (!q) q = new Node;
+       q->id = id;
+       if (maxId < id) maxId = id;
+       if (dir[0] == 'L') p->left = q;
+       if (dir[0] == 'R') p->right = q;
+    }
+    fclose(f);
+    return root;
+}
+
+void  DeleteGraph(Node *root, int maxId)
+{
+   if (!root) return;
+   Node **nodes = new Node*[maxId + 1];
+   for (int i=0; i<=maxId; i++) {
+      nodes[i] = GetNode(root, i);
+   } 
+   for (int i=0; i<=maxId; i++) {
+      if (nodes[i]) delete nodes[i];
+   } 
+   delete [] nodes;
+}
+
+void PrintGraph(FILE *f, Node* root)
+{
+   if (!f) return;
+   if (!root) return;
+   fprintf(f, "node %3d  ", root->id); 
+   if (root->left) fprintf(f, "L->%3d  ", root->left->id);  
+   if (root->right) fprintf(f, "R->%3d  ", root->right->id);  
+   fputc('\n',f);
+   PrintGraph(f, root->left);
+   PrintGraph(f, root->right);
+}
+
+
+int main()
+{
+    Node* root;
+    int maxId;
+    char fname[64];
+    printf("data file name: ");
+    char* s = fgets(fname, 64, stdin);
+    root = ReadGraph(fname, maxId);
+    if (!root || !s) {
+        printf("cannot process file %s\n", fname);
+        return -1;
+    }
+
+    // test print of the graph
+    PrintGraph(stdout, root);
+
+    // you may insert your solution here
+
+    DeleteGraph(root, maxId);
+    return 0;
+}
