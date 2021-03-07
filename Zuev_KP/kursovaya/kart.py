8ffe7386a0cad78999c682999e9935e9cce71718
ki-zuev <kizuev02@gmail.com>
2021-03-07 03:16:50 +0300

    kursovaya

--- Zuev_KP/kursovaya/kart.py
+++ Zuev_KP/kursovaya/kart.py
@@ -30,48 +30,119 @@ class Card():
         self.Num   = j;
  
     
- 
-    
- 
+def pravila1(a, b, c, n1, n2, n3): #если 1 ходит первым
+    s = int;
+    s = 0;
+    for i in range(1, 33):
+        if (a[i] == 1):
+            n1 = i;
+            neurons1[i] = 0;
+            break
+    for i in range(1, 33):
+        if (mast(i) == mast(n1)) & (b[i] == 1):
+            n2 = i;
+            neurons2[i] = 0;
+            break
+        s = s+1;
+    if s == 32:
+        for i in range(1, 33):
+            if (b[i] == 1):
+                n2 = i;
+                neurons2[i] = 0;
+                break
+    s = 0;
+    for i in range(1, 33):
+        if (mast(i) == mast(n1)) & (c[i] == 1):
+            n3 = i;
+            neurons3[i] = 0;
+            break
+        s = s+1;
+    if s == 32:
+        for i in range(1, 33):
+            if (c[i] == 1):
+                n3 = i;
+                neurons3[i] = 0;
+                break
+        
+        
+        
+        
+
+def pravila2(n1, n2, n3): #если 2 ходит первым
+    print(n2)
+    print(n3)
+    if (n1 == n2) | (n1 == n3):
+        print(n1)
+        
+        
+        
+        
+
+def pravila3(n1, n2, n3): #если 3 ходит первым
+    print(n3)
+    if (n1 == n3) | (n1 != n3):
+        print(n1)
+    print(n2)
     
  
 def mast(a):
-    for i in range(1, 33):
-        if (a[i] == 1):
-            if (i>=1) & (i<=8):
-                return(1);
-            if (i>=9) & (i<=16):
-                return(2);
-            if (i>=17) & (i<=24):
-                return(3);
-            if (i>=25) & (i<=32):
-                return(4);
+    if (a>=1) & (a<=8):
+        return(1);
+    if (a>=9) & (a<=16):
+        return(2);
+    if (a>=17) & (a<=24):
+        return(3);
+    if (a>=25) & (a<=32):
+        return(4);
         
         
         
-#def kinut(a): #что кинуть(правила)
-    
+def kinut(a, b, c): #что кинуть(правила)
+    s = step(1);
+    n1 = 0;
+    n2 = 0;
+    n3 = 0;
+    if (s == 1):
+        pravila1(a, b, c, n1, n2, n3);
+        print(n1);
+        print(n2);
+        print(n3);
+        sravnit1(n1, n2, n3);
 
-
-
-#def step(a): #чей ход и кто следующий
+def step(a): #чей ход и кто следующий
+    s = random.randint(1, 3)
+    s = 1;
+    return s;
+        
             
         
         
-def sravnit(a, b, c, m): #сравнивает карты 
+def sravnit1(a, b, c): #сравнивает карты 
     s1 = 0;
     s2 = 0;
     s3 = 0;
-    if (mast(a) == m) & (mast(b) == m) & (mast(b) == m):    
+    if (mast(b) == mast(a)) & (mast(c) == mast(a)):
         if (a > b) & (a > c):
             s1 = s1+1;
         if (b > a) & (b > c):
             s2 = s2+1;
         if (c > a) & (c > b):
             s3 = s3+1;
+    if (mast(b) != mast(a)) & (mast(c) == mast(a)):
+        if (a > c):
+            s1 = s1+1;
+        else:
+            s3 = s3+1;
+    if (mast(b) == mast(a)) & (mast(c) != mast(a)):
+        if (a > b):
+            s1 = s1+1;
+        else:
+            s2 = s2+1;
+    if (mast(b) != mast(a)) & (mast(c) != mast(a)):
+        s1 = s1+1;
     
-    if (s1 > s2) & (s1 > s3):
-        print(s1);
+    if (s1 < s2) & (s1 < s3):
+        print('');
     else:
         print('');
  
@@ -94,6 +165,7 @@ if __name__ == '__main__':
         print(hand1[i], end='.\n');
         card = deck.get(hand1[i]-1);
         print(card, end='.\n');
+    global neurons1;
     neurons1 = [None] * 33;
     for i in range(1, 11):
          for j in range(1, 33):
@@ -104,6 +176,7 @@ if __name__ == '__main__':
     print(neurons1);
     for i in range(11, 21):
         hand2[i-10] = t[i];
+    global neurons2;
     neurons2 = [None] * 33;
     for i in range(1, 11):
          for j in range(1, 33):
@@ -113,6 +186,7 @@ if __name__ == '__main__':
                  neurons2[j] = 0;
     for i in range(21, 31):
         hand3[i-20] = t[i];
+    global neurons3;
     neurons3 = [None] * 33;
     for i in range(1, 11):
          for j in range(1, 33):
@@ -120,6 +194,5 @@ if __name__ == '__main__':
                  neurons3[j] = 1;
              if (j != hand3[i]) & (neurons3[j] != 1):
                  neurons3[j] = 0;
-    
-    sravnit(neurons1, neurons2, neurons3, 1);
-    
+    for i in range(1, 11):
+        kinut (neurons1, neurons2, neurons3);
