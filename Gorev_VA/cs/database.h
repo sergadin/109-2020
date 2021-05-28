4dd4bb00cf20539c78e734a2e1d344a726dbca28
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2021-05-28 22:31:39 +0300

    Add files via upload

--- Gorev_VA/cs/database.h
+++ Gorev_VA/cs/database.h
@@ -10,6 +10,7 @@ public:
 	int num;
 	std::vector < std::vector <int> > hash;
 	int ind(std::string str) { return (str.length() % MAXLEN); }
+	
 	Base()
 	{
 		name.resize(0);
@@ -18,11 +19,22 @@ public:
 		for (int i = 0; i < MAXLEN; i++)
 			hash[i].resize(0);
 	}
+	int find_detail(std::string str)
+	{
+		int i = ind(str);
+		for (int j = 0; j < hash[i].size(); j++)
+			if (str == name[hash[i][j]])
+				return hash[i][j];
+		return -1;
+	}
 	int add_detail(std::string str)
 	{
+		if (find_detail(str) != -1)
+			return -1;
 		num++;
 		name.push_back(str);
 		hash[ind(str)].push_back(num - 1);
 		return 0;
 	}
+	
 };
