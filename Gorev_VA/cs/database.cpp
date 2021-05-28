90b3578c7bca75c302b1372c8e1bad6544125d88
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2021-05-28 22:17:47 +0300

    Add files via upload

--- Gorev_VA/cs/database.cpp
+++ Gorev_VA/cs/database.cpp
@@ -2,21 +2,29 @@
 #include <string>
 #include <iostream>
 
+#define MAXLEN 10
 class Base
 {
 public:
 	std::vector <std::string> name;
 	int num;
 	std::vector < std::vector <int> > hash;
-	int len(std::string str) { return str.length() % 10; }
+	int ind(std::string str) { return (str.length() % MAXLEN); }
 	Base()
 	{
 		name.resize(0);
 		num = 0;
-		hash.resize(0);
+		hash.resize(MAXLEN);
+		for (int i = 0; i < MAXLEN; i++)
+			hash[i].resize(0);
 	}
 	int add_detail(std::string str)
 	{
+		num++;
+		name.push_back(str);
+		hash[ind(str)].push_back(num - 1);
+		std::cout << MAXLEN << "!!!!!";
+		std::cout << hash[ind(str)][0] << ", " << ind(str) << ", |";
 		return 0;
 	}
 };
@@ -25,5 +33,9 @@ public:
 int main()
 {
 	Base B;
+	std::string S = "ABC";
+	B.add_detail(S);
+	std::cout << B.num << ", " << B.name[0] << ", " << B.hash[3][0];
+	
 	return 0;
 }
--- Gorev_VA/cs/database.h
+++ Gorev_VA/cs/database.h
@@ -2,22 +2,27 @@
 #include <string>
 #include <iostream>
 
+#define MAXLEN 10
 class Base
 {
 public:
 	std::vector <std::string> name;
 	int num;
 	std::vector < std::vector <int> > hash;
-	int len(std::string str) { return str.length() % 10; }
+	int ind(std::string str) { return (str.length() % MAXLEN); }
 	Base()
 	{
 		name.resize(0);
 		num = 0;
-		hash.resize(0);
+		hash.resize(MAXLEN);
+		for (int i = 0; i < MAXLEN; i++)
+			hash[i].resize(0);
 	}
 	int add_detail(std::string str)
 	{
+		num++;
+		name.push_back(str);
+		hash[ind(str)].push_back(num - 1);
 		return 0;
 	}
 };
-
