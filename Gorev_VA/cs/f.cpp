d9aa334db42d58b40349a1610a1dac16b1698481
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2021-05-30 17:35:26 +0300

    Create f.cpp

---
+++ Gorev_VA/cs/f.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <sstream>
+
+int main()
+{
+	char *str ="ABC DEF";
+	
+	char mes[20];
+	sscanf(str+4, "%s", &mes);
+	std::cout << mes << strlen(mes);
+	
+	return 0;
+}
