40f0f8cc4f1b13f7c2ce37ea41aad35099ecf4ad
gorevvadim <62562215+gorevvadim@users.noreply.github.com>
2021-06-01 20:01:48 +0300

    Add files via upload

--- Gorev_VA/cs/server.cpp
+++ Gorev_VA/cs/server.cpp
@@ -135,7 +135,7 @@ int main(int argc, char *argv[])
                 }
 
                 for (int i = 0; i < map.size(); i++)
-                    std::cout << "~~~~" << map[0] << "\n";
+                    std::cout << "~~~~" << map[i] << "\n";
                 // добавляем карту в базу
                 B.add_map(map);
             }
