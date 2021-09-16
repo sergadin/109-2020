d7e7d50d61a04d0a2fa2bef35411922963f0647d
Anastasiya Ponomar <kkkitya@yandex.ru>
2021-09-16 15:02:59 +0300

    :)

---
+++ Ponomar_AE/N2/Client/main.cpp
@@ -0,0 +1,30 @@
+#include "connector.h"
+#include "autotest.h"
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "Error: too few arguments passed." << std::endl;
+		return -1;
+	}
+
+	const std::string localhost = "127.0.0.1";
+	int default_port = 8000;
+
+	// Generating Instructions if needed
+	gen_instructions("Long_Instructions.txt", 50);
+
+	// Running autotest if needed
+        if (autotest("Long_Instructions.txt", "Short_1.txt", "Short_2.txt", localhost, default_port) == true)
+	{
+		std::cout << "Autotest passed successfully." << std::endl;
+		std::cout << "* -------- * * -------- *" << std::endl;
+	}
+	
+	connector C_1(localhost, default_port);
+
+	C_1.polling();
+
+	return 0;
+}
