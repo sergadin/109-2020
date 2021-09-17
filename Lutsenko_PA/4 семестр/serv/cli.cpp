e6155e0266f66ead1a201666e7ee494b52b6e191
lutsenkoPA <63011581+lutsenkoPA@users.noreply.github.com>
2021-09-17 20:56:20 +0300

    0 успеха за целый дн=ень

--- Lutsenko_PA/4 семестр/serv/cli.cpp
+++ Lutsenko_PA/4 семестр/serv/cli.cpp
@@ -14,133 +14,133 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-
 #define PORT_THIS 5556
 #define PORT_TO 5555
 #define SERVER_NAME "127.0.0.1"
-#define BUFLEN 512
+
 
 int sendNToServer (int fd);
 int sendMatrixToServer (int fd);
 int readFromServer (int fd);
 int N;
 
-
-
 int main(void)
 {
- 
-    
-    
 
-    int sock , i, err, opt = 1;
 
-    struct sockaddr_in addr_to; // номер порта и айпи алрес
-    struct sockaddr_in addr_from;
-    struct sockaddr_in addr_this;
-    struct hostent *hostinfo;
 
-    hostinfo = gethostbyname(SERVER_NAME);  // получаем информацию о сервере и кго днс имени
-    if (hostinfo == NULL)
+
+int sock , i, err, opt = 1;
+
+struct sockaddr_in addr_to; // номер порта и айпи алрес
+struct sockaddr_in addr_from;
+struct sockaddr_in addr_this;
+struct hostent *hostinfo;
+
+hostinfo = gethostbyname(SERVER_NAME); // получаем информацию о сервере и кго днс имени
+if (hostinfo == NULL)
+{
+fprintf(stderr, "unknown host %s\n",SERVER_NAME);
+exit (EXIT_FAILURE);
+}
+
+addr_to.sin_family = AF_INET;
+addr_to.sin_port = htons(PORT_TO);
+addr_to.sin_addr = *(struct in_addr*) hostinfo ->h_addr;
+
+/*addr_this.sin_family = AF_INET;
+addr_this.sin_port = htons(PORT_THIS);
+addr_this.sin_addr.s_addr = INADDR_ANY; */
+
+//sock = socket (AF_INET, SOCK_DGRAM, 0);
+    sock = socket (AF_INET, SOCK_STREAM, 0);
+if (sock < 0)
+{
+perror("client: socket was not created \n");
+exit (EXIT_FAILURE);
+}
+setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(char*)&opt,sizeof(opt));
+
+/*err = connect (sock, (struct sockaddr*)&addr_to,sizeof(addr_to));
+if (err < 0)
+{
+perror("client: connect failure \n");
+exit (EXIT_FAILURE);
+}
+fprintf (stdout, "connection is ready\n"); */
+    if(connect(sock, (const struct sockaddr*)&addr_to, sizeof(struct sockaddr_in)) == -1)
     {
-        fprintf(stderr, "unknown host %s\n",SERVER_NAME);
-        exit (EXIT_FAILURE);
+        printf("client: connecting error.\n");
+        exit(-1);
     }
-    
-    addr_to.sin_family = hostinfo-> h_addrtype;
-    addr_to.sin_port = htons(PORT_TO);
-    addr_to.sin_addr = *(struct in_addr*) hostinfo ->h_addr;
-    
-    addr_this.sin_family = AF_INET;
-    addr_this.sin_port = htons(PORT_THIS);
-    addr_this.sin_addr.s_addr  = INADDR_ANY;
-    
-    sock = socket (PF_INET, SOCK_DGRAM, 0);
-    if (sock < 0)
-    {
-        perror("client: socket was not created \n");
-        exit (EXIT_FAILURE);
-    }
-    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(char*)&opt,sizeof(opt));
-    
-    err = bind (sock, (struct sockaddr*)&addr_this,sizeof(addr_this));
-    if (err < 0)
-    {
-        perror("client: connect failure \n");
-        exit (EXIT_FAILURE);
-    }
-    fprintf (stdout, "connection is ready\n");
-    
-    while (1)
-   {    if (sendNToServer(sock)<0) break;
-        if (sendMatrixToServer(sock)<0) break;
-        if (readFromServer(sock)<0) break;
-   }
-    
-    fprintf (stdout, "the end\n");
-    
-    close(sock);
-    exit (EXIT_SUCCESS);
-    
+
+while (1)
+{ if (sendNToServer(sock)<0) break;
+if (sendMatrixToServer(sock)<0) break;
+if (readFromServer(sock)<0) break;
+}
+
+fprintf (stdout, "the end\n");
+
+close(sock);
+exit (EXIT_SUCCESS);
+
 }
 int sendNToServer (int fd)
 {
-    
-    char buf[1];
-    
-    fprintf(stdout, "сколько строк в матрице? > ");
-    fscanf(stdin, "%s", buf);
-    N = atoi(buf);
 
-    int ok;
-     
-    ok = write (fd,buf,1);
-    if (ok < 0) {perror("client:sending N problem");return -1;}
-    else {fprintf(stdout, "client:  я отправил  N = %d .Введите элементы матрицы:\n",N);}
-    return 0;
+char buf[1];
+
+fprintf(stdout, "сколько строк в матрице? > ");
+fscanf(stdin, "%s", buf);
+N = atoi(buf);
+
+int ok;
+
+ok = write (fd,buf,1);
+if (ok < 0) {perror("client:sending N problem");return -1;}
+else {fprintf(stdout, "client: я отправил N = %d .Введите элементы матрицы:\n",N);}
+return 0;
 }
 
 int sendMatrixToServer (int fd)
 {
-   
-    int ok;
-    
-    double *matrix = (double *)malloc(sizeof(double)*N*N);
 
-    
-        for ( int i = 0; i < N; i++ )
-        for (int  j = 0; j < N; j++ )
-        fscanf(stdin, "%lf",  &matrix[i*(N)+j] );
-     
+int ok;
+double *matrixbuf = (double *)malloc(sizeof(double) * N * N);
 
-    ok = write (fd, matrix,N*N);
-    if (ok < 0) {perror("/n client: problem with sending matrix");return -1;}
-    else {fprintf(stdout, "client:  я отправил  matrix ");}
-    return 0;
+
+for ( int i = 0; i < N; i++ ) {
+for (int j = 0; j < N; j++ )
+{ fscanf(stdin, "%lf", &matrixbuf[i*(N)+j] );}}
+
+
+
+ok = write (fd, matrixbuf,N*N);
+if (ok < 0) {perror("\n client: problem with sending matrix");return -1;}
+else {fprintf(stdout, "client: я отправил matrix ");}
+return 0;
 }
 
 int readFromServer (int fd)
 {
-    int ok;
-    double *matrix = (double *)malloc(sizeof(double)*BUFLEN);
+int ok;
+double *matrix = (double *)malloc(sizeof(double)*N*N);
 
-    
-    ok = read(fd,matrix,BUFLEN);
-    if (ok < 0) {perror("read");return -1;}
-    else if (ok == 0) {fprintf (stderr,"client: no message\n");}
-    else {fprintf(stdout, "server's reply:");
-          for ( int i = 0; i < N; i++ )
-            {
-              for (int  j = 0; j < N; j++ )
-                 {
-                   printf( "%.1f\t",  matrix[i*(N)+j] );
-                  }
-               printf( "\n");
-            }}
-    
 
-    return 0;
+ok = read(fd,matrix,N*N);
+if (ok < 0) {perror("read");return -1;}
+else if (ok == 0) {fprintf (stderr,"client: no message\n");}
+else {fprintf(stdout, "server's reply:");
+for ( int i = 0; i < N; i++ )
+{
+for (int j = 0; j < N; j++ )
+{
+printf( "%.1f\t", matrix[i*(N)+j] );
 }
+printf( "\n");
+}}
 
 
-
+return 0;
+}
--- Lutsenko_PA/4 семестр/serv/ser.cpp
+++ Lutsenko_PA/4 семестр/serv/ser.cpp
@@ -12,111 +12,139 @@
 #include "unistd.h"
 
 #define PORT 5555
-#define BUFLEN 512
 
 
-int writeToClient (int fd,  double *matrix);
-int readMatrixFromClient (int fd, double *matrix );
+int writeToClient (int fd, double *matrixbuf);
+int readMatrixFromClient (int fd, double *matrixbuf );
 int readNFromClient (int fd, char *buf );
+int SendMax(int n);
 int N;
 
 int main(void)
 {
- 
-    int sock , i, err;
-    int opt = 1;
-    struct sockaddr_in addr_from;
-    struct sockaddr_in addr_this;
-    char buf[1];
-  
-   
+
+int sock , i, err;
+int opt = 1;
+struct sockaddr_in addr_from;
+struct sockaddr_in addr_this;
+char buf[1];
+    
+    sock = socket (AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+    perror("server: socket was not created \n");
+    exit (EXIT_FAILURE);
+    }
+    else fprintf(stdout, "server: socket is ready\n");
+    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(char*)&opt,sizeof(opt));
+
     
     addr_this.sin_family = AF_INET;
     addr_this.sin_port = htons(PORT);
-    addr_this.sin_addr.s_addr =INADDR_ANY;
-    
-    
-    sock = socket (PF_INET, SOCK_DGRAM, 0);
-    if (sock < 0)
-    {
-        perror("server: socket was not created \n");
-        exit (EXIT_FAILURE);
-    }
-    else fprintf(stdout, "server: socket is ready/n");
-    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(char*)&opt,sizeof(opt));
-    
-    
-    
-    err = bind (sock, (struct sockaddr*)&addr_this,sizeof(addr_this));
-    if (err < 0)
-    {
-        perror("server: can not bund socket \n");
-        exit (EXIT_FAILURE);
-    }
-    else fprintf(stdout, "server: socket is bound/n");
-    
-    
+    addr_this.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    
-    fprintf(stdout, "server: connection ready/n");
-    
-    while (1)
-    {
-    err = readNFromClient(sock,buf);
-    double *matrix = (double *)malloc(sizeof(double) * N*N);
-    err = readMatrixFromClient(sock,matrix);
-    }
-    
-    
+ /*addr_this.sin_family = AF_INET;
+addr_this.sin_port = htons(PORT);
+addr_this.sin_addr.s_addr =INADDR_ANY;
+*/
 
-    
-    
-    fprintf (stdout, "the end\n");
-    
-    close(sock);
-    exit (EXIT_SUCCESS);
-    
-}
 
-int writeToClient (int fd, double *matrix)
+
+
+
+/*err = bind (sock, (struct sockaddr*)&addr_this,sizeof(addr_this));
+if (err < 0)
 {
-    int ok;
-    
-   //  for ( int i = 0; i < n; i++ )
-   //  for (int  j = 0; j < n; j++ )
-    //      matrix[i*(N)+j] = matrix[i*(N)+j]*3;
-        
-      
-
-    ok = write (fd, &matrix ,N*N);
-    if (ok != 0) fprintf(stdout, "server: i sent the answer");
-    
-    if (ok < 0) {perror("server: writing failure");return -1;}
-    return 0;
+perror("server: can not bund socket \n");
+exit (EXIT_FAILURE);
 }
+else fprintf(stdout, "server: socket is bound\n");
+
+   */
+    if(bind(sock, (struct sockaddr*) &addr_this, sizeof(addr_this)) == -1)
+    {
+        printf("Binding socket error.\n");
+        exit(-1);
+    }
+
+    if(listen(sock, 1) == -1)
+    {
+        printf("Listening error.\n");
+        exit(-1);
+    }
+
+
+
+fprintf(stdout, "server: connection ready/n");
+
+while (1)
+{
+err = readNFromClient(sock,buf);
+double *matrixbuf = (double *)malloc(sizeof(double) * N*N);
+err = readMatrixFromClient(sock,matrixbuf);
+    if (err<0) break;
+}
+
+
+
+
+
+fprintf (stdout, "the end\n");
+
+close(sock);
+exit (EXIT_SUCCESS);
+
+}
+
+int writeToClient (int fd, double *matrixbuf)
+{
+int ok;
+
+
+ok = write (fd, &matrixbuf ,N*N);
+if (ok != 0) fprintf(stdout, "server: i sent the answer");
+
+if (ok < 0) {perror("server: writing failure");return -1;}
+return 0;
+}
+
+/*int SendMax(int fd,int n, double *matrix)
+{
+int ok;
+int max = matrix[n*(N)];
+char charmax;
+for (int j = 0; j < n; j++ )
+if (matrix[n*(N)+j] > max) {max = matrix[n*(N)+j];}
+
+charmax = max + '0';
+
+ok = write (fd, charmax ,sizeof(char));
+if (ok != 0) fprintf(stdout, "server: i sent max");
+if (ok < 0) {perror("server: sending max failure");return -1;}
+return 0;
+} */
 
 int readNFromClient (int fd, char *buf)
 {
-    int ok;
-    ok = read(fd,buf,1);
-    N = atoi(buf);
-    if (ok < 0) {perror("server:reading N failure");return -1;}
-    else if (ok == 0) {fprintf (stderr,"server: no N\n"); return -1;}
-    else {fprintf(stdout, "server: i got the N ");}
+int ok;
+ok = read(fd,buf,1);
+N = atoi(buf);
+if (ok < 0) {perror("server:reading N failure");return -1;}
+else if (ok == 0) {fprintf (stderr,"server: no N\n"); return -1;}
+else {fprintf(stdout, "server: i got the N ");}
 
-    return 0;
+return 0;
 }
 
-int readMatrixFromClient (int fd, double *matrix)
+int readMatrixFromClient (int fd, double *matrixbuf)
 {
-    int ok;
-    ok = read(fd,&matrix,N*N);
-    if (ok < 0) {perror("server:reading Matrix failure");return -1;}
-    else if (ok == 0) {fprintf (stderr,"server: no Matrix data\n"); return -1;}
-    else {fprintf(stdout, "server: i got the Matrix ");}
+int ok;
+ok = read(fd,matrixbuf,N*N);
+if (ok < 0) {perror("server:reading Matrix failure");return -1;}
+else if (ok == 0) {fprintf (stderr,"server: no Matrix data\n"); return -1;}
+else {fprintf(stdout, "server: i got the Matrix ");}
 
-    return 0;
+return 0;
 }
-
 
 
